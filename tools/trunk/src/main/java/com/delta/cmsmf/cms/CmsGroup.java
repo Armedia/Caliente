6e5ba398d750f4e0d1fc9330118491714b54f9a3
Diego Rivera <diego.rivera@armedia.com>
2014-09-08 15:27:50 +0000

    Added support for linking the group back to the users that have it marked as their default group.

--- tools/trunk/src/main/java/com/delta/cmsmf/cms/CmsGroup.java
+++ tools/trunk/src/main/java/com/delta/cmsmf/cms/CmsGroup.java
@@ -6,12 +6,17 @@ package com.delta.cmsmf.cms;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.delta.cmsmf.cms.CmsAttributeHandlers.AttributeHandler;
 import com.delta.cmsmf.exception.CMSMFException;
+import com.documentum.com.DfClientX;
+import com.documentum.fc.client.IDfCollection;
 import com.documentum.fc.client.IDfGroup;
 import com.documentum.fc.client.IDfPersistentObject;
+import com.documentum.fc.client.IDfQuery;
 import com.documentum.fc.client.IDfSession;
 import com.documentum.fc.client.IDfUser;
 import com.documentum.fc.common.DfException;
@@ -22,6 +27,8 @@ import com.documentum.fc.common.IDfValue;
  *
  */
 public class CmsGroup extends CmsObject<IDfGroup> {
+
+	private static final String USERS_WITH_DEFAULT_GROUP = "usersWithDefaultGroup";
 
 	private static boolean HANDLERS_READY = false;
 
@@ -42,13 +49,34 @@ public class CmsGroup extends CmsObject<IDfGroup> {
 		CmsGroup.HANDLERS_READY = true;
 	}
 
+	/**
+	 * This DQL will find all users for which this group is marked as the default group,
+	 * and thus all users for whom it must be restored later on.
+	 */
+	private static final String DQL_FIND_USERS_WITH_DEFAULT_GROUP = "SELECT u.user_name FROM dm_user u, dm_group g WHERE u.user_group_name = g.group_name AND g.r_object_id = ''%s''";
+
 	public CmsGroup() {
 		super(CmsObjectType.GROUP, IDfGroup.class);
 		CmsGroup.initHandlers();
 	}
 
 	@Override
-	protected void getDataProperties(Collection<CmsProperty> properties, IDfGroup user) throws DfException {
+	protected void getDataProperties(Collection<CmsProperty> properties, IDfGroup group) throws DfException {
+		final String groupId = group.getObjectId().getId();
+
+		// Store all the users that have this group as their default group
+		IDfQuery dqlQry = new DfClientX().getQuery();
+		dqlQry.setDQL(String.format(CmsGroup.DQL_FIND_USERS_WITH_DEFAULT_GROUP, groupId));
+		IDfCollection resultCol = dqlQry.execute(group.getSession(), IDfQuery.EXEC_QUERY);
+		try {
+			CmsProperty property = new CmsProperty(CmsGroup.USERS_WITH_DEFAULT_GROUP, CmsDataType.DF_STRING);
+			while (resultCol.next()) {
+				property.addValue(resultCol.getValueAt(0));
+			}
+			properties.add(property);
+		} finally {
+			closeQuietly(resultCol);
+		}
 	}
 
 	@Override
@@ -64,22 +92,45 @@ public class CmsGroup extends CmsObject<IDfGroup> {
 		}
 		final IDfSession session = object.getSession();
 		CmsAttribute usersNames = getAttribute(CmsAttributes.USERS_NAMES);
+		// Keep track of missing users so we don't look for them again.
+		Set<String> missingUsers = new HashSet<String>();
 		if (usersNames != null) {
 			// TODO: Support merging in the future?
 			List<IDfValue> actualUsers = new ArrayList<IDfValue>();
 			for (IDfValue v : usersNames) {
 				IDfUser user = session.getUser(v.asString());
 				if (user == null) {
+					missingUsers.add(v.asString());
 					this.logger
-					.warn(String
-						.format(
-							"Failed to link Group [%s] to user [%s] as a member - the user wasn't found - probably didn't need to be copied over",
-							groupName.asString(), v.asString()));
+						.warn(String
+							.format(
+								"Failed to link Group [%s] to user [%s] as a member - the user wasn't found - probably didn't need to be copied over",
+								groupName.asString(), v.asString()));
 					continue;
 				}
 				actualUsers.add(v);
 			}
 			setAttributeOnObject(usersNames, actualUsers, object);
+		}
+
+		// Set this group as users' default group
+		CmsProperty property = getProperty(CmsGroup.USERS_WITH_DEFAULT_GROUP);
+		if (property != null) {
+			for (IDfValue v : property) {
+				if (missingUsers.contains(v.asString())) {
+					continue;
+				}
+				IDfUser user = session.getUser(v.asString());
+				if (user == null) {
+					this.logger
+						.warn(String
+							.format(
+								"Failed to link Group [%s] to user [%s] as the user's default group - the user wasn't found - probably didn't need to be copied over",
+								groupName.asString(), v.asString()));
+					continue;
+				}
+				user.setUserGroupName(groupName.asString());
+			}
 		}
 	}
 
@@ -95,10 +146,10 @@ public class CmsGroup extends CmsObject<IDfGroup> {
 				IDfGroup other = session.getGroup(v.asString());
 				if (other == null) {
 					this.logger
-						.warn(String
-							.format(
-								"Failed to link Group [%s] to group [%s] as a member - the group wasn't found - probably didn't need to be copied over",
-								groupName, v.asString()));
+					.warn(String
+						.format(
+							"Failed to link Group [%s] to group [%s] as a member - the group wasn't found - probably didn't need to be copied over",
+							groupName, v.asString()));
 					continue;
 				}
 				actualGroups.add(v);
