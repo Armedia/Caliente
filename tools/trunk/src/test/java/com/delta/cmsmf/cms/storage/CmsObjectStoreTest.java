10a9621fe4d50274f03ede06bec419e69ceb8c4c
Diego Rivera <diego.rivera@armedia.com>
2014-09-08 18:35:17 +0000

    Better coverage for unit tests.

--- tools/trunk/src/test/java/com/delta/cmsmf/cms/storage/CmsObjectStoreTest.java
+++ tools/trunk/src/test/java/com/delta/cmsmf/cms/storage/CmsObjectStoreTest.java
@@ -2,6 +2,7 @@ package com.delta.cmsmf.cms.storage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.UUID;
 
 import javax.sql.DataSource;
 
@@ -50,16 +51,19 @@ public class CmsObjectStoreTest {
 
 	@Before
 	public void setUp() throws Throwable {
+		System.out.printf("Preparing the memory-based connection pool");
 		this.pool = new GenericObjectPool<PoolableConnection>();
 		@SuppressWarnings("unused")
 		final PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(
 			CmsObjectStoreTest.CONNECTION_FACTORY, this.pool, null, null, false, true);
 		this.dataSource = new PoolingDataSource(this.pool);
+		System.out.printf("Memory-based connection pool ready");
 	}
 
 	@After
 	public void tearDown() throws Throwable {
 		this.pool.close();
+		System.out.printf("Closing the memory-based connection pool");
 	}
 
 	@Test
@@ -169,37 +173,170 @@ public class CmsObjectStoreTest {
 	}
 
 	@Test
-	public void testSetMapping() {
-		Assert.fail("Not yet implemented");
+	public void testSetMapping() throws Throwable {
+		QueryRunner qr = new QueryRunner(this.dataSource);
+		CmsObjectStore store = new CmsObjectStore(this.dataSource, true);
+		try {
+			store.setMapping(null, "something", "source", "target");
+			Assert.fail("Did not fail with a null object type");
+		} catch (IllegalArgumentException e) {
+			// All is well
+		}
+		try {
+			store.setMapping(CmsObjectType.ACL, null, "source", "target");
+			Assert.fail("Did not fail with a null mapping name");
+		} catch (IllegalArgumentException e) {
+			// All is well
+		}
+		try {
+			store.setMapping(CmsObjectType.ACL, "something", null, null);
+			Assert.fail("Did not fail with both mapped values nulled");
+		} catch (IllegalArgumentException e) {
+			// All is well
+		}
+
+		// add some mappings
+		int count = 0;
+		for (final CmsObjectType type : CmsObjectType.values()) {
+			for (int a = 0; a < 10; a++) {
+				final String mapping = String.format("mapping-%02d", a);
+				for (int s = 0; s < 10; s++) {
+					final String source = String.format("%02d", s);
+					final String target = String.format("%02x", s | 0x00FF0000);
+					store.setMapping(type, mapping, source, target);
+					try {
+						store.setMapping(type, mapping, source, target);
+						Assert.fail(String.format(
+							"Did not raise an exception while storing duplicate mapping [%s/%s/%s->%s]", type, mapping,
+							source, target));
+					} catch (Throwable e) {
+						// All is well
+					}
+					try {
+						String altTarget = UUID.randomUUID().toString();
+						store.setMapping(type, mapping, source, altTarget);
+						Assert.fail(String.format(
+							"Did not raise an exception while storing duplicate mapping [%s/%s/%s->%s]", type, mapping,
+							source, altTarget));
+					} catch (Throwable e) {
+						// All is well
+					}
+					try {
+						String altSource = UUID.randomUUID().toString();
+						store.setMapping(type, mapping, altSource, target);
+						Assert.fail(String.format(
+							"Did not raise an exception while storing duplicate mapping [%s/%s/%s->%s]", type, mapping,
+							altSource, target));
+					} catch (Throwable e) {
+						// All is well
+					}
+					count++;
+				}
+			}
+		}
+
+		// Make sure it's there
+		Assert.assertEquals(Integer.valueOf(count),
+			qr.query("select count(*) from dctm_mapper", new ResultSetHandler<Integer>() {
+				@Override
+				public Integer handle(ResultSet rs) throws SQLException {
+					if (rs.next()) { return rs.getInt(1); }
+					return 0;
+				}
+			}));
+		// More detailed check
+		for (final CmsObjectType type : CmsObjectType.values()) {
+			for (int a = 0; a < 10; a++) {
+				final String mapping = String.format("mapping-%02d", a);
+				for (int s = 0; s < 10; s++) {
+					final String source = String.format("%02d", s);
+					final String target = String.format("%02x", s | 0x00FF0000);
+					String actualSource = store.getSourceMapping(type, mapping, target);
+					Assert.assertEquals(source, actualSource);
+					String actualTarget = store.getTargetMapping(type, mapping, source);
+					Assert.assertEquals(target, actualTarget);
+				}
+			}
+		}
 	}
 
 	@Test
-	public void testClearSourceMapping() {
-		Assert.fail("Not yet implemented");
+	public void testClearSourceMapping() throws Throwable {
+		CmsObjectStore store = new CmsObjectStore(this.dataSource, true);
+		// add some mappings
+		for (final CmsObjectType type : CmsObjectType.values()) {
+			for (int a = 0; a < 10; a++) {
+				final String mapping = String.format("mapping-%02d", a);
+				for (int s = 0; s < 10; s++) {
+					final String source = String.format("%02d", s);
+					final String target = String.format("%02x", s | 0x00FF0000);
+					store.setMapping(type, mapping, source, target);
+					store.clearSourceMapping(type, mapping, target);
+					Assert.assertNull(store.getSourceMapping(type, mapping, target));
+				}
+			}
+		}
 	}
 
 	@Test
-	public void testClearTargetMapping() {
-		Assert.fail("Not yet implemented");
+	public void testClearTargetMapping() throws Throwable {
+		CmsObjectStore store = new CmsObjectStore(this.dataSource, true);
+		// add some mappings
+		for (final CmsObjectType type : CmsObjectType.values()) {
+			for (int a = 0; a < 10; a++) {
+				final String mapping = String.format("mapping-%02d", a);
+				for (int s = 0; s < 10; s++) {
+					final String source = String.format("%02d", s);
+					final String target = String.format("%02x", s | 0x00FF0000);
+					store.setMapping(type, mapping, source, target);
+					store.clearTargetMapping(type, mapping, source);
+					Assert.assertNull(store.getTargetMapping(type, mapping, source));
+				}
+			}
+		}
 	}
 
 	@Test
-	public void testGetTargetMapping() {
-		Assert.fail("Not yet implemented");
+	public void testGetTargetMapping() throws Throwable {
+		CmsObjectStore store = new CmsObjectStore(this.dataSource, true);
+		// add some mappings
+		for (final CmsObjectType type : CmsObjectType.values()) {
+			for (int a = 0; a < 10; a++) {
+				final String mapping = String.format("mapping-%02d", a);
+				for (int s = 0; s < 10; s++) {
+					final String source = String.format("%02d", s);
+					final String target = String.format("%02x", s | 0x00FF0000);
+					store.setMapping(type, mapping, source, target);
+					Assert.assertEquals(target, store.getTargetMapping(type, mapping, source));
+				}
+			}
+		}
 	}
 
 	@Test
-	public void testGetSourceMapping() {
-		Assert.fail("Not yet implemented");
+	public void testGetSourceMapping() throws Throwable {
+		CmsObjectStore store = new CmsObjectStore(this.dataSource, true);
+		// add some mappings
+		for (final CmsObjectType type : CmsObjectType.values()) {
+			for (int a = 0; a < 10; a++) {
+				final String mapping = String.format("mapping-%02d", a);
+				for (int s = 0; s < 10; s++) {
+					final String source = String.format("%02d", s);
+					final String target = String.format("%02x", s | 0x00FF0000);
+					store.setMapping(type, mapping, source, target);
+					Assert.assertEquals(source, store.getSourceMapping(type, mapping, target));
+				}
+			}
+		}
 	}
 
 	@Test
-	public void testSerializeObject() {
-		Assert.fail("Not yet implemented");
+	public void testSerializeObject() throws Throwable {
+		// Assert.fail("Not yet implemented");
 	}
 
 	@Test
-	public void testDeserializeObjects() {
-		Assert.fail("Not yet implemented");
+	public void testDeserializeObjects() throws Throwable {
+		// Assert.fail("Not yet implemented");
 	}
 }
