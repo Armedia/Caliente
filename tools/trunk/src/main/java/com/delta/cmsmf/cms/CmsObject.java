1558294b80574826b906d037259fd65e2ff474b7
Diego Rivera <diego.rivera@armedia.com>
2014-09-12 17:33:31 +0000

    Modified to use a DB-agnostic mechanism for updating date values through SQL

--- tools/trunk/src/main/java/com/delta/cmsmf/cms/CmsObject.java
+++ tools/trunk/src/main/java/com/delta/cmsmf/cms/CmsObject.java
@@ -19,7 +19,6 @@ import org.apache.log4j.Logger;
 import com.armedia.commons.utilities.Tools;
 import com.delta.cmsmf.cms.CmsAttributeHandlers.AttributeHandler;
 import com.delta.cmsmf.cms.CmsCounter.Result;
-import com.delta.cmsmf.constants.CMSMFAppConstants;
 import com.delta.cmsmf.exception.CMSMFException;
 import com.delta.cmsmf.utils.DfUtils;
 import com.documentum.fc.client.IDfCollection;
@@ -74,7 +73,7 @@ public abstract class CmsObject<T extends IDfPersistentObject> {
 		if (dfClass == null) { throw new IllegalArgumentException("Must provde a DF class"); }
 		if (type.getDfClass() != dfClass) { throw new IllegalArgumentException(String.format(
 			"Class mismatch: type is tied to class [%s], but was given class [%s]", type.getDfClass()
-			.getCanonicalName(), dfClass.getCanonicalName())); }
+				.getCanonicalName(), dfClass.getCanonicalName())); }
 		this.type = type;
 		this.dfClass = dfClass;
 	}
@@ -275,7 +274,7 @@ public abstract class CmsObject<T extends IDfPersistentObject> {
 	}
 
 	public final void persistDependencies(IDfPersistentObject object, CmsDependencyManager manager) throws DfException,
-	CMSMFException {
+		CMSMFException {
 		if (object == null) { throw new IllegalArgumentException(
 			"Must provide the Documentum object from which to identify the dependencies"); }
 		doPersistDependencies(castObject(object), manager);
@@ -285,7 +284,7 @@ public abstract class CmsObject<T extends IDfPersistentObject> {
 	}
 
 	public final Result saveToCMS(IDfSession session, CmsAttributeMapper mapper) throws DfException, CMSMFException,
-		SQLException {
+	SQLException {
 		if (session == null) { throw new IllegalArgumentException("Must provide a session to save the object"); }
 		if (mapper == null) {
 			mapper = this.NULL_MAPPER;
@@ -480,7 +479,7 @@ public abstract class CmsObject<T extends IDfPersistentObject> {
 		if (object == null) { return null; }
 		if (!this.dfClass.isAssignableFrom(object.getClass())) { throw new DfException(String.format(
 			"Expected an object of class %s, but got one of class %s", this.dfClass.getCanonicalName(), object
-			.getClass().getCanonicalName())); }
+				.getClass().getCanonicalName())); }
 		return this.dfClass.cast(object);
 	}
 
@@ -634,20 +633,10 @@ public abstract class CmsObject<T extends IDfPersistentObject> {
 	protected final void updateModifyDate(T object) throws DfException {
 		final String objType = object.getType().getName();
 		final IDfValue modifyDate = getAttribute(CmsAttributes.R_MODIFY_DATE).getValue();
-		/*
-		 * final IDfValue vStamp = getAttribute(CmsAttributes.I_VSTAMP).getSingleValue(); final
-		 * String sqlStr = String.format(
-		 * "UPDATE %s_s SET r_modify_date = TO_DATE(''%s'', ''%s''), i_vstamp = %d WHERE r_object_id = ''%s''"
-		 * , objType, modifyDate.asTime().asString(CMSMFAppConstants.DCTM_DATETIME_PATTERN),
-		 * CMSMFAppConstants.DCTM_DATETIME_PATTERN, vStamp.asInteger(),
-		 * object.getObjectId().getId());
-		 */
 
 		// TODO: For now we don't touch the i_vstamp b/c we don't think it necessary
-		final String sqlStr = String.format(
-			"UPDATE %s_s SET r_modify_date = TO_DATE(''%s'', ''%s'') WHERE r_object_id = ''%s''", objType, modifyDate
-				.asTime().asString(CMSMFAppConstants.DCTM_DATETIME_PATTERN), CMSMFAppConstants.DCTM_DATETIME_PATTERN,
-			object.getObjectId().getId());
+		final String sqlStr = String.format("UPDATE %s_s SET r_modify_date = %s WHERE r_object_id = ''%s''", objType,
+			DfUtils.generateSqlDateClause(modifyDate.asTime(), object.getSession()), object.getObjectId().getId());
 
 		runExecSQL(object.getSession(), sqlStr);
 	}
