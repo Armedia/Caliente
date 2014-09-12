9072f4b9ac8a1c4007e3da2cc39671d391ba1af1
Diego Rivera <diego.rivera@armedia.com>
2014-09-12 16:25:59 +0000

    First stab at the new importer engine.

--- tools/trunk/src/main/java/com/delta/cmsmf/engine/CmsImporter.java
+++ tools/trunk/src/main/java/com/delta/cmsmf/engine/CmsImporter.java
@@ -4,7 +4,7 @@
 
 package com.delta.cmsmf.engine;
 
-import java.util.UUID;
+import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
@@ -15,24 +15,80 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.log4j.Logger;
 
-import com.delta.cmsmf.cms.DfValueFactory;
+import com.delta.cmsmf.cms.CmsCounter;
+import com.delta.cmsmf.cms.CmsCounter.Result;
+import com.delta.cmsmf.cms.CmsObject;
+import com.delta.cmsmf.cms.CmsObjectType;
+import com.delta.cmsmf.cms.CmsUser;
 import com.delta.cmsmf.cms.pool.DctmSessionManager;
 import com.delta.cmsmf.cms.storage.CmsObjectStore;
+import com.delta.cmsmf.cms.storage.CmsObjectStore.ObjectHandler;
 import com.delta.cmsmf.exception.CMSMFException;
 import com.delta.cmsmf.utils.DfUtils;
-import com.documentum.fc.client.IDfCollection;
-import com.documentum.fc.client.IDfPersistentObject;
-import com.documentum.fc.client.IDfQuery;
 import com.documentum.fc.client.IDfSession;
 import com.documentum.fc.common.DfException;
-import com.documentum.fc.common.IDfId;
-import com.documentum.fc.common.IDfValue;
 
 /**
  * @author Diego Rivera <diego.rivera@armedia.com>
  *
  */
 public class CmsImporter {
+
+	public static class SynchronizedCounter {
+		private long value;
+
+		public SynchronizedCounter() {
+			this(0);
+		}
+
+		public SynchronizedCounter(long initialValue) {
+			this.value = initialValue;
+		}
+
+		public synchronized long decrement() {
+			return increment(-1);
+		}
+
+		public synchronized long decrement(long amount) {
+			return increment(-amount);
+		}
+
+		public synchronized long increment() {
+			return increment(1);
+		}
+
+		public synchronized long increment(long amount) {
+			this.value += amount;
+			notify();
+			return this.value;
+		}
+
+		public synchronized long setValue(long value) {
+			long prev = this.value;
+			this.value = value;
+			notify();
+			return prev;
+		}
+
+		public synchronized long getValue() {
+			return this.value;
+		}
+
+		public synchronized void waitUntilChange() throws InterruptedException {
+			final long current = this.value;
+			while (this.value == current) {
+				wait();
+			}
+			notify();
+		}
+
+		public synchronized void waitForValue(long value) throws InterruptedException {
+			while (this.value != value) {
+				wait();
+			}
+			notify();
+		}
+	}
 
 	private Logger log = Logger.getLogger(getClass());
 
@@ -55,14 +111,12 @@ public class CmsImporter {
 	}
 
 	public void doImport(final CmsObjectStore objectStore, final DctmSessionManager sessionManager) throws DfException,
-	CMSMFException {
+		CMSMFException {
 
-		IDfSession session = sessionManager.acquireSession();
-
+		final SynchronizedCounter waitCounter = new SynchronizedCounter();
 		final AtomicInteger activeCounter = new AtomicInteger(0);
-		final String exitFlag = String.format("%s[%s]", toString(), UUID.randomUUID().toString());
-		final IDfValue exitValue = DfValueFactory.newStringValue(exitFlag);
-		final BlockingQueue<IDfValue> workQueue = new ArrayBlockingQueue<IDfValue>(this.threadCount);
+		final CmsObject<?> exitFlag = new CmsUser();
+		final BlockingQueue<CmsObject<?>> workQueue = new ArrayBlockingQueue<CmsObject<?>>(this.threadCount);
 		ExecutorService executor = new ThreadPoolExecutor(this.threadCount, this.threadCount, 30, TimeUnit.SECONDS,
 			new LinkedBlockingQueue<Runnable>());
 
@@ -70,51 +124,49 @@ public class CmsImporter {
 			@Override
 			public void run() {
 				IDfSession session = sessionManager.acquireSession();
+				if (CmsImporter.this.log.isDebugEnabled()) {
+					CmsImporter.this.log.debug(String.format("Got IDfSession [%s]", DfUtils.getSessionId(session)));
+				}
 				activeCounter.incrementAndGet();
 				try {
 					while (true) {
 						if (CmsImporter.this.log.isDebugEnabled()) {
 							CmsImporter.this.log.debug("Polling the queue...");
 						}
-						final IDfValue next;
+						final CmsObject<?> next;
+						// increase the waiter count
+						waitCounter.increment();
 						try {
 							next = workQueue.take();
 						} catch (InterruptedException e) {
 							Thread.currentThread().interrupt();
 							return;
+						} finally {
+							// decrease the waiter count
+							waitCounter.decrement();
 						}
 
-						if (exitFlag.equals(next.asString())) {
-							// Work complete
+						if (next == exitFlag) {
 							CmsImporter.this.log.info("Exiting the export polling loop");
 							return;
 						}
 
-						final IDfId id = next.asId();
-
 						if (CmsImporter.this.log.isDebugEnabled()) {
-							CmsImporter.this.log.debug(String.format("Polled ID %s", id.getId()));
+							CmsImporter.this.log.debug(String.format("Polled %s", next));
 						}
 
-						if (CmsImporter.this.log.isDebugEnabled()) {
-							CmsImporter.this.log.debug(String.format("Got IDfSession [%s]",
-								DfUtils.getSessionId(session)));
-						}
+						Result result = Result.FAILED;
 						try {
-							IDfPersistentObject dfObj = session.getObject(id);
+							result = next.saveToCMS(session, objectStore);
 							if (CmsImporter.this.log.isDebugEnabled()) {
-								CmsImporter.this.log.debug(String.format("Retrieved [%s] object with id [%s]", dfObj
-									.getType().getName(), dfObj.getObjectId().getId()));
-							}
-							objectStore.persistDfObject(dfObj);
-							if (CmsImporter.this.log.isDebugEnabled()) {
-								CmsImporter.this.log.debug(String.format("Persisted [%s] object with id [%s]", dfObj
-									.getType().getName(), dfObj.getObjectId().getId()));
+								CmsImporter.this.log.debug(String.format("Persisted (%s) %s", result, next));
 							}
 						} catch (Throwable t) {
+							result = Result.FAILED;
 							// Log the error, move on
-							CmsImporter.this.log.error(
-								String.format("Exception caught processing object with ID [%s]", id.getId()), t);
+							CmsImporter.this.log.error(String.format("Exception caught processing %s", next), t);
+						} finally {
+							CmsCounter.incrementCounter(next, result);
 						}
 					}
 				} finally {
@@ -130,70 +182,85 @@ public class CmsImporter {
 		}
 		executor.shutdown();
 
+		// 1: run the query for the given predicate
 		try {
-			// 1: run the query for the given predicate
-			final String dql = String.format("select r_object_id %s", "asfd");
-			IDfCollection results = DfUtils.executeQuery(session, dql, IDfQuery.DF_EXECREAD_QUERY);
-			try {
-				// 2: iterate over the results, gathering up the object IDs
-				int counter = 0;
-				while (results.next()) {
-					if (this.log.isTraceEnabled()) {
-						this.log.trace("Retrieving the next ID from the query");
-					}
-					final IDfValue id = results.getValue("r_object_id");
-					counter++;
-					try {
-						workQueue.put(id);
-					} catch (InterruptedException e) {
-						Thread.currentThread().interrupt();
-						if (this.log.isDebugEnabled()) {
-							this.log.warn(String.format("Thread interrupted after reading %d object IDs", counter), e);
-						} else {
-							this.log.warn(String.format("Thread interrupted after reading %d objects IDs", counter));
+			final Set<CmsObjectType> containedTypes = objectStore.getStoredObjectTypes();
+			final ObjectHandler handler = new ObjectHandler() {
+				@Override
+				public boolean handle(CmsObject<?> dataObject) {
+					workQueue.add(dataObject);
+					return true;
+				}
+			};
+
+			outer: for (CmsObjectType type : CmsObjectType.values()) {
+				if (!containedTypes.contains(type)) {
+					this.log.warn(String.format("No %s objects are contained in the export", type.name()));
+					continue;
+				}
+
+				objectStore.deserializeObjects(type, handler);
+
+				// We're done, we must wait until all workers are waiting
+				// This "weird" condition allows us to wait until there are
+				// as many waiters as there are active threads. This covers
+				// the contingency of threads dying on us, which SHOULDN'T
+				// happen, but still, we defend against it.
+				synchronized (waitCounter) {
+					this.log.info(String.format("Submitted the entire %s workload, waiting for it to complete",
+						type.name()));
+					while (waitCounter.getValue() < activeCounter.get()) {
+						try {
+							waitCounter.wait();
+							waitCounter.notify();
+						} catch (InterruptedException e) {
+							// We've been interrupted...that means that...what?
+							this.log.fatal(String.format("Interrupted while waiting for the %s workload to complete",
+								type));
+							break outer;
 						}
-						break;
 					}
 				}
-				for (int i = 0; i < this.threadCount; i++) {
-					try {
-						workQueue.put(exitValue);
-					} catch (InterruptedException e) {
-						// Here we have a problem: we're timing out while adding the exit values...
-						this.log.warn("Interrupted while attempting to request executor thread termination", e);
-						break;
-					}
+			}
+
+			for (int i = 0; i < this.threadCount; i++) {
+				try {
+					workQueue.put(exitFlag);
+				} catch (InterruptedException e) {
+					// Here we have a problem: we're timing out while adding the exit values...
+					this.log.warn("Interrupted while attempting to request executor thread termination", e);
+					break;
 				}
-				int pending = activeCounter.get();
-				if (pending > 0) {
-					try {
-						this.log.info(String
-							.format("Waiting for pending workers to terminate (maximum 5 minutes, %d pending workers)",
-								pending));
-						executor.awaitTermination(5, TimeUnit.MINUTES);
-					} catch (InterruptedException e) {
-						this.log.warn("Interrupted while waiting for normal executor termination", e);
-					}
+			}
+			int pending = activeCounter.get();
+			if (pending > 0) {
+				try {
+					this.log.info(String.format(
+						"Waiting for pending workers to terminate (maximum 5 minutes, %d pending workers)", pending));
+					executor.awaitTermination(5, TimeUnit.MINUTES);
+				} catch (InterruptedException e) {
+					this.log.warn("Interrupted while waiting for normal executor termination", e);
 				}
-			} finally {
-				executor.shutdownNow();
-				int pending = activeCounter.get();
-				if (pending > 0) {
-					try {
-						this.log
+			}
+		} finally {
+			executor.shutdownNow();
+			int pending = activeCounter.get();
+			if (pending > 0) {
+				try {
+					this.log
 						.info(String
 							.format(
 								"Waiting an additional 60 seconds for worker termination as a contingency (%d pending workers)",
 								pending));
-						executor.awaitTermination(1, TimeUnit.MINUTES);
-					} catch (InterruptedException e) {
-						this.log.warn("Interrupted while waiting for immediate executor termination", e);
-					}
+					executor.awaitTermination(1, TimeUnit.MINUTES);
+				} catch (InterruptedException e) {
+					this.log.warn("Interrupted while waiting for immediate executor termination", e);
 				}
-				DfUtils.closeQuietly(results);
 			}
-		} finally {
-			sessionManager.releaseSession(session);
+			for (CmsObjectType type : CmsObjectType.values()) {
+				this.log.info(String.format("Action report for %s:%n%s", type.name(), CmsCounter.generateReport(type)));
+			}
+			this.log.info(String.format("Summary Report:%n%s", CmsCounter.generateCummulativeReport()));
 		}
 	}
 }
