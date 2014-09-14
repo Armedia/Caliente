713b1adef4a3598ce9811597e2ff2670a16b5fa9
Diego Rivera <diego.rivera@armedia.com>
2014-09-14 18:22:26 +0000

    Added parameters for specifying special users, groups, and types.

--- tools/trunk/src/main/java/com/delta/cmsmf/mainEngine/CLIParam.java
+++ tools/trunk/src/main/java/com/delta/cmsmf/mainEngine/CLIParam.java
@@ -18,6 +18,10 @@ public enum CLIParam {
 	log_name(null, true, "The base name of the log file to use instead of the default (cmsmf-${action})"),
 	log4j(null, true, "The Log4j configuration (XML format) to use instead of the default (overrides --log_name)"),
 	threads(null, true, "The number of threads to use while importing or exporting"),
+	special_users(Setting.SPECIAL_USERS, true, "The special users that should not be imported into the target instance"),
+	special_groups(Setting.SPECIAL_GROUPS, true,
+		"The special users that should not be imported into the target instance"),
+	special_types(Setting.SPECIAL_TYPES, true, "The special types that should not be imported into the target instance"),
 	batch_size(Setting.EXPORT_BATCH_SIZE, true, "The batch size to use when exporting objects from Documentum"),
 	post_process(Setting.POST_PROCESS_IMPORT, false, "Whether to post-process the imported content"),
 	predicate(Setting.EXPORT_PREDICATE, true, "The DQL 'from-where' predicate to use for exporting"),
@@ -28,12 +32,13 @@ public enum CLIParam {
 	attributes(Setting.OWNER_ATTRIBUTES, true, "The attributes to check for"),
 	errorCount(Setting.IMPORT_MAX_ERRORS, true, "The number of errors to accept before aborting an import"),
 	defaultPassword(Setting.DEFAULT_USER_PASSWORD, true,
-		"The default password to use for users being copied over (leave blank to use the same login name)"), mailTo(
-		Setting.MAIL_RECIPIENTS, true, "The comma-separated list of recipients for the status e-mails"), mailFrom(
-		Setting.MAIL_FROM_ADDX, true, "The e-mail address to use as the sender for the status e-mails"), smtpHost(
-		Setting.MAIL_SMTP_HOST, true, "The SMTP server through which e-mail must be sent"), skip_users(
-		Setting.SKIP_USERS, false, "Skip exporting users"), skip_groups(Setting.SKIP_GROUPS, false,
-		"Skip exporting groups"), skip_acls(Setting.SKIP_ACLS, false, "Skip exporting acls");
+		"The default password to use for users being copied over (leave blank to use the same login name)"),
+	mailTo(Setting.MAIL_RECIPIENTS, true, "The comma-separated list of recipients for the status e-mails"),
+	mailFrom(Setting.MAIL_FROM_ADDX, true, "The e-mail address to use as the sender for the status e-mails"),
+	smtpHost(Setting.MAIL_SMTP_HOST, true, "The SMTP server through which e-mail must be sent"),
+	skip_users(Setting.SKIP_USERS, false, "Skip exporting users"),
+	skip_groups(Setting.SKIP_GROUPS, false, "Skip exporting groups"),
+	skip_acls(Setting.SKIP_ACLS, false, "Skip exporting acls");
 
 	public final Setting property;
 	public final Option option;
