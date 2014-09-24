b92554bf07b954fa801cc9e65cf2838a2bced659
Diego Rivera <diego.rivera@armedia.com>
2014-09-24 04:14:54 +0000

    Release 2.2.5: ================================================================================ * Made the object_label field in the export DB arbitrarily long (varchar(32) -> text), as it appears some files have miles-long names * Added parent folder locking when updating documents' parent folder - the same order as for folders (lowest-id-first) * Added support for short-failing batches as soon as one element in the batch fails (i.e. when importing version histories), to avoid unnecessary noise in the logs from failures stemming from that particular failure. The remaining unprocessed objects will be reported as SKIPPED, since no attempt was made to process them because of the failure.

---
+++ tools/tags/2.2.5/src/main/java/com/delta/cmsmf/engine/CmsImportEngineListener.java
@@ -0,0 +1,84 @@
+package com.delta.cmsmf.engine;
+
+import java.util.Map;
+
+import com.delta.cmsmf.cms.CmsImportResult;
+import com.delta.cmsmf.cms.CmsObject;
+import com.delta.cmsmf.cms.CmsObjectType;
+
+public interface CmsImportEngineListener {
+
+	/**
+	 * <p>
+	 * Invoked when the import has begun and the total number of objects, of all types, that are
+	 * expected to be processed. Please note that due to some import circumstances the actual number
+	 * of objects operated upon may not match the total.
+	 * </p>
+	 *
+	 * @param summary
+	 */
+	public void importStarted(Map<CmsObjectType, Integer> summary);
+
+	/**
+	 * <p>
+	 * Invoked when importing objects of the given type has begun, and the estimated number of
+	 * objects that will be processed.
+	 * </p>
+	 *
+	 * @param objectType
+	 * @param totalObjects
+	 */
+	public void objectTypeImportStarted(CmsObjectType objectType, int totalObjects);
+
+	/**
+	 * <p>
+	 * Invoked when the import has started for the given object.
+	 * </p>
+	 *
+	 * @param object
+	 */
+	public void objectImportStarted(CmsObject<?> object);
+
+	/**
+	 * <p>
+	 * Invoked when the given object has been imported, and indicating the result of the import
+	 * operation.
+	 * </p>
+	 *
+	 * @param object
+	 * @param cmsImportResult
+	 */
+	public void objectImportCompleted(CmsObject<?> object, CmsImportResult cmsImportResult);
+
+	/**
+	 * <p>
+	 * Invoked when the import attempt on the given object has failed, and indicating the exception
+	 * that was raised.
+	 * </p>
+	 *
+	 * @param object
+	 * @param thrown
+	 */
+	public void objectImportFailed(CmsObject<?> object, Throwable thrown);
+
+	/**
+	 * <p>
+	 * Invoked when all the objects of the given type have been imported, indicating a breakdown of
+	 * the actual counts of objects and their results.
+	 * </p>
+	 *
+	 * @param objectType
+	 * @param counters
+	 */
+	public void objectTypeImportFinished(CmsObjectType objectType, Map<CmsImportResult, Integer> counters);
+
+	/**
+	 * <p>
+	 * Invoked when the import process has concluded, and with a breakdown including the total
+	 * counters for all object types.
+	 * </p>
+	 *
+	 * @param counters
+	 */
+	public void importFinished(Map<CmsImportResult, Integer> counters);
+}
