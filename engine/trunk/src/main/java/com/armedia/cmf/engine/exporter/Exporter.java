2e5a073a39c71412059aa7bc2ee591f1c4b40638
Diego Rivera <diego.rivera@armedia.com>
2014-10-31 17:40:48 +0000

    Completed the generalized algorithm for exporting objects.

--- engine/trunk/src/main/java/com/armedia/cmf/engine/exporter/Exporter.java
+++ engine/trunk/src/main/java/com/armedia/cmf/engine/exporter/Exporter.java
@@ -5,11 +5,11 @@
 package com.armedia.cmf.engine.exporter;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CancellationException;
@@ -29,9 +29,12 @@ import com.armedia.cmf.engine.SessionWrapper;
 import com.armedia.cmf.storage.ContentStreamStore;
 import com.armedia.cmf.storage.ObjectStorageTranslator;
 import com.armedia.cmf.storage.ObjectStore;
+import com.armedia.cmf.storage.ObjectStoreOperation;
 import com.armedia.cmf.storage.StorageException;
 import com.armedia.cmf.storage.StoredObject;
 import com.armedia.cmf.storage.StoredObjectType;
+import com.armedia.cmf.storage.StoredValueEncoderException;
+import com.armedia.cmf.storage.UnsupportedObjectTypeException;
 import com.armedia.commons.utilities.Tools;
 
 /**
@@ -39,6 +42,61 @@ import com.armedia.commons.utilities.Tools;
  *
  */
 public abstract class Exporter<T, V, S, W extends SessionWrapper<S>> implements ExportEngineListener {
+
+	protected static final class Target {
+		private final StoredObjectType type;
+		private final String id;
+		private final Long number;
+
+		private Target() {
+			this.type = null;
+			this.id = null;
+			this.number = null;
+		}
+
+		public Target(StoredObjectType type, String id) {
+			this(type, id, null);
+		}
+
+		public Target(StoredObjectType type, String id, Long number) {
+			if (type == null) { throw new IllegalArgumentException("Must provide an object type"); }
+			if (id == null) { throw new IllegalArgumentException("Must provide an object id"); }
+			this.type = type;
+			this.id = id;
+			this.number = number;
+		}
+
+		public StoredObjectType getType() {
+			return this.type;
+		}
+
+		public String getId() {
+			return this.id;
+		}
+
+		public Long getNumber() {
+			return this.number;
+		}
+
+		@Override
+		public int hashCode() {
+			return Tools.hashTool(this, null, this.type, this.id);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (!Tools.baseEquals(this, obj)) { return false; }
+			Target other = Target.class.cast(obj);
+			if (!Tools.equals(this.type, other.type)) { return false; }
+			if (!Tools.equals(this.id, other.id)) { return false; }
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("Target [type=%s, id=%s, number=%s]", this.type, this.id, this.number);
+		}
+	}
 
 	private static final int DEFAULT_THREAD_COUNT = 16;
 	private static final int MIN_THREAD_COUNT = 1;
@@ -50,20 +108,29 @@ public abstract class Exporter<T, V, S, W extends SessionWrapper<S>> implements 
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
-	private final ObjectStore objectStore;
+	private final ObjectStore<?, ?> objectStore;
 	private final ContentStreamStore contentStreamStore;
 	private final ObjectStorageTranslator<T, V> translator;
 	private final SessionFactory<S, W> sessionFactory;
 	private int threadCount = Exporter.DEFAULT_THREAD_COUNT;
 	private int backlogSize = Exporter.DEFAULT_BACKLOG_SIZE;
 
-	public Exporter(ObjectStore objectStore, ContentStreamStore contentStreamStore,
-		ObjectStorageTranslator<T, V> translator, SessionFactory<S, W> sessionFactory) {
+	public <C, O extends ObjectStoreOperation<C>> Exporter(ObjectStore<C, O> objectStore,
+		ContentStreamStore contentStreamStore, ObjectStorageTranslator<T, V> translator,
+		SessionFactory<S, W> sessionFactory) {
 		super();
 		this.objectStore = objectStore;
 		this.contentStreamStore = contentStreamStore;
 		this.translator = translator;
 		this.sessionFactory = sessionFactory;
+	}
+
+	protected final ContentStreamStore getContentStreamStore() {
+		return this.contentStreamStore;
+	}
+
+	protected final ObjectStorageTranslator<T, V> getTranslator() {
+		return this.translator;
 	}
 
 	public final synchronized int setThreadCount(int threadCount) {
@@ -90,16 +157,139 @@ public abstract class Exporter<T, V, S, W extends SessionWrapper<S>> implements 
 		return null;
 	}
 
-	protected final ExportResult processObject(T object) throws ExportException {
-		if (object == null) { throw new IllegalArgumentException("Must provide an object to load"); }
-		return null;
+	protected final T getObject(W session, StoredObjectType type, String id) throws Exception {
+		if (session == null) { throw new IllegalArgumentException("Must provide a session to operate with"); }
+		if (type == null) { throw new IllegalArgumentException("Must provide an object type"); }
+		if (id == null) { throw new IllegalArgumentException("Must provide an object id"); }
+		return doGetObject(session, type, id);
 	}
 
-	protected abstract Iterator<String> findExportResults(S session, Map<String, Object> settings) throws Exception;
+	protected abstract T doGetObject(W session, StoredObjectType type, String id) throws Exception;
 
-	protected abstract StoredObject<V> marshal(T object) throws ExportException;
+	protected final Collection<T> identifyRequirements(W session, T object) throws Exception {
+		if (session == null) { throw new IllegalArgumentException("Must provide a session to operate with"); }
+		if (object == null) { throw new IllegalArgumentException(
+			"Must provide an object whose requirements to identify"); }
+		return doIdentifyRequirements(session, object);
+	}
 
-	protected abstract T getObjectById(String id) throws Exception;
+	protected Collection<T> doIdentifyRequirements(W session, T object) throws Exception {
+		return Collections.emptyList();
+	}
+
+	protected final Collection<T> identifyDependents(W session, T object) throws Exception {
+		if (session == null) { throw new IllegalArgumentException("Must provide a session to operate with"); }
+		if (object == null) { throw new IllegalArgumentException("Must provide an object whose dependents to identify"); }
+		return doIdentifyDependents(session, object);
+	}
+
+	protected Collection<T> doIdentifyDependents(W session, T object) throws Exception {
+		return Collections.emptyList();
+	}
+
+	protected abstract StoredObject<V> marshal(W session, T object) throws ExportException;
+
+	protected final void storeSupplemental(W session, T object) throws Exception {
+		if (session == null) { throw new IllegalArgumentException("Must provide a session to operate with"); }
+		if (object == null) { throw new IllegalArgumentException("Must provide an object whose dependents to identify"); }
+		doStoreSupplemental(session, object);
+	}
+
+	protected abstract void doStoreSupplemental(W session, T object) throws Exception;
+
+	private Long exportObject(W session, StoredObject<V> marshaled, T sourceObject) throws ExportException,
+		StorageException, StoredValueEncoderException, UnsupportedObjectTypeException {
+		if (session == null) { throw new IllegalArgumentException("Must provide a session to operate with"); }
+		if (marshaled == null) { throw new IllegalArgumentException(
+			"Must provide an object whose dependents to identify"); }
+
+		final String id = marshaled.getId();
+		final StoredObjectType type = marshaled.getType();
+		final String label = String.format("%s [%s](%s)", type, marshaled.getLabel(), id);
+
+		// First, make sure other threads don't work on this same object
+		if (!this.objectStore.lockForStorage(type, id)) {
+			if (this.log.isTraceEnabled()) {
+				this.log.trace(String.format("%s is already locked for storage", label));
+			}
+			return null;
+		}
+
+		if (this.log.isDebugEnabled()) {
+			this.log.debug(String.format("Locked %s for storage", label));
+		}
+
+		// Make sure the object hasn't already been exported
+		if (this.objectStore.isStored(type, id)) {
+			// Should be impossible, but still guard against it
+			if (this.log.isTraceEnabled()) {
+				this.log.trace(String.format("%s was locked for storage by this thread, but is already stored...",
+					label));
+			}
+			return null;
+		}
+
+		Collection<T> referenced;
+		try {
+			referenced = identifyRequirements(session, sourceObject);
+		} catch (Exception e) {
+			throw new ExportException(String.format("Failed to identify the requirements for %s", label), e);
+		}
+
+		if (this.log.isDebugEnabled()) {
+			this.log.debug(String.format("%s requires %d objects for successful storage", label, referenced.size()));
+		}
+		for (T requirement : referenced) {
+			StoredObject<V> req = marshal(session, requirement);
+			if (this.log.isDebugEnabled()) {
+				this.log.debug(String.format("%s requires %s [%s](%s)", label, req.getType(), req.getLabel(),
+					req.getId()));
+			}
+			exportObject(session, req, requirement);
+		}
+
+		final Long ret = this.objectStore.storeObject(marshaled, this.translator);
+		if (ret == null) {
+			// Should be impossible, but still guard against it
+			if (this.log.isTraceEnabled()) {
+				this.log.trace(String.format("%s was stored by another thread", label));
+			}
+			return null;
+		}
+
+		if (this.log.isDebugEnabled()) {
+			this.log.debug(String.format("Successfully stored %s as object # %d", label, ret));
+		}
+
+		try {
+			referenced = identifyDependents(session, sourceObject);
+		} catch (Exception e) {
+			throw new ExportException(String.format("Failed to identify the dependents for %s", label), e);
+		}
+
+		if (this.log.isDebugEnabled()) {
+			this.log.debug(String.format("%s has %d dependent objects to store", label, referenced.size()));
+		}
+		for (T dependent : referenced) {
+			StoredObject<V> dep = marshal(session, dependent);
+			if (this.log.isDebugEnabled()) {
+				this.log.debug(String.format("%s requires %s [%s](%s)", label, dep.getType(), dep.getLabel(),
+					dep.getId()));
+			}
+			exportObject(session, dep, dependent);
+		}
+
+		if (this.log.isDebugEnabled()) {
+			this.log.debug(String.format("Executing supplemental storage for %s", label));
+		}
+		try {
+			storeSupplemental(session, sourceObject);
+		} catch (Exception e) {
+			throw new ExportException(String.format("Failed to execute the supplemental storage for %s", label), e);
+		}
+
+		return ret;
+	}
 
 	public final void runExport(Map<String, Object> settings) throws ExportException, StorageException {
 		// We get this at the very top because if this fails, there's no point in continuing.
@@ -119,8 +309,8 @@ public abstract class Exporter<T, V, S, W extends SessionWrapper<S>> implements 
 		}
 
 		final AtomicInteger activeCounter = new AtomicInteger(0);
-		final String exitValue = String.format("%s[%s]", toString(), UUID.randomUUID().toString());
-		final BlockingQueue<String> workQueue = new ArrayBlockingQueue<String>(backlogSize);
+		final Target exitValue = new Target();
+		final BlockingQueue<Target> workQueue = new ArrayBlockingQueue<Target>(backlogSize);
 		final ExecutorService executor = new ThreadPoolExecutor(threadCount, threadCount, 30, TimeUnit.SECONDS,
 			new LinkedBlockingQueue<Runnable>());
 
@@ -137,7 +327,7 @@ public abstract class Exporter<T, V, S, W extends SessionWrapper<S>> implements 
 				if (Exporter.this.log.isDebugEnabled()) {
 					Exporter.this.log.debug(String.format("Got session [%s]", session.getId()));
 				}
-				final Logger output = getOutput();
+				// final Logger output = getOutput();
 				// Begin transaction
 				activeCounter.incrementAndGet();
 				session.begin();
@@ -146,7 +336,7 @@ public abstract class Exporter<T, V, S, W extends SessionWrapper<S>> implements 
 						if (Exporter.this.log.isDebugEnabled()) {
 							Exporter.this.log.debug("Polling the queue...");
 						}
-						final String next;
+						final Target next;
 						try {
 							next = workQueue.take();
 						} catch (InterruptedException e) {
@@ -166,54 +356,42 @@ public abstract class Exporter<T, V, S, W extends SessionWrapper<S>> implements 
 						}
 
 						if (Exporter.this.log.isDebugEnabled()) {
-							Exporter.this.log.debug(String.format("Polled ID %s", next));
+							Exporter.this.log.debug(String.format("Polled %s", next));
 						}
 
-						final T sourceObject;
-						final StoredObjectType type;
 						try {
-							sourceObject = getObjectById(next);
+							final T sourceObject = getObject(baseSession, next.type, next.id);
 							if (sourceObject == null) {
 								// No object found with that ID...
-								Exporter.this.log.warn(String.format("No object found with ID[%s]", next));
+								Exporter.this.log.warn(String.format("No %s object found with ID[%s]", next.type,
+									next.id));
 								continue;
 							}
 
-							type = Exporter.this.translator.decodeObjectType(sourceObject);
 							if (Exporter.this.log.isDebugEnabled()) {
-								Exporter.this.log.debug(String.format("Retrieved %s object with id [%s]", type.name(),
-									next));
+								Exporter.this.log.debug(String.format("Exporting the source %s object with ID[%s]",
+									next.type, next.id));
 							}
-						} catch (Exception e) {
-							// Failed to identify the object being exported
-							Exporter.this.log.error(
-								String.format("Failed to identify the exported object with ID [%s]", next), e);
-							continue;
-						}
-
-						try {
-							objectExportStarted(type, next);
-
-							StoredObject<V> object = marshal(sourceObject);
-							/*
-							DctmExportContext ctx = new DctmExportContext(next, session, Exporter.this.objectStore,
-								Exporter.this.contentStreamStore, output, Exporter.this.exportListener);
-							 */
-							Long objectNumber = Exporter.this.objectStore.storeObject(object, Exporter.this.translator);
-							if (objectNumber != null) {
-								objectExportCompleted(object, objectNumber);
+							objectExportStarted(next.type, next.id);
+							StoredObject<V> marshaled = marshal(baseSession, sourceObject);
+							Long result = exportObject(baseSession, marshaled, sourceObject);
+							if (marshaled != null) {
+								if (Exporter.this.log.isDebugEnabled()) {
+									Exporter.this.log.debug(String.format("Exported %s [%s](%s) in position %d",
+										marshaled.getType(), marshaled.getLabel(), marshaled.getId(), result));
+								}
+								objectExportCompleted(marshaled, result);
 							} else {
-								objectSkipped(type, next);
-							}
-							if (Exporter.this.log.isDebugEnabled()) {
-								Exporter.this.log.debug(String.format("Persisted %s object with id [%s]", type, next));
+								if (Exporter.this.log.isDebugEnabled()) {
+									Exporter.this.log.debug(String.format("%s object with ID[%s] was already exported",
+										next.type, next.id));
+								}
+								objectSkipped(next.type, next.id);
 							}
 						} catch (Exception e) {
-							// Log the error, move on
-							objectExportFailed(type, next, e);
 							Exporter.this.log.error(
-								String.format("Exception caught processing object with ID [%s]", next), e);
-						} finally {
+								String.format("Failed to export %s object with ID[%s]", next.type, next.id), e);
+							objectExportFailed(next.type, next.id, e);
 						}
 					}
 				} finally {
@@ -230,7 +408,7 @@ public abstract class Exporter<T, V, S, W extends SessionWrapper<S>> implements 
 		}
 		executor.shutdown();
 
-		final Iterator<String> it;
+		final Iterator<Target> it;
 		try {
 			it = findExportResults(baseSession.getWrapped(), settings);
 		} catch (Exception e) {
@@ -244,17 +422,17 @@ public abstract class Exporter<T, V, S, W extends SessionWrapper<S>> implements 
 			// 2: iterate over the results, gathering up the object IDs
 			while (it.hasNext()) {
 				if (this.log.isTraceEnabled()) {
-					this.log.trace("Retrieving the next ID from search");
+					this.log.trace("Retrieving the next target from search");
 				}
-				String id = it.next();
+				Target id = it.next();
 				try {
 					workQueue.put(id);
 				} catch (InterruptedException e) {
 					Thread.currentThread().interrupt();
 					if (this.log.isDebugEnabled()) {
-						this.log.warn(String.format("Thread interrupted after reading %d object IDs", counter), e);
+						this.log.warn(String.format("Thread interrupted after reading %d object targets", counter), e);
 					} else {
-						this.log.warn(String.format("Thread interrupted after reading %d objects IDs", counter));
+						this.log.warn(String.format("Thread interrupted after reading %d objects targets", counter));
 					}
 					break;
 				}
@@ -298,9 +476,9 @@ public abstract class Exporter<T, V, S, W extends SessionWrapper<S>> implements 
 				}
 				this.log.info("All the export workers are done.");
 			} finally {
-				List<String> remaining = new ArrayList<String>();
+				List<Target> remaining = new ArrayList<Target>();
 				workQueue.drainTo(remaining);
-				for (String v : remaining) {
+				for (Target v : remaining) {
 					if (v == exitValue) {
 						continue;
 					}
@@ -350,4 +528,7 @@ public abstract class Exporter<T, V, S, W extends SessionWrapper<S>> implements 
 			}
 		}
 	}
+
+	protected abstract Iterator<Target> findExportResults(S session, Map<String, Object> settings) throws Exception;
+
 }
