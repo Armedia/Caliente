9ec20e995e5bbf58eef7f925cca80abbf80277dc
Diego Rivera <diego.rivera@armedia.com>
2014-11-06 22:06:01 +0000

    Removed the export target - no longer needed as that's specified when obtaining the Export Engine instance via the static method.

--- engine/trunk/src/main/java/com/armedia/cmf/engine/exporter/ExportEngine.java
+++ engine/trunk/src/main/java/com/armedia/cmf/engine/exporter/ExportEngine.java
@@ -41,7 +41,7 @@ import com.armedia.commons.utilities.CfgTools;
  *
  */
 public abstract class ExportEngine<S, W extends SessionWrapper<S>, T, V, C extends ExportContext<S, T, V>> extends
-	TransferEngine<S, T, V, ExportEngineListener> {
+TransferEngine<S, T, V, ExportEngineListener> {
 
 	private Logger log = LoggerFactory.getLogger(getClass());
 
@@ -224,8 +224,7 @@ public abstract class ExportEngine<S, W extends SessionWrapper<S>, T, V, C exten
 	}
 
 	public final void runExport(final Logger output, final ObjectStore<?, ?> objectStore,
-		final ContentStreamStore streamStore, String exportTarget, Map<String, Object> settings)
-		throws ExportException, StorageException {
+		final ContentStreamStore streamStore, Map<String, Object> settings) throws ExportException, StorageException {
 		// We get this at the very top because if this fails, there's no point in continuing.
 
 		final SessionFactory<S> sessionFactory = newSessionFactory();
@@ -477,10 +476,10 @@ public abstract class ExportEngine<S, W extends SessionWrapper<S>, T, V, C exten
 			if (pending > 0) {
 				try {
 					this.log
-						.info(String
-							.format(
-								"Waiting an additional 60 seconds for worker termination as a contingency (%d pending workers)",
-								pending));
+					.info(String
+						.format(
+							"Waiting an additional 60 seconds for worker termination as a contingency (%d pending workers)",
+							pending));
 					executor.awaitTermination(1, TimeUnit.MINUTES);
 				} catch (InterruptedException e) {
 					this.log.warn("Interrupted while waiting for immediate executor termination", e);
