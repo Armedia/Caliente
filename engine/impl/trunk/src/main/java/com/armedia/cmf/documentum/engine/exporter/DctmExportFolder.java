122e03405c64bd06d24044343a4c994211554cb9
Diego Rivera <diego.rivera@armedia.com>
2014-11-08 19:57:22 +0000

    Fixed label calculation for folders, for better performance.

--- engine/impl/trunk/src/main/java/com/armedia/cmf/documentum/engine/exporter/DctmExportFolder.java
+++ engine/impl/trunk/src/main/java/com/armedia/cmf/documentum/engine/exporter/DctmExportFolder.java
@@ -6,6 +6,8 @@ package com.armedia.cmf.documentum.engine.exporter;
 
 import com.armedia.cmf.documentum.engine.DctmObjectType;
 import com.documentum.fc.client.IDfFolder;
+import com.documentum.fc.client.IDfSession;
+import com.documentum.fc.common.DfException;
 
 /**
  * @author diego
@@ -15,6 +17,11 @@ public class DctmExportFolder extends DctmExportSysObject<IDfFolder> {
 
 	protected DctmExportFolder() {
 		super(DctmObjectType.FOLDER);
+	}
+
+	@Override
+	protected String calculateLabel(IDfSession session, IDfFolder folder) throws DfException {
+		return folder.getFolderPath(0);
 	}
 
 }
