9d25b76c0326976baa180fb3b232269863b75a65
Diego Rivera <diego.rivera@armedia.com>
2014-11-10 23:29:19 +0000

    Removed expansion - no longer needed as it's already implemented upstream.

--- store/impl/trunk/src/main/java/com/armedia/cmf/storage/local/LocalContentStoreFactory.java
+++ store/impl/trunk/src/main/java/com/armedia/cmf/storage/local/LocalContentStoreFactory.java
@@ -2,9 +2,6 @@ package com.armedia.cmf.storage.local;
 
 import java.io.File;
 
-import org.apache.commons.lang3.text.StrLookup;
-import org.apache.commons.lang3.text.StrSubstitutor;
-
 import com.armedia.cmf.storage.ContentStoreFactory;
 import com.armedia.cmf.storage.StorageException;
 import com.armedia.cmf.storage.xml.StoreConfiguration;
@@ -12,28 +9,8 @@ import com.armedia.commons.utilities.CfgTools;
 
 public class LocalContentStoreFactory extends ContentStoreFactory<LocalContentStore> {
 
-	private static final StrLookup<String> VARIABLE_RESOLVER = new StrLookup<String>() {
-
-		private final String envPrefix = "ENV.";
-		private final int envPrefixLength = this.envPrefix.length();
-
-		@Override
-		public String lookup(String key) {
-			if (key.startsWith(this.envPrefix)) {
-				return System.getenv(key.substring(this.envPrefixLength));
-			} else {
-				return System.getProperty(key);
-			}
-		}
-
-	};
-
-	private final StrSubstitutor substitutor;
-
 	public LocalContentStoreFactory() {
 		super("local", "filesystem");
-		this.substitutor = new StrSubstitutor(LocalContentStoreFactory.VARIABLE_RESOLVER);
-		this.substitutor.setEnableSubstitutionInVariables(true);
 	}
 
 	@Override
@@ -44,11 +21,9 @@ public class LocalContentStoreFactory extends ContentStoreFactory<LocalContentSt
 		if (basePath == null) { throw new StorageException(String.format("No setting [%s] specified",
 			Setting.BASE_DIR.getLabel())); }
 		// Resolve system properties
-		String actualPath = this.substitutor.replace(basePath);
 		if (this.log.isDebugEnabled()) {
-			this.log.debug(String.format("Creating a new local file store with base path [%s], expanded as [%s]",
-				basePath, actualPath));
+			this.log.debug(String.format("Creating a new local file store with base path [%s]", basePath));
 		}
-		return new LocalContentStore(new File(actualPath));
+		return new LocalContentStore(new File(basePath));
 	}
 }
