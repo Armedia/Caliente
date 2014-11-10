f0f86ec349d97bb766b20bde716de85531a902d8
Diego Rivera <diego.rivera@armedia.com>
2014-11-10 23:31:00 +0000

    Modified the test order to make it easier to track.  Modified the test stores for easier management.

--- engine/impl/trunk/src/test/java/com/armedia/mf/documentum/engine/exporter/DctmExportEngineTest.java
+++ engine/impl/trunk/src/test/java/com/armedia/mf/documentum/engine/exporter/DctmExportEngineTest.java
@@ -23,16 +23,18 @@ public class DctmExportEngineTest {
 		Logger output = LoggerFactory.getLogger("console");
 		output.info("Console initialized");
 		Map<String, String> settings = new HashMap<String, String>();
-		// settings.put("dql",
-		// "select r_object_id, r_object_type from dm_sysobject where folder('/CMSMFTests')");
-		settings.put("dql", "select r_object_id, 'dm_acl' as r_object_type from dm_acl");
 		settings.put("docbase", "documentum");
 		settings.put("username", "dmadmin2");
 		settings.put("password", "XZ6ZkrcrHEg=");
-		engine.runExport(output, objectStore, streamStore, settings);
+
 		settings.put("dql", "select r_object_id, 'dm_type' as r_object_type from dm_type");
 		engine.runExport(output, objectStore, streamStore, settings);
+
 		settings.put("dql", "select r_object_id, 'dm_format' as r_object_type from dm_format");
+		engine.runExport(output, objectStore, streamStore, settings);
+
+		// settings.put("dql", "select r_object_id, 'dm_acl' as r_object_type from dm_acl");
+		settings.put("dql", "select r_object_id, r_object_type from dm_sysobject where folder('/CMSMFTests')");
 		engine.runExport(output, objectStore, streamStore, settings);
 	}
 }
