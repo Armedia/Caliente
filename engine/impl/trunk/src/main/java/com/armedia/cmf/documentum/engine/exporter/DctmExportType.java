d13abdbc2d347650d14a16060dbb94ab2c075f88
Diego Rivera <diego.rivera@armedia.com>
2014-11-11 22:18:06 +0000

    Major simplification of the depth calculation - parent types will need to be loaded anyway as part of the persistence operation, so there's no extra expense incurred. If anything, we're helping cache items proactively.

--- engine/impl/trunk/src/main/java/com/armedia/cmf/documentum/engine/exporter/DctmExportType.java
+++ engine/impl/trunk/src/main/java/com/armedia/cmf/documentum/engine/exporter/DctmExportType.java
@@ -7,22 +7,17 @@ package com.armedia.cmf.documentum.engine.exporter;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.StrTokenizer;
 
 import com.armedia.cmf.documentum.engine.DctmAttributeHandlers;
 import com.armedia.cmf.documentum.engine.DctmAttributes;
 import com.armedia.cmf.documentum.engine.DctmDataType;
 import com.armedia.cmf.documentum.engine.DctmObjectType;
-import com.armedia.cmf.documentum.engine.DfUtils;
 import com.armedia.cmf.engine.exporter.ExportContext;
 import com.armedia.cmf.storage.StoredObject;
-import com.documentum.fc.client.IDfCollection;
 import com.documentum.fc.client.IDfPersistentObject;
-import com.documentum.fc.client.IDfQuery;
 import com.documentum.fc.client.IDfSession;
 import com.documentum.fc.client.IDfType;
 import com.documentum.fc.common.DfException;
@@ -99,38 +94,14 @@ public class DctmExportType extends DctmExportAbstract<IDfType> {
 	protected String calculateBatchId(IDfSession session, IDfType type) throws DfException {
 		// Calculate the maximum depth that this folder resides in, from its parents.
 		// Keep track of visited nodes, and explode on a loop.
-		Set<String> visited = new LinkedHashSet<String>();
-		int depth = calculateDepth(session, type.getName(), visited);
 		// We return it in zero-padded hex to allow for large numbers (up to 2^64
 		// depth), and also maintain consistent sorting
-		return String.format("%016x", depth);
+		return String.format("%016x", calculateDepth(session, type));
 	}
 
-	private int calculateDepth(IDfSession session, String typeName, Set<String> visited) throws DfException {
-		// If the folder has already been visited, we have a loop...so let's explode loudly
-		if (visited.contains(typeName)) { throw new DfException(String.format(
-			"Type loop detected, element [%s] exists twice: %s", typeName, visited.toString())); }
-		visited.add(typeName);
-		try {
-			int depth = 0;
-			String dql = String.format("select super_name from dm_type where name = '%s'", typeName);
-			IDfCollection results = DfUtils.executeQuery(session, dql, IDfQuery.DF_EXECREAD_QUERY);
-			try {
-				while (results.next()) {
-					// My depth is the maximum depth from any of my parents, plus one
-					String superName = results.getString(DctmAttributes.SUPER_NAME);
-					if (results.isNull(DctmAttributes.SUPER_NAME) || StringUtils.isBlank(superName)) {
-						continue;
-					}
-					depth = Math.max(depth, calculateDepth(session, superName, visited) + 1);
-				}
-			} finally {
-				DfUtils.closeQuietly(results);
-			}
-			return depth;
-		} finally {
-			visited.remove(typeName);
-		}
+	private int calculateDepth(IDfSession session, IDfType type) throws DfException {
+		if (type == null) { return -1; }
+		return calculateDepth(session, type.getSuperType()) + 1;
 	}
 
 	@Override
