3c0a777e0ed946113137e6358138bab5eca3c9ca
Diego Rivera <diego.rivera@armedia.com>
2014-11-17 14:47:44 +0000

    Updated to the latest API changes in URIStrategy. Some documentation fixes.

--- engine/impl/trunk/src/main/java/com/armedia/cmf/documentum/engine/DocumentumURIStrategy.java
+++ engine/impl/trunk/src/main/java/com/armedia/cmf/documentum/engine/DocumentumURIStrategy.java
@@ -13,28 +13,28 @@ public class DocumentumURIStrategy extends URIStrategy {
 	}
 
 	@Override
-	public String calculateSSP(StoredObjectType objectType, String objectId) {
-		String relative = String.format("%s/%s", objectType, objectId);
-		if (objectId.length() == 16) {
-			try {
-				Long.parseLong(objectId, 16);
-				// 16 character object id in dctm consists of first 2 chars of obj type, next 6
-				// chars of docbase id in hex and last 8 chars server generated. We will use first 6
-				// characters of this last 8 characters and generate the unique path.
-				// For ex: if the id is 0600a92b80054db8 than the path would be b8/4d/05
-				String uniqueId = objectId.substring(8);
-				String[] components = {
-					objectId.substring(0, 2), // The object type
-					objectId.substring(2, 8), // The docbase ID
-					uniqueId.substring(0, 3), // The first 3 characters of the unique object ID
-					uniqueId.substring(3, 6), // The 2nd 3 characters of the unique object ID
-					objectId
-				};
-				relative = FileNameTools.reconstitute(Arrays.asList(components), false, false, '/');
-			} catch (NumberFormatException e) {
-				// Not a documentum ID, so do something else
-			}
+	protected String calculateSSP(StoredObjectType objectType, String objectId) {
+		if (objectId.length() != 16) { return null; }
+
+		try {
+			Long.parseLong(objectId, 16);
+		} catch (NumberFormatException e) {
+			// Not a documentum ID, so do something else
+			return null;
 		}
-		return relative;
+
+		// 16 character object id in dctm consists of first 2 chars of obj type, next 6
+		// chars of docbase id in hex and last 8 chars server generated.
+		// For ex: if the id is 0600a92b80054db8 than the SSP would be
+		// 06/00a92b/800/54d/0600a92b80054db8
+		String uniqueId = objectId.substring(8);
+		String[] components = {
+			objectId.substring(0, 2), // The object type
+			objectId.substring(2, 8), // The docbase ID
+			uniqueId.substring(0, 3), // The first 3 characters of the unique object ID
+			uniqueId.substring(3, 6), // The 2nd 3 characters of the unique object ID
+			objectId
+		};
+		return FileNameTools.reconstitute(Arrays.asList(components), false, false, '/');
 	}
 }
