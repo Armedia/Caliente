e88460d791097e9adf9c3e325f6851f88150d076
Diego Rivera <diego.rivera@armedia.com>
2014-11-21 15:36:53 +0000

    Removed more useless constants

--- engine/impl/trunk/src/main/java/com/armedia/cmf/documentum/engine/DctmConstant.java
+++ engine/impl/trunk/src/main/java/com/armedia/cmf/documentum/engine/DctmConstant.java
@@ -1,6 +1,5 @@
 package com.armedia.cmf.documentum.engine;
 
-
 /**
  * The Class Constant. This class contains various constant values used throughout the CMSMF
  * application.
@@ -9,35 +8,8 @@ package com.armedia.cmf.documentum.engine;
  */
 public class DctmConstant {
 
-	/**
-	 * The buffer size in bytes used to retrieve the content file. This buffer size is used to read
-	 * the content from input stream these many bytes at time and writing to output stream.
-	 */
-	public static final int CONTENT_READ_BUFFER_SIZE = 2048;
-
-	/** The description set to temporary ACLs created during the import process. */
-	public static final String CMSMF_TEMP_ACL_DESCRIPTION = "CMSMF Temp ACL";
-
 	/** The string value that is stored in user type attribute for inline users. */
 	public static final String USER_SOURCE_INLINE_PASSWORD = "inline password";
-
-	/**
-	 * The default value for cmsmf.app.inlinepassworduser.passwordvalue property in
-	 * CMSMF_app.properties file. If this property is missing in the properties file, this default
-	 * value will be used. If this default value is used during inline user creation during import,
-	 * it will set their password same as their login id
-	 */
-	public static final String INLINE_PASSWORD_DEFAULT_VALUE = "sameasloginid";
-
-	/**
-	 * The select clause of export query used to export sysobjects. To locate system object using a
-	 * dql, we need several of the attributes present in select clause of the query. Hence this
-	 * select clause specified here.
-	 */
-	public static final String EXPORT_QUERY_SELECT_CLAUSE = "select distinct r_object_id";
-
-	/** The file name and path of the properties file used in CMSMF application. */
-	public static final String FULLY_QUALIFIED_CONFIG_FILE_NAME = "cmsmf.properties";
 
 	/** The "sql-neutral" date and time pattern that will be fed for SQL updates to date attributes */
 	public static final String SQL_DATETIME_PATTERN = "yyyy-mm-dd hh:mi:ss";
