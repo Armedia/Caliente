f7d46a268f03c5976d6ae742ae959f5f20c1c6d9
Diego Rivera <diego.rivera@armedia.com>
2014-11-24 22:11:12 +0000

    Release 2.3.3: ================================================================================
    * Fixed a HUGE bug with type attribute handlers - weren't being properly registered.
    * Fixed a bug with group creation - setting users' default group should be done after the group is completed, and the group shouldn't be saved afterwards.
    * Applied the modifications for updateReferenced() from CMF, to avoid problems with saving objects (ACL, Group, Folders) after re-linking them to the users that have them set as default. This was causing problems and there really needs to be no modification to the object being saved afterwards.

---
+++ tools/tags/2.3.3/src/main/java/com/delta/cmsmf/cms/CmsCounter.java
@@ -0,0 +1,214 @@
+package com.delta.cmsmf.cms;
+
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import org.apache.commons.lang3.StringUtils;
+
+public final class CmsCounter<R extends Enum<R>> {
+
+	private static final String TOTAL_LABEL = "processed".intern();
+	private static final String NEW_LINE = String.format("%n");
+
+	private final ReadWriteLock lock = new ReentrantReadWriteLock();
+	private final Map<CmsObjectType, Map<R, AtomicInteger>> counters;
+	private final Map<R, AtomicInteger> cummulative;
+	private final Class<R> rClass;
+	private final String formatString;
+
+	public CmsCounter(Class<R> rClass) {
+		if (rClass == null) { throw new IllegalArgumentException("Must provide an enum class"); }
+		this.rClass = rClass;
+
+		int maxWidth = 0;
+		Map<R, AtomicInteger> cummulative = new EnumMap<R, AtomicInteger>(rClass);
+		for (R result : this.rClass.getEnumConstants()) {
+			cummulative.put(result, new AtomicInteger(0));
+			maxWidth = Math.max(maxWidth, result.name().length());
+		}
+		this.cummulative = Collections.unmodifiableMap(cummulative);
+
+		Map<CmsObjectType, Map<R, AtomicInteger>> counters = new EnumMap<CmsObjectType, Map<R, AtomicInteger>>(
+			CmsObjectType.class);
+		for (CmsObjectType objectType : CmsObjectType.values()) {
+			Map<R, AtomicInteger> results = new EnumMap<R, AtomicInteger>(rClass);
+			for (R result : this.rClass.getEnumConstants()) {
+				results.put(result, new AtomicInteger(0));
+			}
+			counters.put(objectType, Collections.unmodifiableMap(results));
+		}
+		this.counters = Collections.unmodifiableMap(counters);
+
+		maxWidth = Math.max(maxWidth, CmsCounter.TOTAL_LABEL.length());
+		this.formatString = String.format("%%s objects %%-%ds: %%6d%%n", maxWidth);
+	}
+
+	public int increment(CmsObject<?> object, R result) {
+		if (object == null) { throw new IllegalArgumentException("Must provide an object whose type to count for"); }
+		return increment(object.getType(), result);
+	}
+
+	public int increment(CmsObjectType objectType, R result) {
+		if (objectType == null) { throw new IllegalArgumentException("Unsupported null object type"); }
+		if (result == null) { throw new IllegalArgumentException("Must provide a valid result to count for"); }
+		this.lock.readLock().lock();
+		try {
+			AtomicInteger counter = this.counters.get(objectType).get(result);
+			final int ret = counter.incrementAndGet();
+			this.cummulative.get(result).incrementAndGet();
+			return ret;
+		} finally {
+			this.lock.readLock().unlock();
+		}
+	}
+
+	public Map<R, Integer> getCummulative() {
+		return getCounters((CmsObjectType) null);
+	}
+
+	public Map<R, Integer> getCounters(CmsObject<?> object) {
+		if (object == null) { throw new IllegalArgumentException("Must provide an object to return the counters for"); }
+		return getCounters(object.getType());
+	}
+
+	public Map<R, Integer> getCounters(CmsObjectType type) {
+		Map<R, Integer> ret = new EnumMap<R, Integer>(this.rClass);
+		Map<R, AtomicInteger> m = (type != null ? this.counters.get(type) : this.cummulative);
+		this.lock.writeLock().lock();
+		try {
+			for (Map.Entry<R, AtomicInteger> e : m.entrySet()) {
+				ret.put(e.getKey(), e.getValue().get());
+			}
+			return Collections.unmodifiableMap(ret);
+		} finally {
+			this.lock.writeLock().unlock();
+		}
+	}
+
+	public void reset() {
+		this.lock.writeLock().lock();
+		try {
+			for (CmsObjectType t : CmsObjectType.values()) {
+				Map<R, AtomicInteger> m = this.counters.get(t);
+				for (R r : this.rClass.getEnumConstants()) {
+					m.get(r).set(0);
+				}
+			}
+			for (R r : this.rClass.getEnumConstants()) {
+				this.cummulative.get(r).set(0);
+			}
+		} finally {
+			this.lock.writeLock().unlock();
+		}
+	}
+
+	public Map<R, Integer> reset(CmsObject<?> object) {
+		if (object == null) { throw new IllegalArgumentException(
+			"Must provide an object whose type to reset the counters for"); }
+		return reset(object.getType());
+	}
+
+	public Map<R, Integer> reset(CmsObjectType type) {
+		Map<R, Integer> ret = new EnumMap<R, Integer>(this.rClass);
+		Map<R, AtomicInteger> m = (type != null ? this.counters.get(ret) : this.cummulative);
+		this.lock.writeLock().lock();
+		try {
+			for (Map.Entry<R, AtomicInteger> e : m.entrySet()) {
+				final R r = e.getKey();
+				final int val = e.getValue().getAndSet(0);
+				ret.put(r, val);
+				this.cummulative.get(r).addAndGet(-val);
+			}
+			return Collections.unmodifiableMap(ret);
+		} finally {
+			this.lock.writeLock().unlock();
+		}
+	}
+
+	public String generateCummulativeReport() {
+		return generateCummulativeReport(0);
+	}
+
+	public String generateCummulativeReport(int indentLevel) {
+		return generateReport(this.cummulative, indentLevel, "Total");
+	}
+
+	public String generateReport(CmsObject<?> object) {
+		return generateReport(object, 0);
+	}
+
+	public String generateReport(CmsObject<?> object, int indentLevel) {
+		if (object == null) { throw new IllegalArgumentException(
+			"Must provide an object to determine which report to render"); }
+		return generateReport(object.getType(), indentLevel);
+	}
+
+	public String generateReport(CmsObjectType objectType) {
+		return generateReport(objectType, 0);
+	}
+
+	public String generateReport(CmsObjectType objectType, int indentLevel) {
+		if (objectType == null) { throw new IllegalArgumentException("Unsupported null object type"); }
+		Map<R, AtomicInteger> results = this.counters.get(objectType);
+		return generateReport(results, indentLevel, String.format("Number of %s", objectType));
+	}
+
+	private String generateReport(Map<R, AtomicInteger> results, int indentLevel, String entryLabel) {
+		this.lock.writeLock().lock();
+		try {
+			return CmsCounter.generateSummary(this.rClass, results, indentLevel, entryLabel, CmsCounter.TOTAL_LABEL,
+				this.formatString);
+		} finally {
+			this.lock.writeLock().unlock();
+		}
+	}
+
+	private static <E extends Enum<E>> String calculateFormatString(Class<E> klass, String totalLabel) {
+		int maxWidth = 0;
+		for (E e : klass.getEnumConstants()) {
+			maxWidth = Math.max(maxWidth, e.name().length());
+		}
+		maxWidth = Math.max(maxWidth, totalLabel.length());
+		return String.format("%%s objects %%-%ds: %%6d%%n", maxWidth);
+	}
+
+	public static <E extends Enum<E>> String generateSummary(Class<E> klass, Map<E, ? extends Number> results,
+		int indentLevel, String entryLabel, String totalLabel) {
+		return CmsCounter.generateSummary(klass, results, indentLevel, entryLabel, totalLabel, null);
+	}
+
+	public static <E extends Enum<E>> String generateSummary(Class<E> klass, Map<E, ? extends Number> results,
+		int indentLevel, String entryLabel, String totalLabel, String formatString) {
+
+		if (formatString == null) {
+			formatString = CmsCounter.calculateFormatString(klass, totalLabel);
+		}
+
+		StringBuilder s = new StringBuilder();
+		if (indentLevel < 0) {
+			indentLevel = 0;
+		}
+		for (int i = 0; i < indentLevel; i++) {
+			s.append('\t');
+		}
+		final String indent = s.toString();
+		s.setLength(0);
+		int total = 0;
+		for (Map.Entry<E, ? extends Number> e : results.entrySet()) {
+			final E r = e.getKey();
+			final Number i = e.getValue();
+			int intValue = i.intValue();
+			total += intValue;
+			s.append(indent).append(String.format(formatString, entryLabel, r, intValue));
+		}
+		String totalLine = String.format(formatString, entryLabel, totalLabel, total);
+		// PATCH: need to repeat one less than the length of the line, or we'll overflow by 1...
+		s.append(indent).append(StringUtils.repeat("=", totalLine.length() - 1)).append(CmsCounter.NEW_LINE);
+		s.append(indent).append(totalLine).append(CmsCounter.NEW_LINE);
+		return s.toString();
+	}
+}
---
+++ tools/tags/2.3.3/src/main/java/com/delta/cmsmf/utils/DfUtils.java
@@ -0,0 +1,240 @@
+package com.delta.cmsmf.utils;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.log4j.Logger;
+
+import com.delta.cmsmf.cfg.Constant;
+import com.documentum.com.DfClientX;
+import com.documentum.fc.client.IDfACL;
+import com.documentum.fc.client.IDfCollection;
+import com.documentum.fc.client.IDfPermit;
+import com.documentum.fc.client.IDfQuery;
+import com.documentum.fc.client.IDfSession;
+import com.documentum.fc.common.DfException;
+import com.documentum.fc.common.IDfTime;
+
+public class DfUtils {
+
+	public static enum DbType {
+		//
+		ORACLE("^.*\\.Oracle$"),
+		MSSQL("^.*\\.SQLServer$");
+
+		private final Pattern pattern;
+
+		private DbType(String pattern) {
+			this.pattern = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+		}
+
+		public boolean matches(String serverVersion) {
+			if (serverVersion == null) { throw new IllegalArgumentException(
+				"Must provide a server version string to examine"); }
+			return this.pattern.matcher(serverVersion).matches();
+		}
+
+		public boolean matches(IDfSession session) throws DfException {
+			if (session == null) { throw new IllegalArgumentException("Must provide a session to examine"); }
+			return matches(session.getServerVersion());
+		}
+	}
+
+	private static final Logger LOG = Logger.getLogger(DfUtils.class);
+
+	private static enum Permission {
+		//
+		DF_PERMIT_NONE(IDfACL.DF_PERMIT_NONE, IDfACL.DF_PERMIT_NONE_STR),
+		DF_PERMIT_BROWSE(IDfACL.DF_PERMIT_BROWSE, IDfACL.DF_PERMIT_BROWSE_STR),
+		DF_PERMIT_READ(IDfACL.DF_PERMIT_READ, IDfACL.DF_PERMIT_READ_STR),
+		DF_PERMIT_RELATE(IDfACL.DF_PERMIT_RELATE, IDfACL.DF_PERMIT_RELATE_STR),
+		DF_PERMIT_VERSION(IDfACL.DF_PERMIT_VERSION, IDfACL.DF_PERMIT_VERSION_STR),
+		DF_PERMIT_WRITE(IDfACL.DF_PERMIT_WRITE, IDfACL.DF_PERMIT_WRITE_STR),
+		DF_PERMIT_DELETE(IDfACL.DF_PERMIT_DELETE, IDfACL.DF_PERMIT_DELETE_STR);
+		private final int num;
+		private final String str;
+
+		private Permission(int num, String str) {
+			this.str = str;
+			this.num = num;
+		}
+	}
+
+	private static enum PermitType {
+		//
+		DF_ACCESS_PERMIT(IDfPermit.DF_ACCESS_PERMIT, IDfPermit.DF_ACCESS_PERMIT_STR),
+		DF_ACCESS_RESTRICTION(IDfPermit.DF_ACCESS_RESTRICTION, IDfPermit.DF_ACCESS_RESTRICTION_STR),
+		DF_APPLICATION_PERMIT(IDfPermit.DF_APPLICATION_PERMIT, IDfPermit.DF_APPLICATION_PERMIT_STR),
+		DF_APPLICATION_RESTRICTION(IDfPermit.DF_APPLICATION_RESTRICTION, IDfPermit.DF_APPLICATION_RESTRICTION_STR),
+		DF_EXTENDED_PERMIT(IDfPermit.DF_EXTENDED_PERMIT, IDfPermit.DF_EXTENDED_PERMIT_STR),
+		DF_EXTENDED_RESTRICTION(IDfPermit.DF_EXTENDED_RESTRICTION, IDfPermit.DF_EXTENDED_RESTRICTION_STR),
+		DF_REQUIRED_GROUP(IDfPermit.DF_REQUIRED_GROUP, IDfPermit.DF_REQUIRED_GROUP_STR),
+		DF_REQUIRED_GROUP_SET(IDfPermit.DF_REQUIRED_GROUP_SET, IDfPermit.DF_REQUIRED_GROUP_SET_STR);
+		private final int num;
+		private final String str;
+
+		private PermitType(int num, String str) {
+			this.str = str;
+			this.num = num;
+		}
+	}
+
+	private static final Map<String, Integer> PERMISSIONS_MAP;
+	private static final Map<String, Integer> PERMIT_TYPES_MAP;
+	static {
+		Map<String, Integer> m = new HashMap<String, Integer>();
+		for (Permission p : Permission.values()) {
+			m.put(p.str, p.num);
+		}
+		PERMISSIONS_MAP = Collections.unmodifiableMap(m);
+		m = new HashMap<String, Integer>();
+		for (PermitType p : PermitType.values()) {
+			m.put(p.str, p.num);
+		}
+		PERMIT_TYPES_MAP = Collections.unmodifiableMap(m);
+	}
+
+	public static void closeQuietly(IDfCollection c) {
+		if (c == null) { return; }
+		try {
+			c.close();
+		} catch (DfException e) {
+			// quietly swallowed
+			if (DfUtils.LOG.isTraceEnabled()) {
+				DfUtils.LOG.trace("Swallowing exception on close", e);
+			}
+		}
+	}
+
+	public static IDfQuery newQuery() {
+		return new DfClientX().getQuery();
+	}
+
+	public static IDfCollection executeQuery(IDfSession session, String dql) throws DfException {
+		return DfUtils.executeQuery(session, dql, IDfQuery.DF_QUERY);
+	}
+
+	public static IDfCollection executeQuery(IDfSession session, String dql, int queryType) throws DfException {
+		if (session == null) { throw new IllegalArgumentException("Must provide a session to execute the DQL on"); }
+		if (dql == null) { throw new IllegalArgumentException("Must provide a DQL statement to execute"); }
+		IDfQuery query = DfUtils.newQuery();
+		if (DfUtils.LOG.isTraceEnabled()) {
+			DfUtils.LOG.trace(String.format("Executing DQL (type=%d):%n%s", queryType, dql));
+		}
+		query.setDQL(dql);
+		boolean ok = false;
+		try {
+			IDfCollection ret = query.execute(session, queryType);
+			ok = true;
+			return ret;
+		} finally {
+			if (!ok) {
+				DfUtils.LOG.fatal(String.format("Exception raised while executing the query:%n%s", dql));
+			}
+		}
+	}
+
+	public static String getSessionId(IDfSession session) {
+		if (session == null) { throw new IllegalArgumentException("Must provide a session to get the ID from"); }
+		try {
+			return session.getSessionId();
+		} catch (DfException e) {
+			return "(unknown)";
+		}
+	}
+
+	public static DbType getDbType(IDfSession session) throws DfException {
+		if (session == null) { throw new IllegalArgumentException(
+			"Must provide a session to identify the database from"); }
+		final String serverVersion = session.getServerVersion();
+		for (DbType type : DbType.values()) {
+			if (type.matches(serverVersion)) { return type; }
+		}
+		throw new UnsupportedOperationException(String.format(
+			"Failed to identify a supported database from the server version string [%s]", serverVersion));
+	}
+
+	public static String generateSqlDateClause(IDfTime date, IDfSession session) throws DfException {
+		// First, output to the "netural" format
+		final String dateString = DateFormatUtils.formatUTC(date.getDate(), Constant.JAVA_SQL_DATETIME_PATTERN);
+		// Now, select the database format string
+		final String ret;
+		DbType dbType = DfUtils.getDbType(session);
+		switch (dbType) {
+			case ORACLE:
+				ret = String.format("TO_DATE(''%s'', ''%s'')", dateString, Constant.ORACLE_DATETIME_PATTERN);
+				break;
+			case MSSQL:
+				ret = String.format("CONVERT(DATETIME, ''%s'', %d)", dateString, Constant.MSSQL_DATETIME_PATTERN);
+				break;
+			default:
+				throw new UnsupportedOperationException(String.format("Unsupported database type [%s]", dbType));
+		}
+		if (DfUtils.LOG.isTraceEnabled()) {
+			DfUtils.LOG.trace(String.format("Generated %s SQL Date string [%s] from [%s](%d)", dbType, ret,
+				date.asString(Constant.SQL_DATETIME_PATTERN), date.getDate().getTime()));
+		}
+		return ret;
+	}
+
+	public static String decodeAccessPermission(int permission) throws DfException {
+		// We do it the "hardcoded" way here because it's MUCH faster than maps...
+		switch (permission) {
+			case IDfACL.DF_PERMIT_NONE:
+				return IDfACL.DF_PERMIT_NONE_STR;
+			case IDfACL.DF_PERMIT_BROWSE:
+				return IDfACL.DF_PERMIT_BROWSE_STR;
+			case IDfACL.DF_PERMIT_READ:
+				return IDfACL.DF_PERMIT_READ_STR;
+			case IDfACL.DF_PERMIT_RELATE:
+				return IDfACL.DF_PERMIT_RELATE_STR;
+			case IDfACL.DF_PERMIT_VERSION:
+				return IDfACL.DF_PERMIT_VERSION_STR;
+			case IDfACL.DF_PERMIT_WRITE:
+				return IDfACL.DF_PERMIT_WRITE_STR;
+			case IDfACL.DF_PERMIT_DELETE:
+				return IDfACL.DF_PERMIT_DELETE_STR;
+			default:
+				throw new DfException(String.format("Unknown permissions value [%d] detected", permission));
+		}
+	}
+
+	public static int decodeAccessPermission(String permission) throws DfException {
+		if (permission == null) { throw new IllegalArgumentException("Must provide a permission to map"); }
+		Integer ret = DfUtils.PERMISSIONS_MAP.get(permission);
+		if (ret == null) { throw new DfException(String.format("Unknown permissions value [%s] detected", permission)); }
+		return ret;
+	}
+
+	public static String decodePermitType(int permitType) throws DfException {
+		switch (permitType) {
+			case IDfPermit.DF_ACCESS_PERMIT:
+				return IDfPermit.DF_ACCESS_PERMIT_STR;
+			case IDfPermit.DF_ACCESS_RESTRICTION:
+				return IDfPermit.DF_ACCESS_RESTRICTION_STR;
+			case IDfPermit.DF_APPLICATION_PERMIT:
+				return IDfPermit.DF_APPLICATION_PERMIT_STR;
+			case IDfPermit.DF_APPLICATION_RESTRICTION:
+				return IDfPermit.DF_APPLICATION_RESTRICTION_STR;
+			case IDfPermit.DF_EXTENDED_PERMIT:
+				return IDfPermit.DF_EXTENDED_PERMIT_STR;
+			case IDfPermit.DF_EXTENDED_RESTRICTION:
+				return IDfPermit.DF_EXTENDED_RESTRICTION_STR;
+			case IDfPermit.DF_REQUIRED_GROUP:
+				return IDfPermit.DF_REQUIRED_GROUP_STR;
+			case IDfPermit.DF_REQUIRED_GROUP_SET:
+				return IDfPermit.DF_REQUIRED_GROUP_SET_STR;
+		}
+		throw new DfException(String.format("Unknown permit type [%d] detected", permitType));
+	}
+
+	public static int decodePermitType(String permitType) throws DfException {
+		if (permitType == null) { throw new IllegalArgumentException("Must provide a permit type to map"); }
+		Integer ret = DfUtils.PERMIT_TYPES_MAP.get(permitType);
+		if (ret == null) { throw new DfException(String.format("Unknown permit type value [%s] detected", permitType)); }
+		return ret;
+	}
+}
