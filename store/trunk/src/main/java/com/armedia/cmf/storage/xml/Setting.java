3c1fc67db08d736c5591fef4c88f710f9a48f166
Diego Rivera <diego.rivera@armedia.com>
2014-11-24 19:54:42 +0000

    Fixed a javadoc issue.

--- store/trunk/src/main/java/com/armedia/cmf/storage/xml/Setting.java
+++ store/trunk/src/main/java/com/armedia/cmf/storage/xml/Setting.java
@@ -1,4 +1,3 @@
-
 package com.armedia.cmf.storage.xml;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -7,81 +6,76 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.XmlValue;
 
-
 /**
- * <p>Java class for setting.t complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
+ * <p>
+ * Java class for setting.t complex type.
+ *
+ * <p>
+ * The following schema fragment specifies the expected content contained within this class.
+ *
  * <pre>
- * &lt;complexType name="setting.t">
- *   &lt;simpleContent>
- *     &lt;extension base="&lt;http://www.armedia.com/ns/audit>trimmed_string.t">
- *       &lt;attribute name="name" use="required" type="{http://www.armedia.com/ns/audit}settingName.t" />
- *     &lt;/extension>
- *   &lt;/simpleContent>
- * &lt;/complexType>
+ * &lt;complexType name="setting.t"&gt;
+ *   &lt;simpleContent&gt;
+ *     &lt;extension base="&lt;http://www.armedia.com/ns/audit&gt;trimmed_string.t"&gt;
+ *       &lt;attribute name="name" use="required" type="{http://www.armedia.com/ns/audit}settingName.t" /&gt;
+ *     &lt;/extension&gt;
+ *   &lt;/simpleContent&gt;
+ * &lt;/complexType&gt;
  * </pre>
- * 
- * 
+ *
+ *
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "setting.t", propOrder = {
-    "value"
+	"value"
 })
 public class Setting {
 
-    @XmlValue
-    protected String value;
-    @XmlAttribute(name = "name", required = true)
-    protected String name;
+	@XmlValue
+	protected String value;
+	@XmlAttribute(name = "name", required = true)
+	protected String name;
 
-    /**
-     * Gets the value of the value property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    public String getValue() {
-        return value;
-    }
+	/**
+	 * Gets the value of the value property.
+	 *
+	 * @return possible object is {@link String }
+	 *
+	 */
+	public String getValue() {
+		return this.value;
+	}
 
-    /**
-     * Sets the value of the value property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    public void setValue(String value) {
-        this.value = value;
-    }
+	/**
+	 * Sets the value of the value property.
+	 *
+	 * @param value
+	 *            allowed object is {@link String }
+	 *
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
 
-    /**
-     * Gets the value of the name property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    public String getName() {
-        return name;
-    }
+	/**
+	 * Gets the value of the name property.
+	 *
+	 * @return possible object is {@link String }
+	 *
+	 */
+	public String getName() {
+		return this.name;
+	}
 
-    /**
-     * Sets the value of the name property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    public void setName(String value) {
-        this.name = value;
-    }
+	/**
+	 * Sets the value of the name property.
+	 *
+	 * @param value
+	 *            allowed object is {@link String }
+	 *
+	 */
+	public void setName(String value) {
+		this.name = value;
+	}
 
 }
