1b252801ae9a8ab3b052e7e47ba2aa49674e4ac7
Diego Rivera <diego.rivera@armedia.com>
2014-11-24 19:55:16 +0000

    Release 1.0.0

---
+++ store/tags/1.0.0/src/main/java/com/armedia/cmf/storage/StoredDataType.java
@@ -0,0 +1,12 @@
+package com.armedia.cmf.storage;
+
+public enum StoredDataType {
+	//
+	BOOLEAN,
+	INTEGER,
+	DOUBLE,
+	STRING,
+	ID,
+	TIME,
+	OTHER
+}
---
+++ store/tags/1.0.0/src/main/java/com/armedia/cmf/storage/StoredObjectType.java
@@ -0,0 +1,15 @@
+package com.armedia.cmf.storage;
+
+public enum StoredObjectType {
+	//
+	DATASTORE,
+	USER,
+	GROUP,
+	ACL,
+	TYPE,
+	FORMAT,
+	FOLDER,
+	DOCUMENT,
+	WORKFLOW,
+	CONTENT_STREAM;
+}
