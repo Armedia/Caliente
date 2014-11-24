fc7c0dc41b01dda71bfb91c629a4e6ae6072d0a9
Diego Rivera <diego.rivera@armedia.com>
2014-11-24 14:09:37 +0000

    Removed unnecessary code.

--- tools/branches/3.0.0/src/main/java/com/delta/cmsmf/utils/CMSMFUtils.java
+++ tools/branches/3.0.0/src/main/java/com/delta/cmsmf/utils/CMSMFUtils.java
@@ -1,7 +1,6 @@
 package com.delta.cmsmf.utils;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -42,25 +41,6 @@ public class CMSMFUtils {
 
 	/** The log object used for logging. */
 	static Logger log = LoggerFactory.getLogger(CMSMFUtils.class);
-
-	/**
-	 * Gets the content path from content id.
-	 *
-	 * @param contentId
-	 *            the content obj id
-	 * @return the content path from content id
-	 */
-	public static File getContentDirectory(String contentId) {
-		if (contentId.length() != 16) { return null; }
-		// 16 character object id in dctm consists of first 2 chars of obj type, next 6 chars of
-		// docbase id in hex and last 8 chars server generated. We will use first 6 characters
-		// of this last 8 characters and generate the unique path.
-		// For ex: if the id is 0600a92b80054db8 than the path would be 80/05/4d
-		String pathComponents = contentId.substring(8, 16);
-		File tier1 = new File(pathComponents.substring(0, 2));
-		File tier2 = new File(tier1, pathComponents.substring(2, 4));
-		return new File(tier2, pathComponents.substring(4, 6));
-	}
 
 	private static IDfSysObject getCmsmfStateObject(IDfSession dctmSession, boolean createIfMissing) throws DfException {
 		final String targetDocbaseName = CLIParam.docbase.getString();
