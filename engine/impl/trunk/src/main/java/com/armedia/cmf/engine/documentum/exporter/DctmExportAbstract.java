13d38c2ba87bf90f1960986afcd9c19453a9797d
Diego Rivera <diego.rivera@armedia.com>
2014-11-25 21:16:15 +0000

    Fixed package naming.

---
+++ engine/impl/trunk/src/main/java/com/armedia/cmf/engine/documentum/exporter/DctmExportAbstract.java
@@ -0,0 +1,117 @@
+package com.armedia.cmf.engine.documentum.exporter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.armedia.cmf.engine.documentum.DctmAttributeHandlers;
+import com.armedia.cmf.engine.documentum.DctmDataType;
+import com.armedia.cmf.engine.documentum.DctmDelegateBase;
+import com.armedia.cmf.engine.documentum.DctmObjectType;
+import com.armedia.cmf.engine.documentum.UnsupportedDctmObjectTypeException;
+import com.armedia.cmf.engine.documentum.DctmAttributeHandlers.AttributeHandler;
+import com.armedia.cmf.engine.exporter.ExportContext;
+import com.armedia.cmf.engine.exporter.ExportException;
+import com.armedia.cmf.engine.exporter.ExportTarget;
+import com.armedia.cmf.storage.ContentStore;
+import com.armedia.cmf.storage.ContentStore.Handle;
+import com.armedia.cmf.storage.StoredAttribute;
+import com.armedia.cmf.storage.StoredObject;
+import com.armedia.cmf.storage.StoredProperty;
+import com.documentum.fc.client.IDfPersistentObject;
+import com.documentum.fc.client.IDfSession;
+import com.documentum.fc.common.DfException;
+import com.documentum.fc.common.IDfAttr;
+import com.documentum.fc.common.IDfValue;
+
+public class DctmExportAbstract<T extends IDfPersistentObject> extends DctmDelegateBase<T, DctmExportEngine> {
+
+	protected DctmExportAbstract(DctmExportEngine engine, DctmObjectType type) {
+		super(engine, type);
+	}
+
+	public final Collection<IDfPersistentObject> identifyRequirements(IDfSession session,
+		StoredObject<IDfValue> marshaled, IDfPersistentObject object, DctmExportContext ctx) throws Exception {
+		return findRequirements(session, marshaled, castObject(object), ctx);
+	}
+
+	protected Collection<IDfPersistentObject> findRequirements(IDfSession session, StoredObject<IDfValue> marshaled,
+		T object, DctmExportContext ctx) throws Exception {
+		return new ArrayList<IDfPersistentObject>();
+	}
+
+	public final Collection<IDfPersistentObject> identifyDependents(IDfSession session,
+		StoredObject<IDfValue> marshaled, IDfPersistentObject object,
+		ExportContext<IDfSession, IDfPersistentObject, IDfValue> ctx) throws Exception {
+		return findDependents(session, marshaled, castObject(object), ctx);
+	}
+
+	protected Collection<IDfPersistentObject> findDependents(IDfSession session, StoredObject<IDfValue> marshaled,
+		T object, ExportContext<IDfSession, IDfPersistentObject, IDfValue> ctx) throws Exception {
+		return new ArrayList<IDfPersistentObject>();
+	}
+
+	protected final StoredObject<IDfValue> marshal(IDfSession session, IDfPersistentObject object) throws DfException,
+		ExportException, UnsupportedDctmObjectTypeException {
+		final String id = object.getObjectId().getId();
+		final String subtype = object.getType().getName();
+		final T typedObject = castObject(object);
+
+		final String batchId = calculateBatchId(session, typedObject);
+		final String label = calculateLabel(session, typedObject);
+		final StoredObject<IDfValue> storedObject = new StoredObject<IDfValue>(getDctmType().getStoredObjectType(), id,
+			batchId, label, subtype);
+
+		// First, the attributes
+		final int attCount = object.getAttrCount();
+		for (int i = 0; i < attCount; i++) {
+			final IDfAttr attr = object.getAttr(i);
+			final AttributeHandler handler = DctmAttributeHandlers.getAttributeHandler(getDctmType(), attr);
+			// Get the attribute handler
+			if (handler.includeInExport(object, attr)) {
+				StoredAttribute<IDfValue> attribute = new StoredAttribute<IDfValue>(attr.getName(), DctmDataType
+					.fromAttribute(attr).getStoredType(), attr.getId(), attr.getLength(), attr.isRepeating(),
+					attr.isQualifiable(), handler.getExportableValues(object, attr));
+				storedObject.setAttribute(attribute);
+			}
+		}
+
+		// Properties are different from attributes in that they require special handling. For
+		// instance, a property would only be settable via direct SQL, or via an explicit method
+		// call, etc., because setting it directly as an attribute would cmsImportResult in an error
+		// from DFC, and therefore specialized code is required to handle it
+		List<StoredProperty<IDfValue>> properties = new ArrayList<StoredProperty<IDfValue>>();
+		getDataProperties(properties, typedObject);
+		for (StoredProperty<IDfValue> property : properties) {
+			// This mechanism overwrites properties, and intentionally so
+			storedObject.setProperty(property);
+		}
+		return storedObject;
+	}
+
+	protected void getDataProperties(Collection<StoredProperty<IDfValue>> properties, T object) throws DfException,
+		ExportException {
+	}
+
+	protected String calculateBatchId(IDfSession session, T object) throws DfException {
+		return null;
+	}
+
+	protected String calculateLabel(IDfSession session, T object) throws DfException {
+		return String.format("%s[%s]", getDctmType().name(), object.getObjectId().getId());
+	}
+
+	protected final String calculateLabel(IDfPersistentObject object) throws DfException {
+		return calculateLabel(object.getSession(), castObject(object));
+	}
+
+	public final Handle storeContent(IDfSession session, ExportTarget referrent, IDfPersistentObject object,
+		ContentStore streamStore) throws Exception {
+		return doStoreContent(session, referrent, castObject(object), streamStore);
+	}
+
+	protected Handle doStoreContent(IDfSession session, ExportTarget referrent, T object, ContentStore streamStore)
+		throws Exception {
+		return null;
+	}
+}
---
+++ engine/impl/trunk/src/main/java/com/armedia/cmf/engine/documentum/importer/DctmImportDelegateFactory.java
@@ -0,0 +1,39 @@
+package com.armedia.cmf.engine.documentum.importer;
+
+import com.armedia.cmf.engine.documentum.DctmObjectType;
+import com.armedia.cmf.engine.documentum.DctmTranslator;
+import com.armedia.cmf.engine.documentum.UnsupportedDctmObjectTypeException;
+import com.armedia.cmf.storage.StoredObject;
+import com.armedia.cmf.storage.UnsupportedObjectTypeException;
+import com.documentum.fc.common.IDfValue;
+
+public class DctmImportDelegateFactory {
+
+	public static DctmImportDelegate<?> newDelegate(DctmImportEngine engine, StoredObject<IDfValue> marshaled)
+		throws UnsupportedDctmObjectTypeException, UnsupportedObjectTypeException {
+		DctmObjectType type = DctmTranslator.translateType(marshaled.getType());
+		if (type == null) { throw new UnsupportedObjectTypeException(marshaled.getType()); }
+		switch (type) {
+			case ACL:
+				return new DctmImportACL(engine, marshaled);
+			case DOCUMENT:
+				return new DctmImportDocument(engine, marshaled);
+			case STORE:
+				return new DctmImportStore(engine, marshaled);
+			case FOLDER:
+				return new DctmImportFolder(engine, marshaled);
+			case FORMAT:
+				return new DctmImportFormat(engine, marshaled);
+			case GROUP:
+				return new DctmImportGroup(engine, marshaled);
+			case TYPE:
+				return new DctmImportType(engine, marshaled);
+			case USER:
+				return new DctmImportUser(engine, marshaled);
+			case CONTENT:
+			default:
+				break;
+		}
+		throw new UnsupportedDctmObjectTypeException(String.format("Type [%s] is not supported", type.name()));
+	}
+}
