30ac5655e6e5fa249a5c8d8e7a6f3b5dc9ef2e06
Diego Rivera <diego.rivera@armedia.com>
2014-11-25 19:31:22 +0000

    Release 1.0.0

---
+++ engine/impl/tags/1.0.0/src/main/java/com/armedia/cmf/documentum/engine/common/DctmSysObject.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.documentum.engine.common;
+
+/**
+ * @author diego
+ *
+ */
+public interface DctmSysObject {
+
+	static final String TARGET_PATHS = "targetPaths";
+	static final String TARGET_PARENTS = "targetParents";
+
+}
---
+++ engine/impl/tags/1.0.0/src/main/java/com/armedia/cmf/documentum/engine/exporter/DctmObjectIdIterator.java
@@ -0,0 +1,55 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.documentum.engine.exporter;
+
+import java.util.Iterator;
+
+import com.armedia.cmf.documentum.engine.DctmAttributes;
+import com.armedia.cmf.documentum.engine.DctmCollectionIterator;
+import com.armedia.commons.utilities.Tools;
+import com.documentum.fc.client.IDfCollection;
+import com.documentum.fc.client.IDfTypedObject;
+import com.documentum.fc.common.DfException;
+
+/**
+ * @author diego
+ *
+ */
+public class DctmObjectIdIterator implements Iterator<String> {
+
+	private final String idAttribute;
+	private final DctmCollectionIterator iterator;
+	private int current = 0;
+
+	public DctmObjectIdIterator(IDfCollection collection) {
+		this(collection, null);
+	}
+
+	public DctmObjectIdIterator(IDfCollection collection, String idAttribute) {
+		this.iterator = new DctmCollectionIterator(collection);
+		this.idAttribute = Tools.coalesce(idAttribute, DctmAttributes.R_OBJECT_ID);
+	}
+
+	@Override
+	public boolean hasNext() {
+		return this.iterator.hasNext();
+	}
+
+	@Override
+	public String next() {
+		IDfTypedObject next = this.iterator.next();
+		this.current++;
+		try {
+			return next.getString(this.idAttribute);
+		} catch (DfException e) {
+			throw new RuntimeException(String.format("DfException caught retrieving object ID # %d", this.current), e);
+		}
+	}
+
+	@Override
+	public void remove() {
+		this.iterator.remove();
+	}
+}
