a580a94221985c18f2576c340e0a9e7eefd211fb
Diego Rivera <diego.rivera@armedia.com>
2014-11-25 21:27:04 +0000

    Release 1.0.0

---
+++ engine/tags/1.0.0/src/main/java/com/armedia/cmf/engine/importer/ImportException.java
@@ -0,0 +1,29 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.engine.importer;
+
+/**
+ * @author diego
+ *
+ */
+public class ImportException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ImportException() {
+		super();
+	}
+
+	public ImportException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ImportException(String message) {
+		super(message);
+	}
+
+	public ImportException(Throwable cause) {
+		super(cause);
+	}
+}
