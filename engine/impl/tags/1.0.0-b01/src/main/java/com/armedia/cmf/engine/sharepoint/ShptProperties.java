e00db1d75dd9b5741a0ea8edb47cf97256136530
Diego Rivera <diego.rivera@armedia.com>
2015-02-07 16:14:10 +0000

    Release 1.0.0-b01: ============================================================ * First functional beta release

---
+++ engine/impl/tags/1.0.0-b01/src/main/java/com/armedia/cmf/engine/sharepoint/ShptProperties.java
@@ -0,0 +1,22 @@
+package com.armedia.cmf.engine.sharepoint;
+
+public enum ShptProperties {
+
+	//
+	USERS_WITH_DEFAULT_GROUP,
+	TARGET_PATHS,
+	TARGET_PARENTS,
+	VERSION_PATCHES,
+	PATCH_ANTECEDENT,
+	USERS_WITH_DEFAULT_FOLDER,
+	USERS_DEFAULT_FOLDER_PATHS,
+	CONTENTS,
+	//
+	;
+
+	public final String name;
+
+	private ShptProperties() {
+		this.name = name().toLowerCase();
+	}
+}
