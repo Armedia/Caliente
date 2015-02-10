b574e43cebb332eb211f8a519cc9a133625ab02b
Diego Rivera <diego.rivera@armedia.com>
2015-02-10 15:18:41 +0000

    Setting no longer needed - common setting now the right way to do this.

--- store/trunk/src/main/java/com/armedia/cmf/storage/ContentStoreFactory.java
+++ store/trunk/src/main/java/com/armedia/cmf/storage/ContentStoreFactory.java
@@ -4,8 +4,6 @@ import java.util.Collection;
 
 public abstract class ContentStoreFactory<S extends ContentStore> extends StoreFactory<S> {
 
-	public static final String FRESH_START = "fresh.start";
-
 	protected ContentStoreFactory(String... aliases) {
 		super(aliases);
 	}
