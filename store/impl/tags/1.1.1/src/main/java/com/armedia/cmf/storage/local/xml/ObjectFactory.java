3348b462b8d430b4eb23a0a55f6e10e2aaf8f291
Diego Rivera <diego.rivera@armedia.com>
2015-02-16 16:25:03 +0000

    Release 1.1.1: ================================================================================ * Added support for store properties * Added support for instantiation with clean data

---
+++ store/impl/tags/1.1.1/src/main/java/com/armedia/cmf/storage/local/xml/ObjectFactory.java
@@ -0,0 +1,49 @@
+package com.armedia.cmf.storage.local.xml;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+@XmlRegistry
+public class ObjectFactory {
+
+	public static final String NAMESPACE = "http://www.armedia.com/ns/cmf/stores/local/store-properties";
+
+	private final static QName _StoreProperties_QNAME = new QName(ObjectFactory.NAMESPACE, "store-properties");
+
+	/**
+	 * Create a new ObjectFactory that can be used to create new instances of schema derived classes
+	 * for package: com.armedia.cmf.storage.local.xml
+	 *
+	 */
+	public ObjectFactory() {
+	}
+
+	/**
+	 * Create an instance of {@link StorePropertiesT }
+	 *
+	 */
+	public StorePropertiesT createStorePropertiesT() {
+		return new StorePropertiesT();
+	}
+
+	/**
+	 * Create an instance of {@link PropertyT }
+	 *
+	 */
+	public PropertyT createPropertyT() {
+		return new PropertyT();
+	}
+
+	/**
+	 * Create an instance of {@link JAXBElement }{@code <}{@link StorePropertiesT }{@code >}
+	 *
+	 */
+	@XmlElementDecl(namespace = ObjectFactory.NAMESPACE, name = "store-properties")
+	public JAXBElement<StorePropertiesT> createStoreProperties(StorePropertiesT value) {
+		return new JAXBElement<StorePropertiesT>(ObjectFactory._StoreProperties_QNAME, StorePropertiesT.class, null,
+			value);
+	}
+
+}
