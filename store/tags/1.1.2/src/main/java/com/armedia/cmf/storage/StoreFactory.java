b5400cc9101dd42f6077472b6bf3a368c4d4f3aa
Diego Rivera <diego.rivera@armedia.com>
2015-03-06 18:14:14 +0000

    Release 1.1.2: ================================================================================ * Updated to use the latest utilities API, with the changes to PluggableServiceLocator which allow for service instantiation errors to be hidden.

---
+++ store/tags/1.1.2/src/main/java/com/armedia/cmf/storage/StoreFactory.java
@@ -0,0 +1,61 @@
+package com.armedia.cmf.storage;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.armedia.cmf.storage.xml.StoreConfiguration;
+
+public abstract class StoreFactory<S extends Store> {
+
+	public static final String CFG_CLEAN_DATA = "clean.data";
+
+	private static final Pattern VALIDATOR = Pattern.compile("^[a-zA-Z_$][a-zA-Z\\d_$]*$");
+
+	private static final boolean isValidAlias(String alias) {
+		return (alias != null) && StoreFactory.VALIDATOR.matcher(alias).matches();
+	}
+
+	protected final Logger log = LoggerFactory.getLogger(getClass());
+
+	private final Set<String> aliases;
+
+	StoreFactory(String... aliases) {
+		this(aliases != null ? Arrays.asList(aliases) : null);
+	}
+
+	StoreFactory(Collection<String> aliases) {
+		Set<String> a = new TreeSet<String>();
+		if (aliases != null) {
+			for (String alias : aliases) {
+				if (StoreFactory.isValidAlias(alias)) {
+					a.add(alias);
+				}
+			}
+		}
+		if (a.isEmpty()) {
+			String msg = String.format("The final alias set for [%s] is empty - cannot continue", getClass()
+				.getCanonicalName());
+			this.log.error(msg);
+			throw new IllegalArgumentException(msg);
+		}
+		this.aliases = Collections.unmodifiableSet(a);
+		this.log.debug("StoreFactory [{}] will attempt to register for the following aliases: {}", getClass()
+			.getCanonicalName(), this.aliases);
+	}
+
+	protected final Set<String> getAliases() {
+		return this.aliases;
+	}
+
+	protected abstract S newInstance(StoreConfiguration cfg, boolean cleanData) throws StorageException;
+
+	protected void close() {
+	}
+}
---
+++ store/tags/1.1.2/src/main/java/com/armedia/cmf/storage/UnsupportedObjectTypeException.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.storage;
+
+/**
+ * @author Diego Rivera &lt;diego.rivera@armedia.com&gt;
+ *
+ */
+public class UnsupportedObjectTypeException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	UnsupportedObjectTypeException(String type) {
+		super(String.format("The object type [%s] is not supported", type));
+	}
+
+	public UnsupportedObjectTypeException(StoredObjectType type) {
+		this(type != null ? type.name() : "(null-value)");
+	}
+}
