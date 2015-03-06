31f5e1f9f7bcd61bc64d4fdc76aecb0fdfbb3c5b
Diego Rivera <diego.rivera@armedia.com>
2015-03-06 15:59:29 +0000

    Updated to the latest version of commons-pool2

--- engine/trunk/src/main/java/com/armedia/cmf/engine/SessionFactory.java
+++ engine/trunk/src/main/java/com/armedia/cmf/engine/SessionFactory.java
@@ -3,14 +3,15 @@ package com.armedia.cmf.engine;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-import org.apache.commons.pool.PoolableObjectFactory;
-import org.apache.commons.pool.impl.GenericObjectPool;
+import org.apache.commons.pool2.PooledObjectFactory;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.armedia.commons.utilities.CfgTools;
 
-public abstract class SessionFactory<S> implements PoolableObjectFactory<S> {
+public abstract class SessionFactory<S> implements PooledObjectFactory<S> {
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -20,19 +21,20 @@ public abstract class SessionFactory<S> implements PoolableObjectFactory<S> {
 
 	private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
-	protected static GenericObjectPool.Config getDefaultPoolConfig(CfgTools settings) {
-		GenericObjectPool.Config config = new GenericObjectPool.Config();
-		config.lifo = true;
-		config.maxActive = -1;
-		config.maxWait = 0; // wait indefinitely
-		config.whenExhaustedAction = GenericObjectPool.WHEN_EXHAUSTED_GROW;
-		config.minIdle = 1;
+	protected static GenericObjectPoolConfig getDefaultPoolConfig(CfgTools settings) {
+		GenericObjectPoolConfig config = new GenericObjectPoolConfig();
+		config.setLifo(true);
+		config.setMaxTotal(-1);
+		config.setMaxWaitMillis(0);
+		; // wait indefinitely
+		config.setBlockWhenExhausted(false);
+		config.setMinIdle(1);
 		// Make sure garbage gets collected quickly
-		config.testOnBorrow = true;
-		config.testOnReturn = true;
-		config.maxIdle = Math.max(config.minIdle, config.maxActive / 2);
-		config.timeBetweenEvictionRunsMillis = 30000;
-		config.minEvictableIdleTimeMillis = 45000;
+		config.setTestOnBorrow(true);
+		config.setTestOnReturn(true);
+		config.setMaxIdle(Math.max(config.getMinIdle(), config.getMaxTotal() / 2));
+		config.setTimeBetweenEvictionRunsMillis(30000);
+		config.setMinEvictableIdleTimeMillis(45000);
 		return config;
 	}
 
@@ -81,7 +83,7 @@ public abstract class SessionFactory<S> implements PoolableObjectFactory<S> {
 		}
 	}
 
-	protected GenericObjectPool.Config getPoolConfig(CfgTools settings) {
+	protected GenericObjectPoolConfig getPoolConfig(CfgTools settings) {
 		return SessionFactory.getDefaultPoolConfig(settings);
 	}
 
