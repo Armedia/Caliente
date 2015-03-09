6751682cddab95702fed6c7d7ff7bb5215dbd033
Diego Rivera <diego.rivera@armedia.com>
2015-03-09 23:20:26 +0000

    Release 1.1.6: ================================================================================ * Added code to defend against over-length values for String attributes being copied. They're now truncated as per attribute restrictions.

---
+++ engine/impl/tags/1.1.6/src/main/java/com/armedia/cmf/engine/documentum/common/DctmFolder.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.engine.documentum.common;
+
+/**
+ * @author diego
+ *
+ */
+public interface DctmFolder extends DctmSysObject {
+
+	static final String USERS_WITH_DEFAULT_FOLDER = "usersWithDefaultFolder";
+	static final String USERS_DEFAULT_FOLDER_PATHS = "usersDefaultFolderPaths";
+
+}
