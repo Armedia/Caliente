f3051b175ec62b4cae62e0ccce416909410a3500
Diego Rivera <diego.rivera@armedia.com>
2015-03-09 16:17:27 +0000

    Release 1.0.3: ================================================================================ * Added support for abstracted-out password encryption/decryption, to add more secure methods of operation later on.

---
+++ engine/impl/tags/1.0.3/src/main/java/com/armedia/cmf/engine/sharepoint/ShptSessionWrapper.java
@@ -0,0 +1,64 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.engine.sharepoint;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.armedia.cmf.engine.SessionWrapper;
+import com.independentsoft.share.Service;
+
+/**
+ * @author diego
+ *
+ */
+public class ShptSessionWrapper extends SessionWrapper<Service> {
+
+	private static final AtomicLong SESSION_ID = new AtomicLong(0);
+
+	private final String id;
+
+	protected ShptSessionWrapper(ShptSessionFactory factory, Service wrapped) {
+		super(factory, wrapped);
+		this.id = String.format("%08x", ShptSessionWrapper.SESSION_ID.incrementAndGet());
+	}
+
+	@Override
+	public String getId() {
+		return this.id;
+	}
+
+	@Override
+	protected boolean isSupportsTransactions() {
+		return false;
+	}
+
+	@Override
+	protected boolean isSupportsNestedTransactions() {
+		return false;
+	}
+
+	@Override
+	protected boolean isTransactionActive() throws Exception {
+		return false;
+	}
+
+	@Override
+	protected boolean beginTransaction() throws Exception {
+		return false;
+	}
+
+	@Override
+	protected boolean beginNestedTransaction() throws Exception {
+		return false;
+	}
+
+	@Override
+	protected void commitTransaction() throws Exception {
+	}
+
+	@Override
+	protected void rollbackTransaction() throws Exception {
+	}
+}
---
+++ engine/impl/tags/1.0.3/src/main/java/com/armedia/cmf/engine/sharepoint/exporter/ShptExportContext.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.engine.sharepoint.exporter;
+
+import org.slf4j.Logger;
+
+import com.armedia.cmf.engine.exporter.ExportContext;
+import com.armedia.cmf.engine.sharepoint.types.ShptObject;
+import com.armedia.cmf.storage.StoredObjectType;
+import com.armedia.cmf.storage.StoredValue;
+import com.armedia.commons.utilities.CfgTools;
+import com.independentsoft.share.Service;
+
+/**
+ * @author diego
+ *
+ */
+public class ShptExportContext extends ExportContext<Service, ShptObject<?>, StoredValue> {
+
+	public ShptExportContext(ShptExportEngine engine, CfgTools settings, String rootId, StoredObjectType rootType,
+		Service session, Logger output) {
+		super(engine, settings, rootId, rootType, session, output);
+	}
+
+}
