49f4ee3487b496dededd5e9b87d3391ba427c2f4
Diego Rivera <diego.rivera@armedia.com>
2015-03-09 14:42:52 +0000

    Removed the use of commons-codec in favor of JDK-only Base64 encoding methods.

--- engine/impl/trunk/src/main/java/com/armedia/cmf/engine/sharepoint/ShptEncrypterTool.java
+++ engine/impl/trunk/src/main/java/com/armedia/cmf/engine/sharepoint/ShptEncrypterTool.java
@@ -9,27 +9,19 @@ import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.Hex;
+import javax.xml.bind.DatatypeConverter;
 
 import com.armedia.cmf.engine.TransferEngineException;
 
 public abstract class ShptEncrypterTool {
 
 	// This should never change... EVER...
-	private static final String HEX_KEY = "b0c62998c73b180064618917c294b6e9";
+	private static final String ENCODED_KEY = "6RBjZgfVO+KhuPU0qSqmdQ==";
 	private static final Charset CHARSET = Charset.forName("UTF-8");
 	private static final SecretKey KEY;
 
 	static {
-		final byte[] key;
-		try {
-			key = Hex.decodeHex(ShptEncrypterTool.HEX_KEY.toCharArray());
-		} catch (DecoderException e) {
-			throw new RuntimeException(String.format("Failed to decode the hex key [%s]", ShptEncrypterTool.HEX_KEY), e);
-		}
+		final byte[] key = DatatypeConverter.parseBase64Binary(ShptEncrypterTool.ENCODED_KEY);
 		KEY = new SecretKeySpec(key, "AES");
 		try {
 			ShptEncrypterTool.getCipher(false);
@@ -49,7 +41,7 @@ public abstract class ShptEncrypterTool {
 	}
 
 	private static Cipher getCipher(boolean encrypt) throws NoSuchAlgorithmException, NoSuchPaddingException,
-		InvalidKeyException, InvalidAlgorithmParameterException {
+	InvalidKeyException, InvalidAlgorithmParameterException {
 		Cipher ret = Cipher.getInstance(ShptEncrypterTool.KEY.getAlgorithm());
 		ret.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, ShptEncrypterTool.KEY);
 		return ret;
@@ -62,13 +54,13 @@ public abstract class ShptEncrypterTool {
 		} catch (Exception e) {
 			throw new TransferEngineException("Failed to encrypt the given value", e);
 		}
-		return new Base64().encodeAsString(data);
+		return DatatypeConverter.printBase64Binary(data);
 	}
 
 	public static String decrypt(String value) throws TransferEngineException {
 		final byte[] data;
 		try {
-			data = ShptEncrypterTool.getCipher(false).doFinal(new Base64().decode(value));
+			data = ShptEncrypterTool.getCipher(false).doFinal(DatatypeConverter.parseBase64Binary(value));
 		} catch (Exception e) {
 			throw new TransferEngineException("Failed to decrypt the given value", e);
 		}
