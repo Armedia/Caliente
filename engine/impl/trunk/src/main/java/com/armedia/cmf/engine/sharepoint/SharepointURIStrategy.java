927529dd174a7c69cb4daf521edc7803d825ca51
Diego Rivera <diego.rivera@armedia.com>
2015-03-11 19:03:22 +0000

    Modified the URI strategy to ensure that files and folders are always created without unsafe characters.

--- engine/impl/trunk/src/main/java/com/armedia/cmf/engine/sharepoint/SharepointURIStrategy.java
+++ engine/impl/trunk/src/main/java/com/armedia/cmf/engine/sharepoint/SharepointURIStrategy.java
@@ -1,7 +1,13 @@
 package com.armedia.cmf.engine.sharepoint;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.armedia.cmf.storage.StoredObject;
 import com.armedia.cmf.storage.URIStrategy;
+import com.armedia.commons.utilities.FileNameTools;
 
 public class SharepointURIStrategy extends URIStrategy {
 
@@ -9,9 +15,24 @@ public class SharepointURIStrategy extends URIStrategy {
 		super("sharepoint");
 	}
 
+	protected String encodeSafePathComponent(String pathComponent) {
+		try {
+			return URLEncoder.encode(pathComponent, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("UTF-8 encoding not supported in this JVM", e);
+		}
+	}
+
 	@Override
 	protected String calculateSSP(StoredObject<?> object) {
-		// Put it in the same path as it was in Sharepoint...
-		return object.getSearchKey();
+		// Put it in the same path as it was in Sharepoint, but ensure each path component is
+		// of a "universally-valid" format.
+		String searchKey = object.getSearchKey();
+		final boolean leading = searchKey.startsWith("/");
+		List<String> pathItems = new ArrayList<String>();
+		for (String s : FileNameTools.tokenize(searchKey, '/')) {
+			pathItems.add(encodeSafePathComponent(s));
+		}
+		return FileNameTools.reconstitute(pathItems, leading, false, '/');
 	}
 }
