79c4dfd632deccb39235ae8c4c4d5ade78c74736
Diego Rivera <diego.rivera@armedia.com>
2015-03-12 14:42:36 +0000

    Added the concept of a cmsmf.lib directory which can contain JARs that get added to the classpath on startup.  This will facilitate using other JDBC engines than H2 if necessary.

--- tools/branches/3.0.0-sp/src/main/java/com/delta/cmsmf/launcher/CommonClasspathPatcher.java
+++ tools/branches/3.0.0-sp/src/main/java/com/delta/cmsmf/launcher/CommonClasspathPatcher.java
@@ -1,6 +1,7 @@
 package com.delta.cmsmf.launcher;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -9,6 +10,14 @@ import java.util.List;
 import com.delta.cmsmf.utils.ClasspathPatcher;
 
 public class CommonClasspathPatcher extends ClasspathPatcher {
+
+	private static final FilenameFilter LIB_FILTER = new FilenameFilter() {
+		@Override
+		public boolean accept(File dir, String name) {
+			name = name.toLowerCase();
+			return name.endsWith(".jar") || name.endsWith(".zip");
+		}
+	};
 
 	public CommonClasspathPatcher() {
 		super();
@@ -23,16 +32,20 @@ public class CommonClasspathPatcher extends ClasspathPatcher {
 	public List<URL> getPatches(String engine) {
 		List<URL> ret = new ArrayList<URL>(3);
 		try {
-			String var = null;
-			File base = null;
-			File tgt = null;
-
 			// First, add the ${PWD}/cfg directory to the classpath - whether it exists or not
-			var = System.getProperty("user.dir");
-			base = new File(var);
-			tgt = new File(var, "cfg");
-			ret.add(base.toURI().toURL());
-			ret.add(tgt.toURI().toURL());
+			final File userDir = new File(System.getProperty("user.dir"));
+
+			// Add all jar/zip files in ${user.dir}/cmsmf.lib, and ${user.dir}/cmsmf.lib/classes
+			// to the classpath
+			File libDir = new File(userDir, "cmsmf.lib");
+			if (libDir.exists() && libDir.isDirectory()) {
+				ret.add(new File(libDir, "classes").toURI().toURL());
+				for (File f : libDir.listFiles(CommonClasspathPatcher.LIB_FILTER)) {
+					ret.add(f.toURI().toURL());
+				}
+			}
+			ret.add(new File(userDir, "cfg").toURI().toURL());
+			ret.add(userDir.toURI().toURL());
 		} catch (IOException e) {
 			throw new RuntimeException(String.format("Failed to configure the dynamic classpath for engine [%s]",
 				engine), e);
