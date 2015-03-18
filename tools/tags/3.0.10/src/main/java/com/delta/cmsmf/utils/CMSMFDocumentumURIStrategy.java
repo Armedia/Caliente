8f514ed60accd4baa636fe49db939de21a292a6b
Diego Rivera <diego.rivera@armedia.com>
2015-03-18 20:16:38 +0000

    Release 3.0.10: ================================================================================ * Updated to the latest Sharepoint plugin (with the latest JShare fixes).

---
+++ tools/tags/3.0.10/src/main/java/com/delta/cmsmf/utils/CMSMFDocumentumURIStrategy.java
@@ -0,0 +1,40 @@
+package com.delta.cmsmf.utils;
+
+import java.util.Arrays;
+
+import com.armedia.cmf.storage.StoredObject;
+import com.armedia.cmf.storage.URIStrategy;
+import com.armedia.commons.utilities.FileNameTools;
+
+public class CMSMFDocumentumURIStrategy extends URIStrategy {
+
+	public CMSMFDocumentumURIStrategy() {
+		super("cmsmf");
+	}
+
+	@Override
+	protected String calculateSSP(StoredObject<?> object) {
+		final String objectId = object.getId();
+		if (objectId.length() != 16) { return null; }
+
+		try {
+			Long.parseLong(objectId, 16);
+		} catch (NumberFormatException e) {
+			// Not a documentum ID, so do something else
+			return null;
+		}
+
+		// 16 character object id in dctm consists of first 2 chars of obj type, next 6
+		// chars of docbase id in hex and last 8 chars server generated.
+		// For ex: if the id is 0600a92b80054db8 than the SSP would be
+		// /80/05/4d/0600a92b80054db8
+		String uniqueId = objectId.substring(8);
+		String[] components = {
+			uniqueId.substring(0, 2), // The first 3 characters of the unique object ID
+			uniqueId.substring(2, 4), // The 2nd 3 characters of the unique object ID
+			uniqueId.substring(4, 6), // The first 3 characters of the unique object ID
+			objectId
+		};
+		return FileNameTools.reconstitute(Arrays.asList(components), false, false, '/');
+	}
+}
