fca2016dd457fc7ce3a0a233ffef654da22efc49
Diego Rivera <diego.rivera@armedia.com>
2015-03-19 15:23:51 +0000

    Slight refinement to import type ignore logic.

--- engine/trunk/src/main/java/com/armedia/cmf/engine/importer/ImportEngine.java
+++ engine/trunk/src/main/java/com/armedia/cmf/engine/importer/ImportEngine.java
@@ -46,7 +46,7 @@ import com.armedia.commons.utilities.CfgTools;
  *
  */
 public abstract class ImportEngine<S, W extends SessionWrapper<S>, T, V, C extends ImportContext<S, T, V>> extends
-TransferEngine<S, T, V, C, ImportEngineListener> {
+	TransferEngine<S, T, V, C, ImportEngineListener> {
 
 	private static enum BatchStatus {
 		//
@@ -220,7 +220,7 @@ TransferEngine<S, T, V, C, ImportEngineListener> {
 
 	public final StoredObjectCounter<ImportResult> runImport(final Logger output, final ObjectStore<?, ?> objectStore,
 		final ContentStore streamStore, Map<String, ?> settings, StoredObjectCounter<ImportResult> counter)
-			throws ImportException, StorageException {
+		throws ImportException, StorageException {
 
 		// First things first...we should only do this if the target repo ID
 		// is not the same as the previous target repo - we can tell this by
@@ -377,16 +377,16 @@ TransferEngine<S, T, V, C, ImportEngineListener> {
 												listenerDelegator.objectImportFailed(next, t);
 												// Log the error, move on
 												this.log
-												.error(String.format("Exception caught processing %s", next), t);
+													.error(String.format("Exception caught processing %s", next), t);
 												if (batch.strategy.isBatchFailRemainder()) {
 													// If we're supposed to kill the batch, fail all
 													// the other objects
 													failBatch = true;
 													this.log
-													.debug(String
-														.format(
-															"Objects of type [%s] require that the remainder of the batch fail if an object fails",
-															storedType));
+														.debug(String
+															.format(
+																"Objects of type [%s] require that the remainder of the batch fail if an object fails",
+																storedType));
 													batch.markAborted(t);
 													continue;
 												}
@@ -585,22 +585,6 @@ TransferEngine<S, T, V, C, ImportEngineListener> {
 					listenerDelegator.importStarted(containedTypes);
 					final ObjectStorageTranslator<T, V> translator = getTranslator();
 					for (final StoredObjectType type : StoredObjectType.values()) {
-						if (!isSupported(type)) {
-							this.log.debug(String.format("Ignoring objects of type %s due to engine configuration",
-								type));
-							continue;
-						}
-
-						final ImportStrategy strategy = getImportStrategy(type);
-						if (strategy.isIgnored()) {
-							if (this.log.isDebugEnabled()) {
-								this.log
-								.debug(String.format(
-									"Skipping type %s because it marked as ignored by the given strategy",
-									type.name()));
-							}
-							continue;
-						}
 						final Integer total = containedTypes.get(type);
 						if (total == null) {
 							this.log.warn(String.format("No %s objects are contained in the export", type.name()));
@@ -612,6 +596,23 @@ TransferEngine<S, T, V, C, ImportEngineListener> {
 							continue;
 						}
 
+						if (!isSupported(type)) {
+							this.log.debug(String.format("Ignoring %d objects of type %s due to engine configuration",
+								total, type));
+							continue;
+						}
+
+						final ImportStrategy strategy = getImportStrategy(type);
+						if (strategy.isIgnored()) {
+							if (this.log.isDebugEnabled()) {
+								this.log
+									.debug(String
+										.format(
+											"Skipping %d objects of type %s because it's marked as ignored by the given strategy",
+											total, type.name()));
+							}
+							continue;
+						}
 						listenerDelegator.objectTypeImportStarted(type, total);
 
 						// Start the workers
@@ -717,10 +718,10 @@ TransferEngine<S, T, V, C, ImportEngineListener> {
 					if (pending > 0) {
 						try {
 							this.log
-							.info(String
-								.format(
-									"Waiting an additional 60 seconds for worker termination as a contingency (%d pending workers)",
-									pending));
+								.info(String
+									.format(
+										"Waiting an additional 60 seconds for worker termination as a contingency (%d pending workers)",
+										pending));
 							executor.awaitTermination(1, TimeUnit.MINUTES);
 						} catch (InterruptedException e) {
 							this.log.warn("Interrupted while waiting for immediate executor termination", e);
