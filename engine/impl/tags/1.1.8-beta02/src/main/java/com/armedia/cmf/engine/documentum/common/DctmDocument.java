5801371df7fae39d2fc4a8d1624bfaf03f8210af
Diego Rivera <diego.rivera@armedia.com>
2015-03-20 00:15:34 +0000

    Beta-02 marker

---
+++ engine/impl/tags/1.1.8-beta02/src/main/java/com/armedia/cmf/engine/documentum/common/DctmDocument.java
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.engine.documentum.common;
+
+/**
+ * @author diego
+ *
+ */
+public interface DctmDocument extends DctmSysObject {
+
+	static final String CONTENTS = "contents";
+
+}
