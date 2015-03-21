8ae0b303bf2e15010de68fee6381d8a0d270729c
Diego Rivera <diego.rivera@armedia.com>
2015-03-21 14:56:05 +0000

    Release 1.1.14: ================================================================================ * Updated to the latest cmf-engine API

---
+++ engine/impl/tags/1.0.14/src/main/java/com/armedia/cmf/engine/sharepoint/types/ShptSecurityObject.java
@@ -0,0 +1,35 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.engine.sharepoint.types;
+
+import com.armedia.cmf.engine.exporter.ExportException;
+import com.armedia.cmf.engine.sharepoint.ShptSession;
+import com.armedia.cmf.storage.StoredObject;
+import com.armedia.cmf.storage.StoredObjectType;
+import com.armedia.cmf.storage.StoredValue;
+
+/**
+ * @author diego
+ *
+ */
+public abstract class ShptSecurityObject<T> extends ShptObject<T> {
+
+	protected ShptSecurityObject(ShptSession service, T wrapped, StoredObjectType type) {
+		super(service, wrapped, type);
+	}
+
+	@Override
+	public final String getId() {
+		return String.format("%04x", getNumericId());
+	}
+
+	protected abstract int getNumericId();
+
+	@Override
+	protected void marshal(StoredObject<StoredValue> object) throws ExportException {
+		// TODO Auto-generated method stub
+
+	}
+}
