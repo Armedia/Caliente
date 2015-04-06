1c4cb9d07295a4a17b2ae60e8b59451f76e120c3
Diego Rivera <diego.rivera@armedia.com>
2015-04-06 14:57:06 +0000

    Release 1.2.1: ================================================================================ * Modified the scope for the class logs to be protected, so subclasses can readily make use of them.  Added instance logs where appropriate.

---
+++ engine/tags/1.2.1/src/main/java/com/armedia/cmf/engine/exporter/ExportListener.java
@@ -0,0 +1,47 @@
+package com.armedia.cmf.engine.exporter;
+
+import com.armedia.cmf.storage.StoredObject;
+import com.armedia.cmf.storage.StoredObjectType;
+
+public interface ExportListener {
+
+	/**
+	 * <p>
+	 * Invoked when export has started for the given object.
+	 * </p>
+	 *
+	 * @param objectType
+	 * @param objectId
+	 */
+	public void objectExportStarted(StoredObjectType objectType, String objectId);
+
+	/**
+	 * <p>
+	 * Invoked when the given object has been exported.
+	 * </p>
+	 *
+	 * @param object
+	 */
+	public void objectExportCompleted(StoredObject<?> object, Long objectNumber);
+
+	/**
+	 * <p>
+	 * Invoked when the given object has been skipped.
+	 * </p>
+	 *
+	 * @param objectType
+	 * @param objectId
+	 */
+	public void objectSkipped(StoredObjectType objectType, String objectId);
+
+	/**
+	 * <p>
+	 * Invoked when the given object has been exported.
+	 * </p>
+	 *
+	 * @param objectType
+	 * @param thrown
+	 */
+	public void objectExportFailed(StoredObjectType objectType, String objectId, Throwable thrown);
+
+}
