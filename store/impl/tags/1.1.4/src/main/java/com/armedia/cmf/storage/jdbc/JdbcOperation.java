a46aeef15fc2597a4d077e879f31e76a6b0280d0
Diego Rivera <diego.rivera@armedia.com>
2015-04-06 16:35:03 +0000

    Release 1.1.4: ================================================================================ * Updated to the latest dependency versions

---
+++ store/impl/tags/1.1.4/src/main/java/com/armedia/cmf/storage/jdbc/JdbcOperation.java
@@ -0,0 +1,50 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.storage.jdbc;
+
+import java.sql.Connection;
+
+import com.armedia.cmf.storage.ObjectStoreOperation;
+
+/**
+ * @author diego
+ *
+ */
+class JdbcOperation extends ObjectStoreOperation<Connection> {
+
+	private final boolean jtaManaged;
+
+	public JdbcOperation(Connection wrapped, boolean jtaManaged) {
+		super(wrapped);
+		this.jtaManaged = jtaManaged;
+	}
+
+	@Override
+	protected boolean supportsTransactions() {
+		return true;
+	}
+
+	@Override
+	protected boolean beginTransaction() throws Exception {
+		if (this.jtaManaged) { return true; }
+		getConnection().setAutoCommit(false);
+		return true;
+	}
+
+	@Override
+	protected void commitTransaction() throws Exception {
+		getConnection().commit();
+	}
+
+	@Override
+	protected void rollbackTransaction() throws Exception {
+		getConnection().rollback();
+	}
+
+	@Override
+	protected void closeConnection() throws Exception {
+		getConnection().close();
+	}
+}
