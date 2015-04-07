56daad83d1519c1fa5c20e2bf6fc15db43fcfbc2
Diego Rivera <diego.rivera@armedia.com>
2015-04-07 15:02:07 +0000

    Release 1.2.1: ================================================================================ * Added code to enhance the user mapping ability on import. The code is now able to map username-username, username-login, login-username, or login-login on import, which broadens the flexibility when attempting to match users between disparate environments. * Added code to more easily report errors when setting invalid object attribute values

---
+++ engine/impl/tags/1.2.1/src/main/java/com/armedia/cmf/engine/documentum/common/DctmACL.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.engine.documentum.common;
+
+
+/**
+ * @author diego
+ *
+ */
+public interface DctmACL {
+
+	static final String USERS_WITH_DEFAULT_ACL = "usersWithDefaultACL";
+	static final String ACCESSORS = "accessors";
+	static final String PERMIT_TYPE = "permitType";
+	static final String PERMIT_VALUE = "permitValue";
+
+}
