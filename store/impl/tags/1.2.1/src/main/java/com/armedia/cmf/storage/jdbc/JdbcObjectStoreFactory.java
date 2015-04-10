97c5147d2138cf8e35d2643087b36c51ecfe7548
Diego Rivera <diego.rivera@armedia.com>
2015-04-10 21:37:42 +0000

    Release 1.2.1: ================================================================================ * Updated to support postgresql due to data typing fix (long -> bigint)

---
+++ store/impl/tags/1.2.1/src/main/java/com/armedia/cmf/storage/jdbc/JdbcObjectStoreFactory.java
@@ -0,0 +1,52 @@
+package com.armedia.cmf.storage.jdbc;
+
+import java.sql.Connection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.armedia.cmf.storage.ObjectStoreFactory;
+import com.armedia.cmf.storage.StorageException;
+import com.armedia.cmf.storage.xml.StoreConfiguration;
+import com.armedia.commons.dslocator.DataSourceDescriptor;
+import com.armedia.commons.dslocator.DataSourceLocator;
+import com.armedia.commons.utilities.CfgTools;
+
+public class JdbcObjectStoreFactory extends ObjectStoreFactory<Connection, JdbcOperation, JdbcObjectStore> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(JdbcObjectStoreFactory.class);
+
+	public JdbcObjectStoreFactory() {
+		super("jdbc");
+	}
+
+	@Override
+	protected JdbcObjectStore newInstance(StoreConfiguration configuration, boolean cleanData) throws StorageException {
+		// It's either direct, or taken from Spring or JNDI
+		CfgTools cfg = new CfgTools(configuration.getEffectiveSettings());
+		final String locationType = cfg.getString(Setting.LOCATION_TYPE);
+		for (DataSourceLocator locator : DataSourceLocator.getAllLocatorsFor(locationType)) {
+			final DataSourceDescriptor<?> ds;
+			try {
+				ds = locator.locateDataSource(cfg);
+			} catch (Exception e) {
+				if (JdbcObjectStoreFactory.LOG.isTraceEnabled()) {
+					JdbcObjectStoreFactory.LOG.warn(String.format(
+						"Exception caught attempting to locate a DataSource via %s for ObjectStore %s[%s]", locator
+							.getClass().getCanonicalName(), configuration.getName(), configuration.getId()), e);
+				}
+				continue;
+			}
+			try {
+				return new JdbcObjectStore(ds, cfg.getBoolean(Setting.UPDATE_SCHEMA), cleanData);
+			} catch (Exception e) {
+				throw new StorageException(String.format("Failed to initialize the CmsObjectStore %s[%s]",
+					configuration.getName(), configuration.getId()), e);
+			}
+		}
+
+		// If we got here, then we have no locator for that datasource, so we simply explode
+		throw new StorageException(String.format("Failed to locate a DataSource for building the ObjectStore %s[%s]",
+			configuration.getName(), configuration.getId()));
+	}
+}
