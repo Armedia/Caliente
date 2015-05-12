8f774ce599780d90b7350b4062bad2c37dd1c40c
Diego Rivera <diego.rivera@armedia.com>
2015-05-12 15:11:58 +0000

    Solidified the URI strategy classes to remove superfluous classes, and make sure the correct path(s) are used.  Made the LocalURIStrategy part of the base API, since this is likely to be widely used.

---
+++ engine/trunk/src/main/java/com/armedia/cmf/engine/tools/LocalURIStrategy.java
@@ -0,0 +1,55 @@
+package com.armedia.cmf.engine.tools;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.armedia.cmf.engine.converter.IntermediateAttribute;
+import com.armedia.cmf.engine.converter.IntermediateProperty;
+import com.armedia.cmf.storage.ObjectStorageTranslator;
+import com.armedia.cmf.storage.StoredAttribute;
+import com.armedia.cmf.storage.StoredObject;
+import com.armedia.cmf.storage.StoredObjectType;
+import com.armedia.cmf.storage.StoredProperty;
+import com.armedia.cmf.storage.URIStrategy;
+import com.armedia.commons.utilities.FileNameTools;
+
+public class LocalURIStrategy extends URIStrategy {
+
+	public static final String NAME = "localfs";
+
+	public LocalURIStrategy() {
+		super(LocalURIStrategy.NAME);
+	}
+
+	protected String encodeSafePathComponent(String pathComponent) {
+		try {
+			return URLEncoder.encode(pathComponent, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("UTF-8 encoding not supported in this JVM", e);
+		}
+	}
+
+	@Override
+	protected String calculateSSP(ObjectStorageTranslator<?> translator, StoredObject<?> object) {
+		// Put it in the same path as it was in CMIS, but ensure each path component is
+		// of a "universally-valid" format.
+		final StoredObjectType type = object.getType();
+		StoredProperty<?> paths = object.getProperty(IntermediateProperty.PATH.encode());
+		String attName = translator.decodeAttributeName(type, IntermediateAttribute.NAME.encode());
+		StoredAttribute<?> name = object.getAttribute(attName);
+
+		String basePath = ((paths == null) || !paths.hasValues() ? "" : paths.getValue().toString());
+		String baseName = name.getValue().toString();
+
+		String finalPath = String.format("%s/%s", basePath, baseName);
+
+		List<String> pathItems = new ArrayList<String>();
+		for (String s : FileNameTools.tokenize(finalPath, '/')) {
+			// TODO: Only fix the path if it needs fixing (i.e. for Windows)
+			pathItems.add(encodeSafePathComponent(s));
+		}
+		return FileNameTools.reconstitute(pathItems, true, false, '/');
+	}
+}
