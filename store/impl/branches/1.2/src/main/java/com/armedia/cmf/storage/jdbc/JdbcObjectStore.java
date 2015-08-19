65889e14dbf838e30dba714a99552631f62b4e1c
Diego Rivera <diego.rivera@armedia.com>
2015-08-19 16:17:45 +0000

    Backported the race condition fix for the dependency locking conflict.  This should now fix the issue of some share-dependency or shared-referenced objects not being exported.

--- store/impl/branches/1.2/src/main/java/com/armedia/cmf/storage/jdbc/JdbcObjectStore.java
+++ store/impl/branches/1.2/src/main/java/com/armedia/cmf/storage/jdbc/JdbcObjectStore.java
@@ -89,75 +89,75 @@ public class JdbcObjectStore extends ObjectStore<Connection, JdbcOperation> {
 	private static final String DELETE_BOTH_MAPPINGS_SQL = "delete from cmf_mapper where object_type = ? and name = ? and not (source_value = ? and target_value = ?) and (source_value = ? or target_value = ?)";
 
 	private static final String LOAD_OBJECT_TYPES_SQL = //
-	"   select object_type, count(*) as total " + //
+		"   select object_type, count(*) as total " + //
 		" from cmf_object " + //
 		"group by object_type " + // ;
 		"having total > 0 " + //
 		"order by object_type ";
 
 	private static final String LOAD_OBJECTS_SQL = //
-	"    select * " + //
+		"    select * " + //
 		"  from cmf_object " + //
 		" where object_type = ? " + //
 		" order by batch_id, object_number";
 
 	private static final String LOAD_OBJECTS_BY_ID_ANY_SQL = //
-	"    select * " + //
+		"    select * " + //
 		"  from cmf_object " + //
 		" where object_type = ? " + //
 		"   and object_id = any ( ? ) " + //
 		" order by batch_id, object_number";
 
 	private static final String LOAD_OBJECTS_BY_ID_IN_SQL = //
-	"    select o.* " + //
+		"    select o.* " + //
 		"  from cmf_object o, table(x varchar=?) t " + //
 		" where o.object_type = ? " + //
 		"   and o.object_id = t.x " + //
 		" order by o.batch_id, o.object_number";
 
 	private static final String LOAD_ATTRIBUTES_SQL = //
-	"    select * " + //
+		"    select * " + //
 		"  from cmf_attribute " + //
 		" where object_id = ? " + //
 		" order by name";
 
 	private static final String LOAD_ATTRIBUTE_VALUES_SQL = //
-	"    select * " + //
+		"    select * " + //
 		"  from cmf_attribute_value " + //
 		" where object_id = ? " + //
 		"   and name = ? " + //
 		" order by value_number";
 
 	private static final String LOAD_PROPERTIES_SQL = //
-	"    select * " + //
+		"    select * " + //
 		"  from cmf_property " + //
 		" where object_id = ? " + //
 		" order by name";
 
 	private static final String LOAD_PROPERTY_VALUES_SQL = //
-	"    select * " + //
+		"    select * " + //
 		"  from cmf_property_value " + //
 		" where object_id = ? " + //
 		"   and name = ? " + //
 		" order by value_number";
 
 	private static final String GET_STORE_PROPERTY_SQL = //
-	"    select * from cmf_info where name = ? ";
+		"    select * from cmf_info where name = ? ";
 
 	private static final String UPDATE_STORE_PROPERTY_SQL = //
-	"    update cmf_info set value = ? where name = ? ";
+		"    update cmf_info set value = ? where name = ? ";
 
 	private static final String INSERT_STORE_PROPERTY_SQL = //
-	"    insert into cmf_info (name, data_type, value) values (?, ?, ?) ";
+		"    insert into cmf_info (name, data_type, value) values (?, ?, ?) ";
 
 	private static final String DELETE_STORE_PROPERTY_SQL = //
-	"    delete from cmf_info where name = ? ";
+		"    delete from cmf_info where name = ? ";
 
 	private static final String GET_STORE_PROPERTY_NAMES_SQL = //
-	"    select name from cmf_info order by name ";
+		"    select name from cmf_info order by name ";
 
 	private static final String DELETE_ALL_STORE_PROPERTIES_SQL = //
-	"    truncate table cmf_info ";
+		"    truncate table cmf_info ";
 
 	private static final ResultSetHandler<Object> HANDLER_NULL = new ResultSetHandler<Object>() {
 		@Override
@@ -292,10 +292,10 @@ public class JdbcObjectStore extends ObjectStore<Connection, JdbcOperation> {
 				final String duplicate = encodedNames.put(name, attribute.getName());
 				if (duplicate != null) {
 					this.log
-						.warn(String
-							.format(
-								"Duplicate encoded attribute name [%s] resulted from encoding [%s] (previous encoding came from [%s])",
-								name, attribute.getName(), duplicate));
+					.warn(String
+						.format(
+							"Duplicate encoded attribute name [%s] resulted from encoding [%s] (previous encoding came from [%s])",
+							name, attribute.getName(), duplicate));
 					continue;
 				}
 				final boolean repeating = attribute.isRepeating();
@@ -350,10 +350,10 @@ public class JdbcObjectStore extends ObjectStore<Connection, JdbcOperation> {
 				final String duplicate = encodedNames.put(name, property.getName());
 				if (duplicate != null) {
 					this.log
-						.warn(String
-							.format(
-								"Duplicate encoded property name [%s] resulted from encoding [%s] (previous encoding came from [%s])",
-								name, property.getName(), duplicate));
+					.warn(String
+						.format(
+							"Duplicate encoded property name [%s] resulted from encoding [%s] (previous encoding came from [%s])",
+							name, property.getName(), duplicate));
 					continue;
 				}
 				final String type = translator.encodeValue(property.getType());
@@ -687,7 +687,7 @@ public class JdbcObjectStore extends ObjectStore<Connection, JdbcOperation> {
 			qr.insert(c, JdbcObjectStore.INSERT_MAPPING_SQL, JdbcObjectStore.HANDLER_NULL, type.name(), name,
 				sourceValue, targetValue);
 			this.log
-				.info(String.format("Established the mapping [%s/%s/%s->%s]", type, name, sourceValue, targetValue));
+			.info(String.format("Established the mapping [%s/%s/%s->%s]", type, name, sourceValue, targetValue));
 		} else if (this.log.isDebugEnabled()) {
 			this.log.debug(String.format("The mapping [%s/%s/%s->%s] already exists", type, name, sourceValue,
 				targetValue));
@@ -757,27 +757,27 @@ public class JdbcObjectStore extends ObjectStore<Connection, JdbcOperation> {
 		try {
 			return new QueryRunner().query(c, JdbcObjectStore.LOAD_OBJECT_TYPES_SQL,
 				new ResultSetHandler<Map<StoredObjectType, Integer>>() {
-					@Override
-					public Map<StoredObjectType, Integer> handle(ResultSet rs) throws SQLException {
-						Map<StoredObjectType, Integer> ret = new EnumMap<StoredObjectType, Integer>(
-							StoredObjectType.class);
-						while (rs.next()) {
-							String t = rs.getString("object_type");
-							if ((t == null) || rs.wasNull()) {
-								JdbcObjectStore.this.log.warn(String.format("NULL TYPE STORED IN DATABASE: [%s]", t));
-								continue;
-							}
-							try {
-								ret.put(StoredObjectType.decodeString(t), rs.getInt("total"));
-							} catch (IllegalArgumentException e) {
-								JdbcObjectStore.this.log.warn(String.format(
-									"UNSUPPORTED TYPE STORED IN DATABASE: [%s]", t));
-								continue;
-							}
+				@Override
+				public Map<StoredObjectType, Integer> handle(ResultSet rs) throws SQLException {
+					Map<StoredObjectType, Integer> ret = new EnumMap<StoredObjectType, Integer>(
+						StoredObjectType.class);
+					while (rs.next()) {
+						String t = rs.getString("object_type");
+						if ((t == null) || rs.wasNull()) {
+							JdbcObjectStore.this.log.warn(String.format("NULL TYPE STORED IN DATABASE: [%s]", t));
+							continue;
 						}
-						return ret;
+						try {
+							ret.put(StoredObjectType.decodeString(t), rs.getInt("total"));
+						} catch (IllegalArgumentException e) {
+							JdbcObjectStore.this.log.warn(String.format(
+								"UNSUPPORTED TYPE STORED IN DATABASE: [%s]", t));
+							continue;
+						}
 					}
-				});
+					return ret;
+				}
+			});
 		} catch (SQLException e) {
 			throw new StorageException("Failed to retrieve the stored object types", e);
 		}
@@ -1005,15 +1005,8 @@ public class JdbcObjectStore extends ObjectStore<Connection, JdbcOperation> {
 		QueryRunner qr = JdbcObjectStore.getQueryRunner();
 		final String dbid = composeDatabaseId(type, id);
 		try {
-			if (qr.query(c, JdbcObjectStore.QUERY_EXPORT_PLAN_DUPE_SQL, JdbcObjectStore.HANDLER_EXISTS, dbid)) {
-				// Duplicate dependency...we skip it
-				if (this.log.isTraceEnabled()) {
-					this.log.trace(String.format("DUPLICATE DEPENDENCY [%s::%s]", type.name(), id));
-				}
-				return false;
-			}
 			if (this.log.isTraceEnabled()) {
-				this.log.trace(String.format("PERSISTING DEPENDENCY [%s::%s]", type.name(), id));
+				this.log.trace(String.format("ATTEMPTING TO PERSIST DEPENDENCY [%s::%s]", type.name(), id));
 			}
 			qr.insert(c, JdbcObjectStore.INSERT_EXPORT_PLAN_SQL, JdbcObjectStore.HANDLER_NULL, type.name(), dbid);
 			if (this.log.isDebugEnabled()) {
@@ -1021,6 +1014,19 @@ public class JdbcObjectStore extends ObjectStore<Connection, JdbcOperation> {
 			}
 			return true;
 		} catch (SQLException e) {
+			try {
+				if (qr.query(c, JdbcObjectStore.QUERY_EXPORT_PLAN_DUPE_SQL, JdbcObjectStore.HANDLER_EXISTS, dbid)) {
+					// Duplicate dependency...we skip it
+					if (this.log.isTraceEnabled()) {
+						this.log.trace(String.format("DUPLICATE DEPENDENCY [%s::%s]", type.name(), id));
+					}
+					return false;
+				}
+			} catch (SQLException e2) {
+				this.log.trace(String.format(
+					"Exception caught attempting to avoid a race condition with dependency [%s::%s]", type.name(), id),
+					e2);
+			}
 			throw new StorageException(String.format("Failed to persist the dependency [%s::%s]", type.name(), id), e);
 		}
 	}
@@ -1051,30 +1057,30 @@ public class JdbcObjectStore extends ObjectStore<Connection, JdbcOperation> {
 		try {
 			return JdbcObjectStore.getQueryRunner().query(c, JdbcObjectStore.GET_STORE_PROPERTY_SQL,
 				new ResultSetHandler<StoredValue>() {
-					@Override
-					public StoredValue handle(ResultSet rs) throws SQLException {
-						if (!rs.next()) { return null; }
-						String name = rs.getString("name");
-						String type = rs.getString("data_type");
-						final StoredDataType t;
+				@Override
+				public StoredValue handle(ResultSet rs) throws SQLException {
+					if (!rs.next()) { return null; }
+					String name = rs.getString("name");
+					String type = rs.getString("data_type");
+					final StoredDataType t;
 
-						try {
-							t = StoredDataType.decodeString(type);
-						} catch (IllegalArgumentException e) {
-							throw new SQLException(String.format("Unsupported data type name: [%s]", type), e);
-						}
-						final StoredValueSerializer deserializer = StoredValueSerializer.get(t);
-						if (deserializer == null) { throw new SQLException(String.format(
-							"Unsupported data type name for serialization: [%s]", type)); }
-						String value = rs.getString("value");
-						try {
-							return deserializer.deserialize(value);
-						} catch (ParseException e) {
-							throw new SQLException(String.format(
-								"Failed to deserialize store property [%s]:[%s] as a %s", name, value, type), e);
-						}
+					try {
+						t = StoredDataType.decodeString(type);
+					} catch (IllegalArgumentException e) {
+						throw new SQLException(String.format("Unsupported data type name: [%s]", type), e);
 					}
-				}, property);
+					final StoredValueSerializer deserializer = StoredValueSerializer.get(t);
+					if (deserializer == null) { throw new SQLException(String.format(
+						"Unsupported data type name for serialization: [%s]", type)); }
+					String value = rs.getString("value");
+					try {
+						return deserializer.deserialize(value);
+					} catch (ParseException e) {
+						throw new SQLException(String.format(
+							"Failed to deserialize store property [%s]:[%s] as a %s", name, value, type), e);
+					}
+				}
+			}, property);
 		} catch (SQLException e) {
 			throw new StorageException(String.format("Failed to retrieve the value of store property [%s]", property),
 				e);
@@ -1118,15 +1124,15 @@ public class JdbcObjectStore extends ObjectStore<Connection, JdbcOperation> {
 			return JdbcObjectStore.getQueryRunner().query(c, JdbcObjectStore.GET_STORE_PROPERTY_NAMES_SQL,
 				new ResultSetHandler<Set<String>>() {
 
-					@Override
-					public Set<String> handle(ResultSet rs) throws SQLException {
-						Set<String> ret = new TreeSet<String>();
-						while (rs.next()) {
-							ret.add(rs.getString("name"));
-						}
-						return ret;
+				@Override
+				public Set<String> handle(ResultSet rs) throws SQLException {
+					Set<String> ret = new TreeSet<String>();
+					while (rs.next()) {
+						ret.add(rs.getString("name"));
 					}
-				});
+					return ret;
+				}
+			});
 		} catch (SQLException e) {
 			throw new StorageException("Failed to retrieve the store property names", e);
 		}
