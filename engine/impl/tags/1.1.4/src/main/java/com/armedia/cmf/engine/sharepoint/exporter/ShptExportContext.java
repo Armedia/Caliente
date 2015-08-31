4e11ae22c687502c70e09aad42543df67eb80ebe
Diego Rivera <diego.rivera@armedia.com>
2015-08-31 21:56:57 +0000

    Release 1.1.4: ================================================================================ * Updated the version of cmf-engine required.

---
+++ engine/impl/tags/1.1.4/src/main/java/com/armedia/cmf/engine/sharepoint/exporter/ShptExportContext.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.engine.sharepoint.exporter;
+
+import org.slf4j.Logger;
+
+import com.armedia.cmf.engine.exporter.ExportContext;
+import com.armedia.cmf.engine.sharepoint.ShptSession;
+import com.armedia.cmf.engine.sharepoint.types.ShptObject;
+import com.armedia.cmf.storage.StoredObjectType;
+import com.armedia.cmf.storage.StoredValue;
+import com.armedia.commons.utilities.CfgTools;
+
+/**
+ * @author diego
+ *
+ */
+public class ShptExportContext extends ExportContext<ShptSession, ShptObject<?>, StoredValue> {
+
+	public ShptExportContext(ShptExportContextFactory factory, CfgTools settings, String rootId,
+		StoredObjectType rootType, ShptSession session, Logger output) {
+		super(factory, settings, rootId, rootType, session, output);
+	}
+
+}
