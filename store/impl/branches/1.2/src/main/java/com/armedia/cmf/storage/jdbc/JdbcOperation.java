4b6cccec66011441b70c48677b3b97a06202ff92
Diego Rivera <diego.rivera@armedia.com>
2015-09-22 15:44:10 +0000

    Fixed to properly handle autocommit status.

--- store/impl/branches/1.2/src/main/java/com/armedia/cmf/storage/jdbc/JdbcOperation.java
+++ store/impl/branches/1.2/src/main/java/com/armedia/cmf/storage/jdbc/JdbcOperation.java
@@ -5,8 +5,10 @@
 package com.armedia.cmf.storage.jdbc;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import com.armedia.cmf.storage.ObjectStoreOperation;
+import com.armedia.cmf.storage.StorageException;
 
 /**
  * @author diego
@@ -16,9 +18,26 @@ class JdbcOperation extends ObjectStoreOperation<Connection> {
 
 	private final boolean jtaManaged;
 
-	public JdbcOperation(Connection wrapped, boolean jtaManaged) {
+	public JdbcOperation(Connection wrapped, boolean jtaManaged) throws StorageException {
 		super(wrapped);
 		this.jtaManaged = jtaManaged;
+		try {
+			if (!jtaManaged && !wrapped.getAutoCommit()) {
+				wrapped.setAutoCommit(true);
+			}
+		} catch (SQLException e) {
+			throw new StorageException("Failed to set the default autocommit state to true", e);
+		}
+	}
+
+	protected void restoreAutoCommit(Connection c) {
+		try {
+			c.setAutoCommit(true);
+		} catch (SQLException e) {
+			if (this.log.isDebugEnabled()) {
+				this.log.warn("Failed to re-enable autocommit", e);
+			}
+		}
 	}
 
 	@Override
@@ -27,20 +46,48 @@ class JdbcOperation extends ObjectStoreOperation<Connection> {
 	}
 
 	@Override
-	protected boolean beginTransaction() throws Exception {
+	protected boolean beginTransaction() throws StorageException {
 		if (this.jtaManaged) { return true; }
-		getConnection().setAutoCommit(false);
+		try {
+			getConnection().setAutoCommit(false);
+		} catch (SQLException e) {
+			throw new StorageException("Failed to begin a new transaction", e);
+		}
 		return true;
 	}
 
 	@Override
-	protected void commitTransaction() throws Exception {
-		getConnection().commit();
+	protected void commitTransaction() throws StorageException {
+		if (this.jtaManaged) { return; }
+		final Connection connection = getConnection();
+		boolean ok = false;
+		try {
+			connection.commit();
+			ok = true;
+		} catch (SQLException e) {
+			throw new StorageException("Failed to commit the active transaction", e);
+		} finally {
+			if (ok) {
+				restoreAutoCommit(connection);
+			}
+		}
 	}
 
 	@Override
-	protected void rollbackTransaction() throws Exception {
-		getConnection().rollback();
+	protected void rollbackTransaction() throws StorageException {
+		if (this.jtaManaged) { return; }
+		final Connection connection = getConnection();
+		boolean ok = false;
+		try {
+			connection.rollback();
+			ok = true;
+		} catch (SQLException e) {
+			throw new StorageException("Failed to rollback the active transaction", e);
+		} finally {
+			if (ok) {
+				restoreAutoCommit(connection);
+			}
+		}
 	}
 
 	@Override
