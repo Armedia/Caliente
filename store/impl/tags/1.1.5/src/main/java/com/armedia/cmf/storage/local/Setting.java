b0f2f3404762f96a25fef5055c068f7671f87f02
Diego Rivera <diego.rivera@armedia.com>
2015-09-29 23:16:40 +0000

    Release 1.1.5: ================================================================================ * Updated to the latest storage API

---
+++ store/impl/tags/1.1.5/src/main/java/com/armedia/cmf/storage/local/Setting.java
@@ -0,0 +1,34 @@
+package com.armedia.cmf.storage.local;
+
+import com.armedia.commons.utilities.ConfigurationSetting;
+
+public enum Setting implements ConfigurationSetting {
+	//
+	BASE_DIR,
+	URI_STRATEGY;
+
+	private final String label;
+	private final Object defaultValue;
+
+	private Setting() {
+		this(null);
+	}
+
+	private Setting(Object defaultValue) {
+		String l = name();
+		l = l.toLowerCase();
+		l = l.replaceAll("_", ".");
+		this.label = l;
+		this.defaultValue = defaultValue;
+	}
+
+	@Override
+	public String getLabel() {
+		return this.label;
+	}
+
+	@Override
+	public Object getDefaultValue() {
+		return this.defaultValue;
+	}
+}
