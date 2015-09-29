9e8c4a25d367498a5e13031ef68390f4699eb39f
Diego Rivera <diego.rivera@armedia.com>
2015-09-29 15:02:08 +0000

    Release 1.2.6: ================================================================================ * Updated to the latest cmf-engine

---
+++ engine/impl/tags/1.2.6/src/main/java/com/armedia/cmf/engine/documentum/common/DctmGroup.java
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.engine.documentum.common;
+
+/**
+ * @author diego
+ *
+ */
+public interface DctmGroup {
+
+	static final String USERS_WITH_DEFAULT_GROUP = "usersWithDefaultGroup";
+
+}
