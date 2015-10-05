8ec3a679ae94a16a305c18462eb767a8fd1e9222
Diego Rivera <diego.rivera@armedia.com>
2015-10-05 15:54:44 +0000

    Began modifications towards abstracting out the saving of state for repeat exports/imports.

--- tools/trunk/src/main/java/com/delta/cmsmf/launcher/dctm/CMSMFMain_export.java
+++ tools/trunk/src/main/java/com/delta/cmsmf/launcher/dctm/CMSMFMain_export.java
@@ -1,44 +1,24 @@
 package com.delta.cmsmf.launcher.dctm;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
-import javax.mail.MessagingException;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.time.DateFormatUtils;
 
-import com.armedia.cmf.engine.TransferSetting;
 import com.armedia.cmf.engine.documentum.DocumentumOrganizationStrategy;
 import com.armedia.cmf.engine.documentum.exporter.DctmExportEngine;
-import com.armedia.cmf.engine.exporter.ExportEngine;
 import com.armedia.cmf.engine.exporter.ExportEngineListener;
-import com.armedia.cmf.engine.exporter.ExportResult;
-import com.armedia.cmf.engine.exporter.ExportSkipReason;
-import com.armedia.cmf.storage.CmfObject;
-import com.armedia.cmf.storage.CmfType;
 import com.armedia.commons.dfc.pool.DfcSessionFactory;
 import com.armedia.commons.dfc.pool.DfcSessionPool;
-import com.armedia.commons.utilities.CfgTools;
-import com.armedia.commons.utilities.Tools;
-import com.delta.cmsmf.cfg.CLIParam;
 import com.delta.cmsmf.cfg.Setting;
 import com.delta.cmsmf.exception.CMSMFException;
-import com.delta.cmsmf.launcher.AbstractCMSMFMain;
-import com.delta.cmsmf.launcher.ExportManifest;
-import com.delta.cmsmf.utils.CMSMFUtils;
+import com.delta.cmsmf.launcher.AbstractCMSMFMain_export;
 import com.documentum.fc.client.IDfSession;
 import com.documentum.fc.common.DfException;
 import com.documentum.fc.common.DfTime;
 import com.documentum.fc.common.IDfTime;
 
-public class CMSMFMain_export extends AbstractCMSMFMain<ExportEngineListener, ExportEngine<?, ?, ?, ?, ?, ?>> implements
-	ExportEngineListener {
+public class CMSMFMain_export extends AbstractCMSMFMain_export implements ExportEngineListener {
 
 	protected static final String LAST_EXPORT_DATETIME_PATTERN = IDfTime.DF_TIME_PATTERN26;
 
@@ -51,26 +31,15 @@ public class CMSMFMain_export extends AbstractCMSMFMain<ExportEngineListener, Ex
 	private static final String DEFAULT_PREDICATE = "dm_sysobject where (TYPE(\"dm_folder\") or TYPE(\"dm_document\")) "
 		+ "and not folder('/System', descend)"; // and r_modify_date >= DATE('XX_PLACE_HOLDER_XX')";
 
+	private DfcSessionPool pool = null;
+	private IDfSession session = null;
+
 	public CMSMFMain_export() throws Throwable {
 		super(DctmExportEngine.getExportEngine());
 	}
 
-	/**
-	 * Starts exporting objects from source directory. It reads up the query from properties file
-	 * and executes it against the source repository. It retrieves objects from the repository and
-	 * exports it out.
-	 *
-	 */
 	@Override
-	public void run() throws CMSMFException {
-		Set<ExportResult> outcomes = Tools.parseEnumCSV(ExportResult.class, Setting.MANIFEST_OUTCOMES.getString(),
-			AbstractCMSMFMain.ALL, false);
-		Set<CmfType> types = Tools.parseEnumCSV(CmfType.class, Setting.MANIFEST_TYPES.getString(),
-			AbstractCMSMFMain.ALL, false);
-		this.engine.addListener(this);
-		this.engine.addListener(new ExportManifest(outcomes, types));
-
-		Map<String, Object> settings = new HashMap<String, Object>();
+	protected void customizeSettings(Map<String, Object> settings) throws CMSMFException {
 		if (this.server != null) {
 			settings.put(DfcSessionFactory.DOCBASE, this.server);
 		}
@@ -80,143 +49,102 @@ public class CMSMFMain_export extends AbstractCMSMFMain<ExportEngineListener, Ex
 		if (this.password != null) {
 			settings.put(DfcSessionFactory.PASSWORD, this.password);
 		}
-		settings.put(TransferSetting.EXCLUDE_TYPES.getLabel(), Setting.CMF_EXCLUDE_TYPES.getString(null));
+		String predicate = Setting.EXPORT_PREDICATE.getString();
+		if (StringUtils.isBlank(predicate)) {
+			// Try to locate a object in source repository that represents a last successful export
+			// to a target repository.
+			// NOTE : We will create a cabinet named 'CMSMF_SYNC' in source repository. We will
+			// create a folder for each target repository in this cabinet, the name of the folder
+			// will be the name of a target repository. In this folder we will create an object
+			// named 'cmsmf_last_export' and
 
-		final DfcSessionPool pool;
+			// first get the last export date from the source repository
+			predicate = CMSMFMain_export.DEFAULT_PREDICATE;
+		}
+		// Set the default predicate...
+		settings.put("dql", String.format("select r_object_id from %s", predicate));
+	}
+
+	@Override
+	protected void prepareState(Map<String, Object> settings) throws CMSMFException {
 		try {
-			pool = new DfcSessionPool(settings);
+			this.pool = new DfcSessionPool(settings);
+			this.session = this.pool.acquireSession();
 		} catch (Exception e) {
-			throw new CMSMFException("Failed to initialize the connection pool", e);
+			throw new CMSMFException("Failed to initialize the connection pool or get the primary session", e);
 		}
+	}
 
-		Date end = null;
-		Map<CmfType, Integer> summary = null;
-		String exceptionReport = null;
-		StringBuilder report = new StringBuilder();
-		Date start = null;
+	@Override
+	protected void cleanupState() {
+		if (this.session != null) {
+			this.pool.releaseSession(this.session);
+		}
+		if (this.pool != null) {
+			this.pool.close();
+		}
+	}
+
+	@Override
+	protected boolean loadSettings(String jobName, Map<String, Object> settings) throws CMSMFException {
+
 		try {
-			final IDfSession session;
+			/**
+			 * Now, we try to set the last export date
+			 */
 			try {
-				session = pool.acquireSession();
-			} catch (Exception e) {
-				throw new CMSMFException("Failed to obtain the main session from the pool", e);
-			}
-
-			String dql = String.format("select r_object_id from %s", buildExportPredicate(session));
-			settings.put("dql", dql);
-
-			start = new Date();
-			try {
-				this.log.info("##### Export Process Started #####");
-				this.engine.runExport(this.console, this.cmfObjectStore, this.cmfContentStore, settings);
-				this.log.info("##### Export Process Finished #####");
-
-				/**
-				 * Now, we try to set the last export date
-				 */
-
-				boolean ok = false;
-				try {
-					session.beginTrans();
-					// If this is auto run type of an export instead of an adhoc query export, store
-					// the value of the current export date in the repository. This value will be
-					// looked up in the next run. This is indeed an auto run type of export
-					DctmUtils.setLastExportDate(session, start);
-					ok = true;
-				} finally {
-					try {
-						if (ok) {
-							session.commitTrans();
-						} else {
-							session.abortTrans();
-						}
-					} catch (DfException e) {
-						this.log.error(String.format(
-							"Exception caught while %s the transaction for saving the export metadata",
-							ok ? "committing" : "aborting"), e);
-					}
-				}
-
-				summary = this.cmfObjectStore.getStoredObjectTypes();
-			} catch (Throwable t) {
-				StringWriter sw = new StringWriter();
-				PrintWriter pw = new PrintWriter(sw);
-				report.append(String.format("%n%nException caught while attempting an export%n%n"));
-				t.printStackTrace(pw);
-				exceptionReport = sw.toString();
+				this.session.beginTrans();
+				String dql = String.format("select r_object_id from %s", buildExportPredicate(this.session));
+				settings.put("dql", dql);
 			} finally {
-				// unlock
-				end = new Date();
-			}
-
-			try {
-				session.disconnect();
-			} catch (DfException e) {
-				if (this.log.isTraceEnabled()) {
-					this.log.warn("Exception caught when releasing the master session", e);
-				} else {
-					this.log.warn("Exception caught when releasing the master session: {}", e.getMessage());
+				try {
+					this.session.abortTrans();
+				} catch (DfException e) {
+					this.log.error(
+						"Exception caught while rolling back the transaction for saving the export metadata", e);
 				}
 			}
-		} finally {
-			pool.close();
+			return true;
+		} catch (Exception e) {
+			throw new CMSMFException("Exception caught storing the export metadata", e);
 		}
+	}
 
-		long duration = (end.getTime() - start.getTime());
-		long hours = TimeUnit.HOURS.convert(duration, TimeUnit.MILLISECONDS);
-		duration -= hours * TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS);
-		long minutes = TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS);
-		duration -= minutes * TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES);
-		long seconds = TimeUnit.SECONDS.convert(duration, TimeUnit.MILLISECONDS);
-		report.append(String.format("Export process start    : %s%n",
-			DateFormatUtils.format(start, AbstractCMSMFMain.JAVA_SQL_DATETIME_PATTERN)));
-		report.append(String.format("Export process end      : %s%n",
-			DateFormatUtils.format(end, AbstractCMSMFMain.JAVA_SQL_DATETIME_PATTERN)));
-		report.append(String.format("Export process duration : %02d:%02d:%02d%n", hours, minutes, seconds));
+	@Override
+	protected boolean storeSettings(String jobName, Map<String, Object> settings, Date exportStart, Date exportEnd)
+		throws CMSMFException {
 
-		report.append(String.format("%n%nParameters in use:%n")).append(StringUtils.repeat("=", 30));
-		for (CLIParam p : CLIParam.values()) {
-			if (!p.isPresent()) {
-				continue;
-			}
-			String v = p.getString();
-			if (v == null) {
-				report.append(String.format("%n\t--%s", p.option.getLongOpt()));
-			} else {
-				report.append(String.format("%n\t--%s = [%s]", p.option.getLongOpt(), v));
-			}
-		}
-
-		report.append(String.format("%n%n%nSettings in use:%n")).append(StringUtils.repeat("=", 30));
-		for (Setting s : Setting.values()) {
-			report.append(String.format("%n\t%s = [%s]", s.name, s.getString()));
-		}
-
-		if (summary != null) {
-			report.append(String.format("%n%n%nExported Object Summary:%n")).append(StringUtils.repeat("=", 30));
-			int total = 0;
-			for (CmfType t : summary.keySet()) {
-				Integer count = summary.get(t);
-				if ((count == null) || (count == 0)) {
-					continue;
-				}
-				report.append(String.format("%n%-16s: %6d", t.name(), count));
-				total += count;
-			}
-			report.append(String.format("%n%s%n%-16s: %6d%n", StringUtils.repeat("=", 30), "Total", total));
-		}
-
-		if (exceptionReport != null) {
-			report.append(String.format("%n%n%nEXCEPTION REPORT FOLLOWS:%n%n")).append(exceptionReport);
-		}
-
-		String reportString = report.toString();
-		this.log.info(String.format("Action report for export operation:%n%n%s%n", reportString));
 		try {
-			CMSMFUtils.postCmsmfMail(String.format("Action report for CMSMF Export"), reportString);
-		} catch (MessagingException e) {
-			this.log.error("Exception caught attempting to send the report e-mail", e);
+			/**
+			 * Now, we try to set the last export date
+			 */
+			boolean ok = false;
+			try {
+				this.session.beginTrans();
+				// If this is auto run type of an export instead of an adhoc query export, store
+				// the value of the current export date in the repository. This value will be
+				// looked up in the next run. This is indeed an auto run type of export
+				DctmUtils.setLastExportDate(this.session, exportStart);
+				this.session.commitTrans();
+				ok = true;
+			} finally {
+				try {
+					if (!ok) {
+						this.session.abortTrans();
+					}
+				} catch (DfException e) {
+					this.log.error(
+						"Exception caught while rolling back the transaction for saving the export metadata", e);
+				}
+			}
+			return true;
+		} catch (Exception e) {
+			throw new CMSMFException("Exception caught storing the export metadata", e);
 		}
+	}
+
+	@Override
+	protected void processSettings(Map<String, Object> settings, boolean loaded) throws CMSMFException {
 	}
 
 	private String buildExportPredicate(IDfSession session) {
@@ -243,53 +171,6 @@ public class CMSMFMain_export extends AbstractCMSMFMain<ExportEngineListener, Ex
 				CMSMFMain_export.LAST_EXPORT_DATETIME_PATTERN); }
 		}
 		return predicate;
-	}
-
-	@Override
-	public boolean requiresCleanData() {
-		return true;
-	}
-
-	@Override
-	public void exportStarted(CfgTools config) {
-		this.console.info(String.format("Export process started with settings:%n%n\t%s%n%n", config));
-	}
-
-	@Override
-	public void objectExportStarted(CmfType objectType, String objectId) {
-		this.console.info(String.format("Object export started for %s[%s]", objectType.name(), objectId));
-	}
-
-	@Override
-	public void objectExportCompleted(CmfObject<?> object, Long objectNumber) {
-		if (objectNumber != null) {
-			this.console.info(String.format("%s export completed for [%s](%s) as object #%d", object.getType().name(),
-				object.getLabel(), object.getId(), objectNumber));
-		}
-	}
-
-	@Override
-	public void objectSkipped(CmfType objectType, String objectId, ExportSkipReason reason) {
-		if (reason == ExportSkipReason.SKIPPED) {
-			this.console.info(String.format("%s object [%s] was skipped (%s)", objectType.name(), objectId, reason));
-		}
-	}
-
-	@Override
-	public void objectExportFailed(CmfType objectType, String objectId, Throwable thrown) {
-		this.console.warn(String.format("Object export failed for %s[%s]", objectType.name(), objectId), thrown);
-	}
-
-	@Override
-	public void exportFinished(Map<CmfType, Integer> summary) {
-		this.console.info("Export process finished");
-		for (CmfType t : CmfType.values()) {
-			Integer v = summary.get(t);
-			if ((v == null) || (v.intValue() == 0)) {
-				continue;
-			}
-			this.console.info(String.format("%-16s : %8d", t.name(), v.intValue()));
-		}
 	}
 
 	@Override
--- tools/trunk/src/main/java/com/delta/cmsmf/launcher/local/CMSMFMain_export.java
+++ tools/trunk/src/main/java/com/delta/cmsmf/launcher/local/CMSMFMain_export.java
@@ -2,60 +2,23 @@ package com.delta.cmsmf.launcher.local;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
-import javax.mail.MessagingException;
-
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.time.DateFormatUtils;
-
-import com.armedia.cmf.engine.exporter.ExportEngine;
 import com.armedia.cmf.engine.exporter.ExportEngineListener;
-import com.armedia.cmf.engine.exporter.ExportResult;
-import com.armedia.cmf.engine.exporter.ExportSkipReason;
 import com.armedia.cmf.engine.local.exporter.LocalExportEngine;
 import com.armedia.cmf.engine.tools.LocalOrganizationStrategy;
-import com.armedia.cmf.storage.CmfObject;
-import com.armedia.cmf.storage.CmfType;
-import com.armedia.commons.utilities.CfgTools;
-import com.armedia.commons.utilities.Tools;
 import com.delta.cmsmf.cfg.CLIParam;
-import com.delta.cmsmf.cfg.Setting;
 import com.delta.cmsmf.exception.CMSMFException;
-import com.delta.cmsmf.launcher.AbstractCMSMFMain;
-import com.delta.cmsmf.launcher.ExportManifest;
-import com.delta.cmsmf.utils.CMSMFUtils;
+import com.delta.cmsmf.launcher.AbstractCMSMFMain_export;
 
-public class CMSMFMain_export extends AbstractCMSMFMain<ExportEngineListener, ExportEngine<?, ?, ?, ?, ?, ?>> implements
-	ExportEngineListener {
+public class CMSMFMain_export extends AbstractCMSMFMain_export implements ExportEngineListener {
 
 	public CMSMFMain_export() throws Throwable {
 		super(LocalExportEngine.getExportEngine());
 	}
 
-	/**
-	 * Starts exporting objects from source directory. It reads up the query from properties file
-	 * and executes it against the source repository. It retrieves objects from the repository and
-	 * exports it out.
-	 *
-	 */
 	@Override
-	public void run() throws CMSMFException {
-		Set<ExportResult> outcomes = Tools.parseEnumCSV(ExportResult.class, Setting.MANIFEST_OUTCOMES.getString(),
-			AbstractCMSMFMain.ALL, false);
-		Set<CmfType> types = Tools.parseEnumCSV(CmfType.class, Setting.MANIFEST_TYPES.getString(),
-			AbstractCMSMFMain.ALL, false);
-		this.engine.addListener(this);
-		this.engine.addListener(new ExportManifest(outcomes, types));
-
-		Map<String, Object> settings = new HashMap<String, Object>();
-
+	protected void customizeSettings(Map<String, Object> settings) throws CMSMFException {
 		String srcPath = CLIParam.source.getString();
 		if (srcPath == null) { throw new CMSMFException("Must provide the source path to export from"); }
 
@@ -67,133 +30,6 @@ public class CMSMFMain_export extends AbstractCMSMFMain<ExportEngineListener, Ex
 		}
 
 		settings.put("root", root.getAbsolutePath());
-
-		Date end = null;
-		Map<CmfType, Integer> summary = null;
-		String exceptionReport = null;
-		StringBuilder report = new StringBuilder();
-		Date start = null;
-
-		start = new Date();
-		try {
-			this.log.info("##### Export Process Started #####");
-			this.engine.runExport(this.console, this.cmfObjectStore, this.cmfContentStore, settings);
-			this.log.info("##### Export Process Finished #####");
-
-			summary = this.cmfObjectStore.getStoredObjectTypes();
-		} catch (Throwable t) {
-			StringWriter sw = new StringWriter();
-			PrintWriter pw = new PrintWriter(sw);
-			report.append(String.format("%n%nException caught while attempting an export%n%n"));
-			t.printStackTrace(pw);
-			exceptionReport = sw.toString();
-		} finally {
-			// unlock
-			end = new Date();
-		}
-
-		long duration = (end.getTime() - start.getTime());
-		long hours = TimeUnit.HOURS.convert(duration, TimeUnit.MILLISECONDS);
-		duration -= hours * TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS);
-		long minutes = TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS);
-		duration -= minutes * TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES);
-		long seconds = TimeUnit.SECONDS.convert(duration, TimeUnit.MILLISECONDS);
-		report.append(String.format("Export process start    : %s%n",
-			DateFormatUtils.format(start, AbstractCMSMFMain.JAVA_SQL_DATETIME_PATTERN)));
-		report.append(String.format("Export process end      : %s%n",
-			DateFormatUtils.format(end, AbstractCMSMFMain.JAVA_SQL_DATETIME_PATTERN)));
-		report.append(String.format("Export process duration : %02d:%02d:%02d%n", hours, minutes, seconds));
-
-		report.append(String.format("%n%nParameters in use:%n")).append(StringUtils.repeat("=", 30));
-		for (CLIParam p : CLIParam.values()) {
-			if (!p.isPresent()) {
-				continue;
-			}
-			String v = p.getString();
-			if (v == null) {
-				report.append(String.format("%n\t--%s", p.option.getLongOpt()));
-			} else {
-				report.append(String.format("%n\t--%s = [%s]", p.option.getLongOpt(), v));
-			}
-		}
-
-		report.append(String.format("%n%n%nSettings in use:%n")).append(StringUtils.repeat("=", 30));
-		for (Setting s : Setting.values()) {
-			report.append(String.format("%n\t%s = [%s]", s.name, s.getString()));
-		}
-
-		if (summary != null) {
-			report.append(String.format("%n%n%nExported Object Summary:%n")).append(StringUtils.repeat("=", 30));
-			int total = 0;
-			for (CmfType t : summary.keySet()) {
-				Integer count = summary.get(t);
-				if ((count == null) || (count == 0)) {
-					continue;
-				}
-				report.append(String.format("%n%-16s: %6d", t.name(), count));
-				total += count;
-			}
-			report.append(String.format("%n%s%n%-16s: %6d%n", StringUtils.repeat("=", 30), "Total", total));
-		}
-
-		if (exceptionReport != null) {
-			report.append(String.format("%n%n%nEXCEPTION REPORT FOLLOWS:%n%n")).append(exceptionReport);
-		}
-
-		String reportString = report.toString();
-		this.log.info(String.format("Action report for export operation:%n%n%s%n", reportString));
-		try {
-			CMSMFUtils.postCmsmfMail(String.format("Action report for CMSMF Export"), reportString);
-		} catch (MessagingException e) {
-			this.log.error("Exception caught attempting to send the report e-mail", e);
-		}
-	}
-
-	@Override
-	public boolean requiresCleanData() {
-		return true;
-	}
-
-	@Override
-	public void exportStarted(CfgTools config) {
-		this.console.info(String.format("Export process started with settings:%n%n\t%s%n%n", config));
-	}
-
-	@Override
-	public void objectExportStarted(CmfType objectType, String objectId) {
-		this.console.info(String.format("Object export started for %s[%s]", objectType.name(), objectId));
-	}
-
-	@Override
-	public void objectExportCompleted(CmfObject<?> object, Long objectNumber) {
-		if (objectNumber != null) {
-			this.console.info(String.format("%s export completed for [%s](%s) as object #%d", object.getType().name(),
-				object.getLabel(), object.getId(), objectNumber));
-		}
-	}
-
-	@Override
-	public void objectSkipped(CmfType objectType, String objectId, ExportSkipReason reason) {
-		if (reason == ExportSkipReason.SKIPPED) {
-			this.console.info(String.format("%s object [%s] was skipped (%s)", objectType.name(), objectId, reason));
-		}
-	}
-
-	@Override
-	public void objectExportFailed(CmfType objectType, String objectId, Throwable thrown) {
-		this.console.warn(String.format("Object export failed for %s[%s]", objectType.name(), objectId), thrown);
-	}
-
-	@Override
-	public void exportFinished(Map<CmfType, Integer> summary) {
-		this.console.info("Export process finished");
-		for (CmfType t : CmfType.values()) {
-			Integer v = summary.get(t);
-			if ((v == null) || (v.intValue() == 0)) {
-				continue;
-			}
-			this.console.info(String.format("%-16s : %8d", t.name(), v.intValue()));
-		}
 	}
 
 	@Override
