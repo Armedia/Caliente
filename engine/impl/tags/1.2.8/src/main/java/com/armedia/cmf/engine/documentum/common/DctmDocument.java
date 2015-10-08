91bcd5e8ea14c6f4f32f19f704673fff4f05b3fa
Diego Rivera <diego.rivera@armedia.com>
2015-10-08 16:47:23 +0000

    Release 1.2.8: ================================================================================ * Reduced the number of nested transactions in hopes of reducing the checkpoint problems with Oracle in the Delta environment

---
+++ engine/impl/tags/1.2.8/src/main/java/com/armedia/cmf/engine/documentum/common/DctmDocument.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.engine.documentum.common;
+
+/**
+ * @author diego
+ *
+ */
+public interface DctmDocument extends DctmSysObject {
+
+	static final String CONTENTS = "contents";
+
+	static final String CONTENT_HANDLES = "contentHandles";
+
+}
