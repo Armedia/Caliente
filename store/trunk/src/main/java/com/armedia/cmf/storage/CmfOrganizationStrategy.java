ee35f06f750601f1182ece67823ffc54cc618393
Diego Rivera <diego.rivera@armedia.com>
2016-08-04 18:29:31 +0000

    Modified the structure and algorithm for the use of organization strategies, so that it's simpler and more efficient to use.

--- store/trunk/src/main/java/com/armedia/cmf/storage/CmfOrganizationStrategy.java
+++ store/trunk/src/main/java/com/armedia/cmf/storage/CmfOrganizationStrategy.java
@@ -1,5 +1,6 @@
 package com.armedia.cmf.storage;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,27 +18,104 @@ public abstract class CmfOrganizationStrategy {
 
 	private static final Logger LOG = LoggerFactory.getLogger(CmfOrganizationStrategy.class);
 
+	public static final class Location implements Comparable<Location>, Serializable {
+		private static final long serialVersionUID = 1L;
+
+		public final List<String> containerSpec;
+		public final String baseName;
+		public final String extension;
+		public final String descriptor;
+		public final String appendix;
+		private volatile String string = null;
+
+		private Location(List<String> containerSpec, String baseName, String extension, String descriptor,
+			String appendix) {
+			this.containerSpec = Tools.freezeCopy(containerSpec, true);
+			this.baseName = baseName;
+			this.extension = extension;
+			this.descriptor = descriptor;
+			this.appendix = appendix;
+		}
+
+		@Override
+		public int compareTo(Location o) {
+			if (o == null) { return 1; }
+			int r = 0;
+			r = Tools.compare(this.containerSpec.size(), o.containerSpec.size());
+			if (r != 0) { return r; }
+
+			for (int i = 0; i < this.containerSpec.size(); i++) {
+				r = Tools.compare(this.containerSpec.get(i), o.containerSpec.get(i));
+				if (r != 0) { return r; }
+			}
+
+			r = Tools.compare(this.baseName, o.baseName);
+			if (r != 0) { return r; }
+			r = Tools.compare(this.extension, o.extension);
+			if (r != 0) { return r; }
+			r = Tools.compare(this.descriptor, o.descriptor);
+			if (r != 0) { return r; }
+			r = Tools.compare(this.appendix, o.appendix);
+			if (r != 0) { return r; }
+			return 0;
+		}
+
+		@Override
+		public int hashCode() {
+			return Tools.hashTool(this, null, this.containerSpec, this.baseName, this.extension, this.descriptor,
+				this.appendix);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (!Tools.baseEquals(this, obj)) { return false; }
+			Location other = Location.class.cast(obj);
+			if (!Tools.equals(this.containerSpec, other.containerSpec)) { return false; }
+			if (!Tools.equals(this.baseName, other.baseName)) { return false; }
+			if (!Tools.equals(this.extension, other.extension)) { return false; }
+			if (!Tools.equals(this.descriptor, other.descriptor)) { return false; }
+			if (!Tools.equals(this.appendix, other.appendix)) { return false; }
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			if (this.string == null) {
+				synchronized (this) {
+					if (this.string == null) {
+						final String containerSpec;
+						StringBuilder b = new StringBuilder();
+						for (String s : this.containerSpec) {
+							if (b.length() > 0) {
+								b.append('/');
+							}
+							b.append(s);
+						}
+						containerSpec = b.toString();
+						final String baseName = (!StringUtils.isEmpty(this.baseName) ? this.baseName : "");
+						final String descriptor = (!StringUtils.isEmpty(this.descriptor)
+							? String.format("[%s]", this.descriptor) : "");
+						final String extension = (!StringUtils.isEmpty(this.extension)
+							? String.format(".%s", this.extension) : "");
+						final String appendix = (!StringUtils.isEmpty(this.appendix)
+							? String.format(".%s", this.appendix) : "");
+						return String.format("%s/%s%s%s%s", containerSpec, baseName, descriptor, extension, appendix);
+					}
+				}
+			}
+			return this.string;
+		}
+	}
+
 	private static final CmfOrganizationStrategy DEFAULT_STRATEGY = new CmfOrganizationStrategy() {
 
 		@Override
-		public <T> List<String> calculatePath(CmfAttributeTranslator<T> translator, CmfObject<T> object,
+		public <T> Location calculateLocation(CmfAttributeTranslator<T> translator, CmfObject<T> object,
 			CmfContentInfo info) {
-			List<String> ssp = new ArrayList<String>(3);
-			ssp.add(object.getType().name());
-			ssp.add(object.getSubtype());
-			return ssp;
-		}
-
-		@Override
-		protected <T> String calculateBaseName(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-			CmfContentInfo info) {
-			return object.getId();
-		}
-
-		@Override
-		protected <T> String calculateAppendix(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-			CmfContentInfo info) {
-			return "";
+			List<String> path = new ArrayList<String>(3);
+			path.add(object.getType().name());
+			path.add(object.getSubtype());
+			return newLocation(path, object.getId(), null, null, null);
 		}
 	};
 
@@ -99,64 +177,19 @@ public abstract class CmfOrganizationStrategy {
 		return this.name;
 	}
 
-	protected <T> String calculateDescriptor(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-		CmfContentInfo info) {
-		return String.format("%s.%08x", info.getRenditionIdentifier(), info.getRenditionPage());
-	}
-
-	public final <T> String getDescriptor(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-		CmfContentInfo info) {
-		if (translator == null) { throw new IllegalArgumentException("Must provide an attribute translator"); }
-		if (object == null) { throw new IllegalArgumentException("Must provide a CMF object"); }
-		if (info == null) { throw new IllegalArgumentException("Must provide a valid Content Information object"); }
-		return calculateDescriptor(translator, object, info);
-	}
-
-	protected abstract <T> List<String> calculatePath(CmfAttributeTranslator<T> translator, CmfObject<T> object,
+	protected abstract <T> Location calculateLocation(CmfAttributeTranslator<T> translator, CmfObject<T> object,
 		CmfContentInfo info);
 
-	public final <T> List<String> getPath(CmfAttributeTranslator<T> translator, CmfObject<T> object,
+	public final <T> Location getLocation(CmfAttributeTranslator<T> translator, CmfObject<T> object,
 		CmfContentInfo info) {
 		if (translator == null) { throw new IllegalArgumentException("Must provide an attribute translator"); }
 		if (object == null) { throw new IllegalArgumentException("Must provide a CMF object"); }
 		if (info == null) { throw new IllegalArgumentException("Must provide a valid Content Information object"); }
-		return calculatePath(translator, object, info);
+		return calculateLocation(translator, object, info);
 	}
 
-	protected abstract <T> String calculateBaseName(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-		CmfContentInfo info);
-
-	public final <T> String getBaseName(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-		CmfContentInfo info) {
-		if (translator == null) { throw new IllegalArgumentException("Must provide an attribute translator"); }
-		if (object == null) { throw new IllegalArgumentException("Must provide a CMF object"); }
-		if (info == null) { throw new IllegalArgumentException("Must provide a valid Content Information object"); }
-		return calculateBaseName(translator, object, info);
-	}
-
-	protected <T> String calculateExtension(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-		CmfContentInfo info) {
-		return StringUtils.isEmpty(info.getExtension()) ? "" : String.format(".%s", info.getExtension());
-	}
-
-	public final <T> String getExtension(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-		CmfContentInfo info) {
-		if (translator == null) { throw new IllegalArgumentException("Must provide an attribute translator"); }
-		if (object == null) { throw new IllegalArgumentException("Must provide a CMF object"); }
-		if (info == null) { throw new IllegalArgumentException("Must provide a valid Content Information object"); }
-		return calculateExtension(translator, object, info);
-	}
-
-	protected <T> String calculateAppendix(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-		CmfContentInfo info) {
-		return null;
-	}
-
-	public final <T> String getAppendix(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-		CmfContentInfo info) {
-		if (translator == null) { throw new IllegalArgumentException("Must provide an attribute translator"); }
-		if (object == null) { throw new IllegalArgumentException("Must provide a CMF object"); }
-		if (info == null) { throw new IllegalArgumentException("Must provide a valid Content Information object"); }
-		return calculateAppendix(translator, object, info);
+	protected final Location newLocation(List<String> containerSpec, String baseName, String extension,
+		String descriptor, String appendix) {
+		return new Location(containerSpec, baseName, extension, descriptor, appendix);
 	}
 }
