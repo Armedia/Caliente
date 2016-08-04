835744d22a5e37887b551a4adf7509d714326b91
Diego Rivera <diego.rivera@armedia.com>
2016-08-04 18:29:28 +0000

    Modified the structure and algorithm for the use of organization strategies, so that it's simpler and more efficient to use.

--- engine/impl/trunk/src/main/java/com/armedia/cmf/engine/alfresco/bulk/common/AlfrescoBulkOrganizationStrategy.java
+++ engine/impl/trunk/src/main/java/com/armedia/cmf/engine/alfresco/bulk/common/AlfrescoBulkOrganizationStrategy.java
@@ -10,7 +10,6 @@ import com.armedia.cmf.storage.CmfAttributeTranslator;
 import com.armedia.cmf.storage.CmfContentInfo;
 import com.armedia.cmf.storage.CmfObject;
 import com.armedia.cmf.storage.CmfProperty;
-import com.armedia.cmf.storage.CmfValue;
 import com.armedia.commons.utilities.FileNameTools;
 
 public class AlfrescoBulkOrganizationStrategy extends LocalOrganizationStrategy {
@@ -26,75 +25,47 @@ public class AlfrescoBulkOrganizationStrategy extends LocalOrganizationStrategy 
 	}
 
 	@Override
-	protected <T> List<String> calculatePath(CmfAttributeTranslator<T> translator, CmfObject<T> object,
+	protected <T> Location calculateLocation(CmfAttributeTranslator<T> translator, CmfObject<T> object,
 		CmfContentInfo info) {
-		CmfProperty<T> paths = object.getProperty(IntermediateProperty.PARENT_TREE_IDS);
-		if (paths == null) { return super.calculatePath(translator, object, info); }
+		CmfProperty<T> pathProp = object.getProperty(IntermediateProperty.PARENT_TREE_IDS);
+		if (pathProp == null) { return super.calculateLocation(translator, object, info); }
 
-		List<String> ret = new ArrayList<String>();
-		for (String p : FileNameTools.tokenize(paths.getValue().toString(), '/')) {
-			ret.add(p);
+		final boolean primaryContent = (info.isDefaultRendition() && (info.getRenditionPage() == 0));
+
+		List<String> paths = new ArrayList<String>();
+		for (String p : FileNameTools.tokenize(pathProp.getValue().toString(), '/')) {
+			paths.add(p);
 		}
 
-		if (!info.isDefaultRendition() || (info.getRenditionPage() > 0)) {
+		if (!primaryContent) {
 			// Ok...so this isn't the default rendition, so we have to add the object ID
 			// at the end of the path
-			ret.add(String.format("%s-renditions", object.getId()));
-			ret.add(String.format("rendition-[%s]", info.getRenditionIdentifier()));
+			paths.add(String.format("%s-renditions", object.getId()));
+			paths.add(String.format("rendition-[%s]", info.getRenditionIdentifier()));
 		}
 
-		return ret;
-	}
-
-	@Override
-	protected <T> String calculateBaseName(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-		CmfContentInfo info) {
-		CmfProperty<T> paths = object.getProperty(IntermediateProperty.PARENT_TREE_IDS);
-		if (paths == null) { return super.calculateBaseName(translator, object, info); }
+		String baseName = object.getId();
 		switch (object.getType()) {
 			case DOCUMENT:
-				String baseName = object.getBatchId();
-				if (!info.isDefaultRendition() || (info.getRenditionPage() > 0)) {
+				baseName = object.getBatchId();
+				if (!primaryContent) {
 					baseName = String.format("page-%08x", info.getRenditionPage());
 				}
-				return baseName;
 			default:
-				return object.getId();
+				break;
 		}
-	}
-
-	@Override
-	protected <T> String calculateExtension(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-		CmfContentInfo info) {
-		return null;
-	}
-
-	@Override
-	protected <T> String calculateDescriptor(CmfAttributeTranslator<T> translator, CmfObject<T> object,
-		CmfContentInfo info) {
-		return null;
+		return newLocation(paths, baseName, null, null, calculateAppendix(translator, object, info));
 	}
 
 	@Override
 	protected <T> String calculateAppendix(CmfAttributeTranslator<T> translator, CmfObject<T> object,
 		CmfContentInfo info) {
 		CmfProperty<T> prop = object
-			.getAttribute(translator.decodeAttributeName(object.getType(), IntermediateAttribute.IS_LATEST_VERSION));
-		if (prop != null) {
-			CmfValue v = translator.getCodec(prop.getType()).encodeValue(prop.getValue());
-			if (!v.asBoolean()) {
-				// Not the latest version, so we have to append an appendix...which is
-				// a version number or a sequential number
-				prop = object.getAttribute(
-					translator.decodeAttributeName(object.getType(), IntermediateAttribute.VERSION_LABEL));
-				if ((prop != null) && prop.hasValues()) {
-					final String versionString = translator.getCodec(prop.getType()).encodeValue(prop.getValue())
-						.asString();
-					// TODO: The version string MUST be converted into something
-					// "sequential-ish" that
-					return String.format("v%s", versionString);
-				}
-			}
+			.getAttribute(translator.decodeAttributeName(object.getType(), IntermediateAttribute.VERSION_LABEL));
+		if ((prop != null) && prop.hasValues()) {
+			// TODO: Sequential numbers? How?
+			final String versionString = translator.getCodec(prop.getType()).encodeValue(prop.getValue()).asString();
+			return String.format("v%s", versionString);
 		}
 		return "";
 	}
