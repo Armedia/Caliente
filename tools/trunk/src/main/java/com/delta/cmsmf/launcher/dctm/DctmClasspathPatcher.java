35828344848d3c4196accde3d75df4b268add7c3
Diego Rivera <diego.rivera@armedia.com>
2016-08-29 21:30:18 +0000

    Added code to avoid checking for a DFC install if the DFC is already integrated in the package, or in the classpath.

--- tools/trunk/src/main/java/com/delta/cmsmf/launcher/dctm/DctmClasspathPatcher.java
+++ tools/trunk/src/main/java/com/delta/cmsmf/launcher/dctm/DctmClasspathPatcher.java
@@ -16,6 +16,7 @@ public class DctmClasspathPatcher extends ClasspathPatcher {
 	protected static final String ENV_DOCUMENTUM_SHARED = "DOCUMENTUM_SHARED";
 	protected static final String ENV_DOCUMENTUM = "DOCUMENTUM";
 	protected static final String DCTM_JAR = "dctm.jar";
+	protected static final String DFC_TEST_CLASS = "com.documentum.fc.client.IDfFolder";
 
 	public DctmClasspathPatcher() {
 		super("dctm");
@@ -23,23 +24,24 @@ public class DctmClasspathPatcher extends ClasspathPatcher {
 
 	@Override
 	public List<URL> getPatches(String engine) {
+		final boolean dfcFound;
+		{
+			boolean dfc = false;
+			try {
+				Class.forName(DctmClasspathPatcher.DFC_TEST_CLASS);
+				dfc = true;
+			} catch (Exception e) {
+				dfc = false;
+			}
+			dfcFound = dfc;
+		}
+
 		List<URL> ret = new ArrayList<URL>(3);
 		try {
 
 			String var = System.getProperty("user.dir");
 			File f = new File(var);
 			ret.add(f.toURI().toURL());
-
-			// Next, add ${DOCUMENTUM}/config to the classpath
-			var = CLIParam.dctm.getString(System.getenv(DctmClasspathPatcher.ENV_DOCUMENTUM));
-			// Go with the environment
-			if (var == null) { throw new RuntimeException(String.format("The environment variable [%s] is not set",
-				DctmClasspathPatcher.ENV_DOCUMENTUM)); }
-
-			f = new File(var).getCanonicalFile();
-			if (!f.isDirectory()) { throw new FileNotFoundException(String.format("Could not find the directory [%s]",
-				f.getAbsolutePath())); }
-			ret.add(new File(f, "config").toURI().toURL());
 
 			var = CLIParam.dfc_prop.getString();
 			if (var != null) {
@@ -49,6 +51,20 @@ public class DctmClasspathPatcher extends ClasspathPatcher {
 				}
 			}
 
+			if (dfcFound) { return ret; }
+
+			// Next, add ${DOCUMENTUM}/config to the classpath
+			var = CLIParam.dctm.getString(System.getenv(DctmClasspathPatcher.ENV_DOCUMENTUM));
+			// Go with the environment
+			if (var == null) { throw new RuntimeException(
+				String.format("The environment variable [%s] is not set", DctmClasspathPatcher.ENV_DOCUMENTUM)); }
+
+			f = new File(var).getCanonicalFile();
+			if (!f.isDirectory()) { throw new FileNotFoundException(
+				String.format("Could not find the directory [%s]", f.getAbsolutePath())); }
+
+			ret.add(new File(f, "config").toURI().toURL());
+
 			// Next, identify the DOCUMENTUM_SHARED location, and if dctm.jar is in there
 			var = CLIParam.dfc.getString(System.getenv(DctmClasspathPatcher.ENV_DOCUMENTUM_SHARED));
 			// Go with the environment
@@ -57,20 +73,20 @@ public class DctmClasspathPatcher extends ClasspathPatcher {
 
 			// Next, is it a directory?
 			f = new File(var).getCanonicalFile();
-			if (!f.isDirectory()) { throw new FileNotFoundException(String.format(
-				"Could not find the [%s] directory [%s]", DctmClasspathPatcher.ENV_DOCUMENTUM_SHARED,
-				f.getAbsolutePath())); }
+			if (!f.isDirectory()) { throw new FileNotFoundException(
+				String.format("Could not find the [%s] directory [%s]", DctmClasspathPatcher.ENV_DOCUMENTUM_SHARED,
+					f.getAbsolutePath())); }
 
 			// Next, does dctm.jar exist in there?
 			File tgt = new File(f, DctmClasspathPatcher.DCTM_JAR);
-			if (!tgt.isFile()) { throw new FileNotFoundException(String.format("Could not find the JAR file [%s]",
-				tgt.getAbsolutePath())); }
+			if (!tgt.isFile()) { throw new FileNotFoundException(
+				String.format("Could not find the JAR file [%s]", tgt.getAbsolutePath())); }
 
 			// Next, to the classpath
 			ret.add(tgt.toURI().toURL());
 		} catch (IOException e) {
-			throw new RuntimeException(String.format("Failed to configure the dynamic classpath for engine [%s]",
-				engine), e);
+			throw new RuntimeException(
+				String.format("Failed to configure the dynamic classpath for engine [%s]", engine), e);
 		}
 		return ret;
 	}
