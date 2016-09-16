7e08ba8b392a80767dee3db0f62c8427f0559cac
Diego Rivera <diego.rivera@armedia.com>
2016-09-16 15:12:47 +0000

    Release 1.0.0: ================================================================================ * Final release.

---
+++ tools/tags/1.0.0/src/main/java/com/armedia/cmf/usermapper/CommonClasspathPatcher.java
@@ -0,0 +1,50 @@
+package com.armedia.cmf.usermapper;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommonClasspathPatcher extends ClasspathPatcher {
+
+	private static final FilenameFilter LIB_FILTER = new FilenameFilter() {
+		@Override
+		public boolean accept(File dir, String name) {
+			name = name.toLowerCase();
+			return name.endsWith(".jar") || name.endsWith(".zip");
+		}
+	};
+
+	public CommonClasspathPatcher() {
+		super();
+	}
+
+	@Override
+	public List<URL> getPatches() {
+		List<URL> ret = new ArrayList<URL>(3);
+		try {
+			// First, add the ${PWD}/cfg directory to the classpath - whether it exists or not
+			final File userDir = new File(System.getProperty("user.dir"));
+
+			// Add all jar/zip files in ${user.dir}/cmsmf.lib, and ${user.dir}/cmsmf.lib/classes
+			// to the classpath
+			File libDir = new File(userDir, "cmsmf.lib");
+			if (libDir.exists() && libDir.isDirectory()) {
+				ret.add(new File(libDir, "classes").toURI().toURL());
+				File[] files = libDir.listFiles(CommonClasspathPatcher.LIB_FILTER);
+				if (files != null) {
+					for (File f : files) {
+						ret.add(f.toURI().toURL());
+					}
+				}
+			}
+			ret.add(new File(userDir, "cfg").toURI().toURL());
+			ret.add(userDir.toURI().toURL());
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to configure the dynamic classpath", e);
+		}
+		return ret;
+	}
+}
---
+++ tools/tags/1.0.0/src/main/java/com/armedia/cmf/usermapper/DctmClasspathPatcher.java
@@ -0,0 +1,106 @@
+package com.armedia.cmf.usermapper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DctmClasspathPatcher extends ClasspathPatcher {
+
+	protected static final String DFC_PROPERTIES_PROP = "dfc.properties.file";
+	protected static final String ENV_DOCUMENTUM_SHARED = "DOCUMENTUM_SHARED";
+	protected static final String ENV_DOCUMENTUM = "DOCUMENTUM";
+	protected static final String DCTM_JAR = "dctm.jar";
+	protected static final String DFC_TEST_CLASS = "com.documentum.fc.client.IDfFolder";
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	public DctmClasspathPatcher() {
+		super();
+	}
+
+	@Override
+	public List<URL> getPatches() {
+		final boolean dfcFound;
+		{
+			boolean dfc = false;
+			try {
+				Class.forName(DctmClasspathPatcher.DFC_TEST_CLASS);
+				dfc = true;
+			} catch (Exception e) {
+				dfc = false;
+			}
+			dfcFound = dfc;
+		}
+
+		List<URL> ret = new ArrayList<URL>(3);
+		try {
+
+			String var = System.getProperty("user.dir");
+			File f = new File(var);
+			ret.add(f.toURI().toURL());
+
+			var = CLIParam.dfc_prop.getString();
+			if (var != null) {
+				f = new File(var);
+				if (f.exists() && f.isFile() && f.canRead()) {
+					System.setProperty(DctmClasspathPatcher.DFC_PROPERTIES_PROP, f.getCanonicalPath());
+				}
+			}
+
+			// Next, add ${DOCUMENTUM}/config to the classpath
+			var = CLIParam.dctm.getString(System.getenv(DctmClasspathPatcher.ENV_DOCUMENTUM));
+			// Go with the environment
+			if (var == null) {
+				String msg = String.format("The environment variable [%s] is not set",
+					DctmClasspathPatcher.ENV_DOCUMENTUM);
+				if (!dfcFound) { throw new RuntimeException(msg); }
+				this.log.warn("{}, integrated DFC may encounter errors", msg);
+			}
+
+			if (var != null) {
+				f = new File(var).getCanonicalFile();
+				if (!f.isDirectory()) { throw new FileNotFoundException(
+					String.format("Could not find the directory [%s]", f.getAbsolutePath())); }
+
+				ret.add(new File(f, "config").toURI().toURL());
+			}
+
+			// Next, identify the DOCUMENTUM_SHARED location, and if dctm.jar is in there
+			var = CLIParam.dfc.getString(System.getenv(DctmClasspathPatcher.ENV_DOCUMENTUM_SHARED));
+			// Go with the environment
+			if (var == null) {
+				String msg = String.format("The environment variable [%s] is not set",
+					DctmClasspathPatcher.ENV_DOCUMENTUM_SHARED);
+				if (!dfcFound) { throw new RuntimeException(msg); }
+				this.log.warn("{}, integrated DFC may encounter errors", msg);
+			}
+
+			if (var != null) {
+				// Next, is it a directory?
+				f = new File(var).getCanonicalFile();
+				if (!f.isDirectory()) { throw new FileNotFoundException(
+					String.format("Could not find the [%s] directory [%s]", DctmClasspathPatcher.ENV_DOCUMENTUM_SHARED,
+						f.getAbsolutePath())); }
+
+				// Next, does dctm.jar exist in there?
+				if (!dfcFound) {
+					File tgt = new File(f, DctmClasspathPatcher.DCTM_JAR);
+					if (!tgt.isFile()) { throw new FileNotFoundException(
+						String.format("Could not find the JAR file [%s]", tgt.getAbsolutePath())); }
+
+					// Next, to the classpath
+					ret.add(tgt.toURI().toURL());
+				}
+			}
+			return ret;
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to configure the dynamic classpath for Documentum support", e);
+		}
+	}
+}
