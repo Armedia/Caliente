c107db8381ad736b663dbf0e2b82193a85e9be6e
Diego Rivera <diego.rivera@armedia.com>
2016-09-30 16:05:38 +0000

    Version used in Caliente beta51

---
+++ engine/tags/cmsmf-beta-51/src/main/java/com/armedia/cmf/engine/importer/ImportDelegate.java
@@ -0,0 +1,26 @@
+package com.armedia.cmf.engine.importer;
+
+import java.util.Collection;
+
+import com.armedia.cmf.engine.SessionWrapper;
+import com.armedia.cmf.engine.TransferDelegate;
+import com.armedia.cmf.storage.CmfAttributeTranslator;
+import com.armedia.cmf.storage.CmfObject;
+import com.armedia.cmf.storage.CmfStorageException;
+
+public abstract class ImportDelegate<T, S, W extends SessionWrapper<S>, V, C extends ImportContext<S, V, ?>, DF extends ImportDelegateFactory<S, W, V, C, E>, E extends ImportEngine<S, W, V, C, ?, DF>>
+	extends TransferDelegate<T, S, V, C, DF, E> {
+
+	protected final CmfObject<V> cmfObject;
+	protected final ImportStrategy strategy;
+
+	protected ImportDelegate(DF factory, Class<T> objectClass, CmfObject<V> storedObject) throws Exception {
+		super(factory, objectClass);
+		this.cmfObject = storedObject;
+		this.strategy = factory.getEngine().getImportStrategy(storedObject.getType());
+	}
+
+	protected abstract Collection<ImportOutcome> importObject(CmfAttributeTranslator<V> translator, C ctx)
+		throws ImportException, CmfStorageException;
+
+}
---
+++ engine/tags/cmsmf-beta-51/src/main/java/com/armedia/cmf/engine/importer/ImportOutcome.java
@@ -0,0 +1,46 @@
+package com.armedia.cmf.engine.importer;
+
+public class ImportOutcome {
+
+	public static final ImportOutcome SKIPPED = new ImportOutcome(ImportResult.SKIPPED);
+	public static final ImportOutcome FAILED = new ImportOutcome(ImportResult.FAILED);
+	public static final ImportOutcome IGNORED = new ImportOutcome(ImportResult.IGNORED);
+	public static final ImportOutcome DUPLICATE = new ImportOutcome(ImportResult.DUPLICATE);
+
+	private final ImportResult result;
+	private final String newId;
+	private final String newLabel;
+
+	private ImportOutcome(ImportResult result) {
+		this(result, null, null);
+	}
+
+	/**
+	 * @param result
+	 * @param newId
+	 * @param newLabel
+	 */
+	public ImportOutcome(ImportResult result, String newId, String newLabel) {
+		this.result = result;
+		this.newId = newId;
+		this.newLabel = newLabel;
+	}
+
+	public final ImportResult getResult() {
+		return this.result;
+	}
+
+	public final String getNewId() {
+		return this.newId;
+	}
+
+	public final String getNewLabel() {
+		return this.newLabel;
+	}
+
+	@Override
+	public String toString() {
+		return String
+			.format("ImportOutcome [result=%s, newId=%s, newLabel=%s]", this.result, this.newId, this.newLabel);
+	}
+}
