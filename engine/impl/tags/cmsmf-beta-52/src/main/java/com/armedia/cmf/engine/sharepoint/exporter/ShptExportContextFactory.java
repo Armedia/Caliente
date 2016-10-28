23442dc4205f2d03bad39b737b004c7a82a275f9
Diego Rivera <diego.rivera@armedia.com>
2016-10-28 14:32:05 +0000

    Version used in Caliente beta52

---
+++ engine/impl/tags/cmsmf-beta-52/src/main/java/com/armedia/cmf/engine/sharepoint/exporter/ShptExportContextFactory.java
@@ -0,0 +1,46 @@
+/**
+ *
+ */
+
+package com.armedia.cmf.engine.sharepoint.exporter;
+
+import org.slf4j.Logger;
+
+import com.armedia.cmf.engine.exporter.ExportContextFactory;
+import com.armedia.cmf.engine.sharepoint.ShptSession;
+import com.armedia.cmf.engine.sharepoint.ShptSessionWrapper;
+import com.armedia.cmf.storage.CmfContentStore;
+import com.armedia.cmf.storage.CmfObjectStore;
+import com.armedia.cmf.storage.CmfType;
+import com.armedia.cmf.storage.CmfTypeMapper;
+import com.armedia.cmf.storage.CmfValue;
+import com.armedia.commons.utilities.CfgTools;
+
+/**
+ * @author diego
+ *
+ */
+public class ShptExportContextFactory
+	extends ExportContextFactory<ShptSession, ShptSessionWrapper, CmfValue, ShptExportContext, ShptExportEngine> {
+
+	ShptExportContextFactory(ShptExportEngine engine, CfgTools settings, ShptSession session) throws Exception {
+		super(engine, settings, session);
+	}
+
+	@Override
+	protected ShptExportContext constructContext(String rootId, CmfType rootType, ShptSession session, Logger output,
+		CmfObjectStore<?, ?> objectStore, CmfContentStore<?, ?, ?> contentStore, CmfTypeMapper typeMapper,
+		int batchPosition) {
+		return new ShptExportContext(this, getSettings(), rootId, rootType, session, output);
+	}
+
+	@Override
+	protected String calculateProductName(ShptSession session) throws Exception {
+		return "Sharepoint";
+	}
+
+	@Override
+	protected String calculateProductVersion(ShptSession session) throws Exception {
+		return session.getContextInfo().getLibraryVersion();
+	}
+}
---
+++ engine/impl/tags/cmsmf-beta-52/src/main/java/com/armedia/cmf/engine/sharepoint/exporter/ShptExportDelegateFactory.java
@@ -0,0 +1,44 @@
+package com.armedia.cmf.engine.sharepoint.exporter;
+
+import com.armedia.cmf.engine.exporter.ExportDelegateFactory;
+import com.armedia.cmf.engine.sharepoint.ShptSetting;
+import com.armedia.cmf.engine.sharepoint.ShptSession;
+import com.armedia.cmf.engine.sharepoint.ShptSessionWrapper;
+import com.armedia.cmf.storage.CmfType;
+import com.armedia.cmf.storage.CmfValue;
+import com.armedia.commons.utilities.CfgTools;
+import com.armedia.commons.utilities.Tools;
+
+public class ShptExportDelegateFactory
+	extends ExportDelegateFactory<ShptSession, ShptSessionWrapper, CmfValue, ShptExportContext, ShptExportEngine> {
+
+	protected ShptExportDelegateFactory(ShptExportEngine engine, CfgTools configuration) {
+		super(engine, configuration);
+	}
+
+	String getRelativePath(String relativeUrl) {
+		String exportPath = String.format("%s/", getConfiguration().getString(ShptSetting.PATH));
+		if (relativeUrl.startsWith(exportPath)) {
+			// The -1 is to account for the leading slash we want to preserve
+			relativeUrl = relativeUrl.substring(exportPath.length() - 1);
+		}
+		return relativeUrl;
+	}
+
+	@Override
+	protected ShptExportDelegate<?> newExportDelegate(ShptSession session, CmfType type, String searchKey)
+		throws Exception {
+		switch (type) {
+			case USER:
+				return new ShptUser(this, session.getUser(Tools.decodeInteger(searchKey)));
+			case GROUP:
+				return new ShptGroup(this, session.getGroup(Tools.decodeInteger(searchKey)));
+			case FOLDER:
+				return new ShptFolder(this, session.getFolder(searchKey));
+			case DOCUMENT:
+				return ShptFile.locateFile(this, session, searchKey);
+			default:
+				throw new Exception(String.format("Unsupported object type [%s]", type));
+		}
+	}
+}
