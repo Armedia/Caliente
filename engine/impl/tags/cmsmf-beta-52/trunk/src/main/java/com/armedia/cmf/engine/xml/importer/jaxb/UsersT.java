e85bb49cdae48a9fa45cd58ad61a338e0f959a46
Diego Rivera <diego.rivera@armedia.com>
2016-11-01 22:14:55 +0000

    Version used in Caliente beta53

---
+++ engine/impl/tags/cmsmf-beta-52/trunk/src/main/java/com/armedia/cmf/engine/xml/importer/jaxb/UsersT.java
@@ -0,0 +1,23 @@
+package com.armedia.cmf.engine.xml.importer.jaxb;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "users")
+@XmlType(name = "users.t", propOrder = {
+	"user"
+})
+public class UsersT extends AggregatorBase<UserT> {
+
+	public UsersT() {
+		super("user");
+	}
+
+	@XmlElement(name = "user")
+	public List<UserT> getUser() {
+		return getItems();
+	}
+}
