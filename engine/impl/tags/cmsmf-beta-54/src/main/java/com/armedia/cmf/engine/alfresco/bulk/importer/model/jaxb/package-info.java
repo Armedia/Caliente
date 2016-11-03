d71f94186c35864c680a2c87131aee9ce657dd2b
Diego Rivera <diego.rivera@armedia.com>
2016-11-03 05:21:51 +0000

    Bad tag, retagging

--- engine/impl/tags/cmsmf-beta-54/src/main/java/com/armedia/cmf/engine/alfresco/bulk/importer/model/jaxb/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = com.armedia.cmf.engine.alfresco.bulk.importer.model.jaxb.ObjectFactory.NAMESPACE, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.armedia.cmf.engine.alfresco.bulk.importer.model.jaxb;
