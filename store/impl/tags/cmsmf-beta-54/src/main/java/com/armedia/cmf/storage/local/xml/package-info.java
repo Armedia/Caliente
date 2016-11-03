47da726f824b8b499ec3825bac1080fd89c66fe6
Diego Rivera <diego.rivera@armedia.com>
2016-11-03 05:05:37 +0000

    Version used in Caliente beta54

---
+++ store/impl/tags/cmsmf-beta-54/src/main/java/com/armedia/cmf/storage/local/xml/package-info.java
@@ -0,0 +1,3 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = ObjectFactory.NAMESPACE, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.armedia.cmf.storage.local.xml;
+
