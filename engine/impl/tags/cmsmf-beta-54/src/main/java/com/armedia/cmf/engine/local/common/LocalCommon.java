46bd5bc488f9fe882a07b209e9a05fc28941d1ce
Diego Rivera <diego.rivera@armedia.com>
2016-11-03 05:05:55 +0000

    Version used in Caliente beta54

---
+++ engine/impl/tags/cmsmf-beta-54/src/main/java/com/armedia/cmf/engine/local/common/LocalCommon.java
@@ -0,0 +1,23 @@
+package com.armedia.cmf.engine.local.common;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+
+import com.armedia.commons.utilities.CfgTools;
+
+public final class LocalCommon {
+	public static final String TARGET_NAME = "local";
+	public static final Set<String> TARGETS = Collections.singleton(LocalCommon.TARGET_NAME);
+
+	private LocalCommon() {
+
+	}
+
+	public static File getRootDirectory(CfgTools cfg) throws IOException {
+		String root = cfg.getString(LocalSetting.ROOT);
+		if (root == null) { return null; }
+		return new File(root).getCanonicalFile();
+	}
+}
---
+++ engine/impl/tags/cmsmf-beta-54/src/main/java/com/armedia/cmf/engine/local/common/LocalProperties.java
@@ -0,0 +1,17 @@
+package com.armedia.cmf.engine.local.common;
+
+public enum LocalProperties {
+	//
+	TARGET_PATHS,
+	TARGET_PARENTS,
+	CONTENTS,
+	CURRENT_VERSION,
+	//
+	;
+
+	public final String name;
+
+	private LocalProperties() {
+		this.name = name().toLowerCase();
+	}
+}
