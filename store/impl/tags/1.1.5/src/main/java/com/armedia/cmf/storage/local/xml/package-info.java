10740b04a78465d647cb92325a7e0a696192dbb2
Diego Rivera <diego.rivera@armedia.com>
2016-11-07 23:20:25 +0000

    Repository Unification: Ingested store/impl/local

--- store/impl/tags/1.1.5/src/main/java/com/armedia/cmf/storage/local/xml/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = ObjectFactory.NAMESPACE, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.armedia.cmf.storage.local.xml;
-
