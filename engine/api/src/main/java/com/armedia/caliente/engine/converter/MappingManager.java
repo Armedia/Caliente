e41cca54b22b38e72552a0b1de95e430c9945764
Diego Rivera <diego.rivera@armedia.com>
2016-11-08 18:24:25 +0000

    Change merge from root branch

--- engine/api/src/main/java/com/armedia/caliente/engine/converter/MappingManager.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/converter/MappingManager.java
@@ -18,14 +18,15 @@ public final class MappingManager {
 		return String.format("cmf:%s", name.toLowerCase());
 	}
 
+	@SafeVarargs
 	public static <E extends Mappable> Map<String, E> createMappings(Class<E> mappableClass, E... mappables) {
-		Map<String, E> m = new HashMap<String, E>();
+		Map<String, E> m = new HashMap<>();
 		if (mappables != null) {
 			for (E a : mappables) {
 				E b = m.put(a.getMapping(), a);
-				if (b != null) { throw new IllegalStateException(String.format(
-					"Duplicate mappings (%s) - %s and %s resolve to the same mapping name (%s|%s)",
-					mappableClass.getCanonicalName(), a, b, a.getMapping(), b.getMapping())); }
+				if (b != null) { throw new IllegalStateException(
+					String.format("Duplicate mappings (%s) - %s and %s resolve to the same mapping name (%s|%s)",
+						mappableClass.getCanonicalName(), a, b, a.getMapping(), b.getMapping())); }
 			}
 		}
 		return m;
