7b491ba1508c37e79f482e41084b92615fd60cf6
Diego Rivera <diego.rivera@armedia.com>
2017-03-08 03:12:51 +0000

    Added the beginnings of a bulk deleter, to easily clean up large portions of a repository

---
+++ cli/bulkdel/src/main/java/com/armedia/caliente/cli/bulkdel/CLIParam.java
@@ -0,0 +1,70 @@
+package com.armedia.caliente.cli.bulkdel;
+
+import java.util.Arrays;
+
+import com.armedia.caliente.cli.parser.MutableParameter;
+import com.armedia.caliente.cli.parser.Parameter;
+import com.armedia.caliente.cli.parser.ParameterWrapper;
+
+public enum CLIParam implements ParameterWrapper {
+	debug(new MutableParameter() //
+		.setDescription("Enable increased logging for debugging") //
+	), //
+	abort_on_error(new MutableParameter() //
+		.setDescription("Instead of logging and ignoring errors, abort the operation") //
+	), //
+	recursive(new MutableParameter() //
+		.setDescription("Delete all folder children, recursively") //
+	), //
+	delete_all_children(new MutableParameter() //
+		.setDescription("Delete folder children that may also reside in folders that aren't part of the deletion set") //
+	), //
+	delete_vdoc_children(new MutableParameter() //
+		.setDescription("Delete vdoc children that may reside in folders that aren't part of the deletion set") //
+	), //
+	delete_referenced(new MutableParameter() //
+		.setDescription(
+			"If any references are encountered, delete the referenced object instead of the reference itself (default is to delete just the reference)") //
+	), //
+	delete_versions(new MutableParameter() //
+		.setMinValueCount(1) //
+		.setMaxValueCount(1) //
+		.setValueName("mode") //
+		.setAllowedValues(Arrays.asList("selected", "unused", "all"))
+		.setDescription("Select which versions of the objects to delete (default = all)") //
+	), //
+	answer(new MutableParameter() //
+		.setMinValueCount(1) //
+		.setMaxValueCount(1) //
+		.setValueName("answer") //
+		.setAllowedValues(Arrays.asList("yes", "no", "abort", "ask"))
+		.setDescription("How to answer any yes-no questions posed during the operation (default = abort)") //
+	), //
+	target(new MutableParameter() //
+		.setMinValueCount(0) //
+		.setMaxValueCount(-1) //
+		.setValueName("target") //
+		.setValueSep(null) // Disallow concatenated values
+		.setDescription("The path or r_object_id of each object to be deleted (can be specified multiple times)") //
+	), //
+	predicate(new MutableParameter() //
+		.setMinValueCount(1) //
+		.setMaxValueCount(1) //
+		.setValueName("DQL-predicate") //
+		.setDescription(
+			"A DQL predicate that will be used to retrieve the r_object_id for all the objects that need to be deleted") //
+	), //
+		//
+	;
+
+	private final Parameter parameter;
+
+	private CLIParam(MutableParameter parameter) {
+		this.parameter = MutableParameter.initOptionName(this, parameter).freezeCopy();
+	}
+
+	@Override
+	public Parameter getParameter() {
+		return this.parameter;
+	}
+}
---
+++ cli/bulkdel/src/main/java/com/armedia/caliente/cli/bulkdel/Launcher.java
@@ -0,0 +1,47 @@
+package com.armedia.caliente.cli.bulkdel;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import com.armedia.caliente.cli.launcher.AbstractLauncher;
+import com.armedia.caliente.cli.launcher.LaunchClasspathHelper;
+import com.armedia.caliente.cli.launcher.LaunchParameterSet;
+import com.armedia.caliente.cli.parser.CommandLineValues;
+import com.armedia.caliente.cli.parser.Parameter;
+import com.armedia.caliente.cli.parser.ParameterTools;
+import com.armedia.caliente.cli.utils.DfcLaunchHelper;
+
+public class Launcher extends AbstractLauncher implements LaunchParameterSet {
+
+	public static final void main(String... args) {
+		System.exit(new Launcher().launch(args));
+	}
+
+	private final DfcLaunchHelper dfcLaunchHelper = new DfcLaunchHelper(true);
+
+	@Override
+	public Collection<? extends Parameter> getParameters(CommandLineValues commandLine) {
+		return ParameterTools.getUnwrappedList(CLIParam.values());
+	}
+
+	@Override
+	protected Collection<? extends LaunchParameterSet> getLaunchParameterSets(CommandLineValues cli, int pass) {
+		if (pass > 0) { return null; }
+		return Arrays.asList(this, this.dfcLaunchHelper);
+	}
+
+	@Override
+	protected Collection<? extends LaunchClasspathHelper> getClasspathHelpers(CommandLineValues cli) {
+		return Arrays.asList(this.dfcLaunchHelper);
+	}
+
+	@Override
+	protected String getProgramName(int pass) {
+		return "Caliente Bulk Deleter";
+	}
+
+	@Override
+	protected int run(CommandLineValues cli) throws Exception {
+		return new BulkDel(this.dfcLaunchHelper).run(cli);
+	}
+}
