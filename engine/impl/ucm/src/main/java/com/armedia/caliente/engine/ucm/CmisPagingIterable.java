a9d8f55d6ff0a8e1ccc9efe7b1a3c58c1b393d57
Diego Rivera <diego.rivera@armedia.com>
2017-07-31 10:35:32 -0600

    Repackaged files to resolve conflicts and duplication.

---
+++ engine/impl/ucm/src/main/java/com/armedia/caliente/engine/ucm/CmisPagingIterable.java
@@ -0,0 +1,21 @@
+package com.armedia.caliente.engine.ucm;
+
+import org.apache.chemistry.opencmis.client.api.ItemIterable;
+
+public class CmisPagingIterable<E> implements Iterable<E> {
+
+	private final ItemIterable<E> results;
+
+	public CmisPagingIterable(ItemIterable<E> results) {
+		this.results = results;
+	}
+
+	public final ItemIterable<E> getResults() {
+		return this.results;
+	}
+
+	@Override
+	public CmisPagingIterator<E> iterator() {
+		return new CmisPagingIterator<>(this.results);
+	}
+}
