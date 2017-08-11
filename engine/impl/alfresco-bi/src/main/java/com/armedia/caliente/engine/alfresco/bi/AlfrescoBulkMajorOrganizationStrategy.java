b673f4e4dcce2a62caf8a4665bd3d08cf4623627
Diego Rivera <diego.rivera@armedia.com>
2017-08-11 08:46:25 -0600

    More package renames

---
+++ engine/impl/alfresco-bi/src/main/java/com/armedia/caliente/engine/alfresco/bi/AlfrescoBulkMajorOrganizationStrategy.java
@@ -0,0 +1,10 @@
+package com.armedia.caliente.engine.alfresco.bi;
+
+public class AlfrescoBulkMajorOrganizationStrategy extends AlfrescoBaseBulkOrganizationStrategy {
+
+	public static final String NAME = "alfrescoBulkImport";
+
+	public AlfrescoBulkMajorOrganizationStrategy() {
+		super(AlfrescoBulkMajorOrganizationStrategy.NAME, "");
+	}
+}
