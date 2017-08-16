4a665f19a4f39ede713068a1592a893d4655d91a
Diego Rivera <diego.rivera@armedia.com>
2017-08-16 14:38:09 -0600

    Minor fix to help construct token-related exceptions

--- cli/api/src/main/java/com/armedia/caliente/cli/parser/token/UnknownParameterException.java
+++ cli/api/src/main/java/com/armedia/caliente/cli/parser/token/UnknownParameterException.java
@@ -1,11 +1,16 @@
 package com.armedia.caliente.cli.parser.token;
 
+import com.armedia.caliente.cli.token.Token;
 import com.armedia.caliente.cli.token.TokenSource;
 
 public class UnknownParameterException extends TokenSyntaxException {
 	private static final long serialVersionUID = 1L;
 
 	private final String string;
+
+	public UnknownParameterException(Token token) {
+		this(token.getSource(), token.getIndex(), token.getRawString());
+	}
 
 	public UnknownParameterException(TokenSource source, int index, String string) {
 		super(source, index);
--- cli/api/src/main/java/com/armedia/caliente/cli/parser/token/UnknownSubCommandException.java
+++ cli/api/src/main/java/com/armedia/caliente/cli/parser/token/UnknownSubCommandException.java
@@ -1,11 +1,16 @@
 package com.armedia.caliente.cli.parser.token;
 
+import com.armedia.caliente.cli.token.Token;
 import com.armedia.caliente.cli.token.TokenSource;
 
 public class UnknownSubCommandException extends TokenSyntaxException {
 	private static final long serialVersionUID = 1L;
 
 	private final String string;
+
+	public UnknownSubCommandException(Token token) {
+		this(token.getSource(), token.getIndex(), token.getRawString());
+	}
 
 	public UnknownSubCommandException(TokenSource source, int index, String string) {
 		super(source, index);
