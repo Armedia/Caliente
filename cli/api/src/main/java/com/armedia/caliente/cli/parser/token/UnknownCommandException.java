d84124a5d1bdbcdf54af3e39765545dc6f40d0a0
Diego Rivera <diego.rivera@armedia.com>
2017-08-25 15:13:47 -0600

    Minor refactor

--- cli/api/src/main/java/com/armedia/caliente/cli/parser/token/UnknownCommandException.java
+++ cli/api/src/main/java/com/armedia/caliente/cli/parser/token/UnknownCommandException.java
@@ -3,16 +3,16 @@ package com.armedia.caliente.cli.parser.token;
 import com.armedia.caliente.cli.token.Token;
 import com.armedia.caliente.cli.token.TokenSource;
 
-public class UnknownSubCommandException extends TokenSyntaxException {
+public class UnknownCommandException extends TokenSyntaxException {
 	private static final long serialVersionUID = 1L;
 
 	private final String string;
 
-	public UnknownSubCommandException(Token token) {
+	public UnknownCommandException(Token token) {
 		this(token.getSource(), token.getIndex(), token.getRawString());
 	}
 
-	public UnknownSubCommandException(TokenSource source, int index, String string) {
+	public UnknownCommandException(TokenSource source, int index, String string) {
 		super(source, index);
 		this.string = string;
 	}
