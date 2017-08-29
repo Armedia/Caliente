fe7d10c91294d692362e7d5353eabe8fc18f7400
Diego Rivera <diego.rivera@armedia.com>
2017-08-29 15:22:53 -0600

    Progress towards the help renderer.

--- cli/api/src/main/java/com/armedia/caliente/cli/help/CommonsCLIRenderer.java
+++ cli/api/src/main/java/com/armedia/caliente/cli/help/CommonsCLIRenderer.java
@@ -2,17 +2,34 @@ package com.armedia.caliente.cli.help;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Collection;
 
-import com.armedia.caliente.cli.Option;
-import com.armedia.caliente.cli.exception.HelpRequestedException;
+import com.armedia.caliente.cli.Command;
+import com.armedia.caliente.cli.OptionScheme;
 
 public class CommonsCLIRenderer extends HelpRenderer {
 
 	@Override
-	protected void doRenderHelp(String programName, HelpRequestedException help, int width, Writer w)
-		throws IOException {
-		for (Option o : help.getBaseScheme()) {
-			Character shortOpt = o.getShortOpt();
-		}
+	protected void renderUsage(String programName, int width, Writer w) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void renderScheme(OptionScheme scheme, int width, Writer w) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void renderCommandScheme(Command scheme, int width, Writer w) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void renderCommands(Collection<Command> commands, int width, Writer w) throws IOException {
+		// TODO Auto-generated method stub
+
 	}
 }
--- cli/api/src/main/java/com/armedia/caliente/cli/help/HelpRenderer.java
+++ cli/api/src/main/java/com/armedia/caliente/cli/help/HelpRenderer.java
@@ -6,8 +6,12 @@ import java.io.OutputStreamWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.nio.charset.Charset;
+import java.util.Collection;
 import java.util.Objects;
 
+import com.armedia.caliente.cli.Command;
+import com.armedia.caliente.cli.CommandScheme;
+import com.armedia.caliente.cli.OptionScheme;
 import com.armedia.caliente.cli.exception.HelpRequestedException;
 
 public abstract class HelpRenderer {
@@ -59,8 +63,31 @@ public abstract class HelpRenderer {
 		Objects.requireNonNull(programName, "Must provide a program name");
 		Objects.requireNonNull(help, "Must provide a scheme to render help for");
 		Objects.requireNonNull(w, "Must provide a writer to render on");
+
+		renderUsage(programName, width, w);
+		renderScheme(help.getBaseScheme(), width, w);
+
+		Command command = help.getCommand();
+
+		if (CommandScheme.class.isInstance(help.getBaseScheme()) && (command == null)) {
+			// This is a command scheme, but no command is given, so list them out
+			CommandScheme commandScheme = CommandScheme.class.cast(help.getBaseScheme());
+			renderCommands(commandScheme.getCommands(), width, w);
+			return;
+		}
+
+		if (help.getCommand() != null) {
+			// This is a command, so render out the command's parameters
+			renderCommandScheme(help.getCommand(), width, w);
+			return;
+		}
 	}
 
-	protected abstract void doRenderHelp(String programName, HelpRequestedException help, int width, Writer w)
-		throws IOException;
+	protected abstract void renderUsage(String programName, int width, Writer w) throws IOException;
+
+	protected abstract void renderScheme(OptionScheme scheme, int width, Writer w) throws IOException;
+
+	protected abstract void renderCommandScheme(Command scheme, int width, Writer w) throws IOException;
+
+	protected abstract void renderCommands(Collection<Command> commands, int width, Writer w) throws IOException;
 }
