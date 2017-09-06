4956d24ca578129478a2067eaa873d1cb191aeb3
Diego Rivera <diego.rivera@armedia.com>
2017-09-05 18:03:18 -0600

    Code reorganized to better support the iterator's state being separate from the TokenLoader (i.e. request for multiple different iterators with independent states, which can be iterated over concurrently).

--- cli/api/src/main/java/com/armedia/caliente/cli/token/TokenLoader.java
+++ cli/api/src/main/java/com/armedia/caliente/cli/token/TokenLoader.java
@@ -10,13 +10,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.Stack;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -46,56 +46,221 @@ public class TokenLoader implements Iterable<Token> {
 
 	private static final List<String> NO_TOKENS = Collections.emptyList();
 
-	private class State {
-		private final TokenSource source;
-		private List<String> strings = null;
-		private Iterator<String> iterator = null;
-		private int position = 0;
-		private boolean terminated = false;
+	public class TokenIterator {
 
-		/**
-		 * @param source
-		 */
-		private State(TokenSource source) {
-			this.source = source;
+		private class State {
+			private final TokenSource source;
+			private List<String> strings = null;
+			private Iterator<String> iterator = null;
+			private int position = 0;
+			private boolean terminated = false;
+
+			private State(TokenSource source) {
+				this.source = source;
+			}
+
+			private List<String> loadStrings() throws IOException {
+				if (this.strings == null) {
+					// Nothing in the iteratorCache? Retrieve it and iteratorCache it!
+					this.strings = TokenLoader.this.globalCache.get(this.source.getKey());
+					if (this.strings == null) {
+						this.strings = this.source.getTokenStrings();
+						if (this.strings == null) {
+							// Nothing was returned? Then we iteratorCache an empty token list
+							this.strings = TokenLoader.NO_TOKENS;
+						}
+						TokenLoader.this.globalCache.put(this.source.getKey(), this.strings);
+					}
+				}
+				return this.strings;
+			}
+
+			private Iterator<String> getStrings() throws IOException {
+				if (this.iterator == null) {
+					this.iterator = loadStrings().iterator();
+				}
+				return this.iterator;
+			}
 		}
 
-		private List<String> loadStrings() throws IOException {
-			if (this.strings == null) {
-				this.strings = TokenLoader.this.cache.get(this.source.getKey());
-				if (this.strings == null) {
-					// Nothing in the cache? Retrieve it and cache it!
-					this.strings = this.source.getTokenStrings();
-					if (this.strings == null) {
-						// Nothing was returned? Then we cache an empty token list
-						this.strings = TokenLoader.NO_TOKENS;
+		private final boolean enableRecursion;
+		private final Stack<State> states = new Stack<>();
+		private final Set<String> recursions = new LinkedHashSet<>();
+		private String multiShort = null;
+		private Token next = null;
+
+		private TokenIterator(TokenSource start) {
+			this(start, true);
+		}
+
+		private TokenIterator(TokenSource start, boolean enableRecursion) {
+			this.enableRecursion = enableRecursion;
+			this.states.push(new State(start));
+			this.recursions.add(start.getKey());
+		}
+
+		public boolean hasNext() throws IOException, TokenSourceRecursionLoopException {
+			// If we have a token waiting in the wings, we go with that...
+			if (this.next != null) { return true; }
+
+			// We don't? Ok...then get the next string from our current state,
+			// and parse it into a token
+			while (!this.states.isEmpty()) {
+				final boolean atRoot = (this.states.size() == 1);
+				State state = this.states.peek();
+
+				// If we're processing a multishort (-aBcdEfX), process and return the next short
+				// option
+				this.next = getNextShortToken(state);
+				if (this.next != null) { return true; }
+
+				Iterator<String> it = state.getStrings();
+				if (!it.hasNext()) {
+					// This state is spent... so remove it
+					this.recursions.remove(state.source.getKey());
+					this.states.pop();
+					continue;
+				}
+
+				// There are still strings in the current state's list, so we
+				// parse them out...
+				final String rawArg = getRawValue(it.next(), atRoot);
+				final String current = (atRoot ? rawArg : rawArg.trim());
+				state.position++;
+
+				if (StringUtils.isEmpty(current) && !atRoot) {
+					// If we're inside a recursion, we skip empty tokens. We don't do that
+					// when processing values in the primary (i.e. static command line)
+					continue;
+				}
+
+				if (state.terminated) {
+					// The state has been terminated with -- or equivalent, so all tokens that
+					// follow
+					// are strictly strings for this source
+					this.next = newToken(state, Type.STRING, current, current);
+					return true;
+				}
+
+				// If the next string is a recursion, then we follow the recursion...
+				if (isRecursion(current)) {
+					// Here we check if recursion is enabled...if it's not, we just skip the
+					// recursion code and ignore the flag altogether
+					if (this.enableRecursion) {
+						State newState = recurse(current, state.source);
+						if (!this.recursions.add(newState.source.getKey())) {
+							// Recursion loop!! This is an error!
+							throw new TokenSourceRecursionLoopException(newState.source, this.recursions);
+						}
+
+						// No recursion loop, so we simply push the new state into the stack and
+						// repeat the loop
+						this.states.push(newState);
 					}
-					TokenLoader.this.cache.put(this.source.getKey(), this.strings);
+					continue;
+				}
+
+				if (TokenLoader.this.terminator.equals(current)) {
+					state.terminated = true;
+					continue;
+				}
+
+				Matcher m = TokenLoader.this.patShort.matcher(current);
+				if (m.matches()) {
+					this.multiShort = m.group(1);
+					this.next = getNextShortToken(state);
+					return true;
+				}
+
+				m = TokenLoader.this.patLong.matcher(current);
+				if (m.matches()) {
+					this.next = newToken(state, Type.LONG_OPTION, m.group(1), current);
+					return true;
+				}
+
+				this.next = newToken(state, Type.STRING, current, current);
+				return true;
+			}
+
+			return false;
+		}
+
+		private Token getNextShortToken(State state) {
+			if (this.multiShort == null) { return null; }
+			char c = this.multiShort.charAt(0);
+			Token next = newToken(state, Type.SHORT_OPTION, String.valueOf(c),
+				String.format("%s%s", TokenLoader.OPTION_MARKER, c));
+			this.multiShort = this.multiShort.substring(1);
+			if (StringUtils.isEmpty(this.multiShort)) {
+				this.multiShort = null;
+			}
+			return next;
+		}
+
+		private String getRawValue(String rawArg, boolean atRoot) {
+			rawArg = Tools.coalesce(rawArg, StringUtils.EMPTY);
+			if (atRoot) { return rawArg; }
+			// If we're not on the root, then we need to remove everything to the right of the
+			// first unescaped #, and remove the backslashes from it...
+			Matcher commentMatcher = TokenLoader.COMMENT.matcher(rawArg);
+			if (commentMatcher.find()) {
+				// Strip everything past the first comment character
+				rawArg = rawArg.substring(0, commentMatcher.start());
+			}
+			return rawArg.replaceAll("\\\\#", "#");
+		}
+
+		private State recurse(String current, TokenSource source) throws IOException {
+			// Remove the file marker
+			String fileName = current.substring(TokenLoader.this.fileMarker.length());
+			TokenSource newSource = null;
+			if (fileName.charAt(0) == TokenLoader.this.fileMarkerChar.charValue()) {
+				// It's a URL...
+				// TODO: Eventually port this to support Commons-VFS URLs?
+				final String uriStr = fileName.substring(1);
+				final URI sourceUri;
+				try {
+					sourceUri = new URI(uriStr);
+				} catch (URISyntaxException e) {
+					throw new IOException(String.format("Failed to properly process the URI [%s]", uriStr), e);
+				}
+				if (!sourceUri.getScheme().equals("file")) {
+					// Not a local file, use the URL source
+					newSource = new UriTokenSource(sourceUri);
+				} else {
+					// Local file... treat it as such...
+					fileName = new File(sourceUri).getAbsolutePath();
 				}
 			}
-			return this.strings;
-		}
 
-		private Iterator<String> getStrings() throws IOException {
-			if (this.iterator == null) {
-				this.iterator = loadStrings().iterator();
+			if (newSource == null) {
+				// It's a local file... if the current source is another local file,
+				// and the given path isn't absolute, take its path to be relative to that one
+				Path path = Paths.get(fileName);
+				if (!path.isAbsolute() && LocalPathTokenSource.class.isInstance(source)) {
+					LocalPathTokenSource pathSource = LocalPathTokenSource.class.cast(source);
+					Path relativeTo = pathSource.getSourcePath().getParent();
+					path = relativeTo.resolve(path);
+				}
+				newSource = new LocalPathTokenSource(path);
 			}
-			return this.iterator;
+
+			return new State(newSource);
 		}
 
-		private void reset() {
-			this.strings = null;
-			this.iterator = null;
-			this.position = 0;
-			this.terminated = false;
+		private Token newToken(State state, Type type, String value, String rawString) {
+			return new Token(state.source, state.position, type, value, rawString);
+		}
+
+		public Token next() throws IOException, TokenSourceRecursionLoopException {
+			if (!hasNext()) { throw new NoSuchElementException(); }
+			Token ret = this.next;
+			this.next = null;
+			return ret;
 		}
 	}
 
-	private final Map<String, List<String>> cache = new LinkedHashMap<>();
-	private final Set<String> recursions = new LinkedHashSet<>();
-	private final Stack<State> states = new Stack<>();
-	private String multiShort = null;
-	private Token next = null;
+	private final Map<String, List<String>> globalCache = new ConcurrentHashMap<>();
 
 	private static final String TERMINATOR_FMT = "%1$s%1$s";
 	private static final String SHORT_FMT = "^%1$s(\\S)$";
@@ -108,6 +273,7 @@ public class TokenLoader implements Iterable<Token> {
 	private static final char OPTION_MARKER = '-';
 	private static final Character FILE_MARKER = '@';
 
+	private final TokenSource start;
 	private final String terminator;
 	private final Pattern patShort;
 	private final Pattern patLong;
@@ -159,8 +325,7 @@ public class TokenLoader implements Iterable<Token> {
 
 	public TokenLoader(TokenSource start, char valueSeparator, boolean allowRecursion) {
 		if (start == null) { throw new IllegalArgumentException("Must provide a starting token source"); }
-		this.states.push(new State(start));
-		this.recursions.add(start.getKey());
+		this.start = start;
 
 		if (allowRecursion) {
 			this.fileMarkerChar = TokenLoader.FILE_MARKER;
@@ -185,170 +350,6 @@ public class TokenLoader implements Iterable<Token> {
 		return this.valueSeparator;
 	}
 
-	private Token getNextShortToken(State state) {
-		if (this.multiShort == null) { return null; }
-		char c = this.multiShort.charAt(0);
-		Token next = newToken(state, Type.SHORT_OPTION, String.valueOf(c),
-			String.format("%s%s", TokenLoader.OPTION_MARKER, c));
-		this.multiShort = this.multiShort.substring(1);
-		if (StringUtils.isEmpty(this.multiShort)) {
-			this.multiShort = null;
-		}
-		return next;
-	}
-
-	public Collection<Token> getBaseTokens() {
-		if (this.states.isEmpty()) { return Collections.emptyList(); }
-		State root = this.states.firstElement();
-		List<Token> ret = new ArrayList<>(root.strings.size());
-		boolean terminated = false;
-		final List<String> strings;
-		try {
-			strings = root.loadStrings();
-		} catch (IOException e) {
-			// This won't happen as there are no recursions in play
-			throw new RuntimeException("Unexpected exception while reading from memory", e);
-		}
-		for (String current : strings) {
-
-			// If we're already terminated, everything else is a string...
-			if (terminated) {
-				ret.add(newToken(root, Type.STRING, current, current));
-				continue;
-			}
-
-			// Skip recursions...
-			if (isRecursion(current)) {
-				continue;
-			}
-
-			// Is it the terminator?
-			if (this.terminator.equals(current)) {
-				terminated = true;
-				continue;
-			}
-
-			// Is it a short option?
-			Matcher m = this.patShort.matcher(current);
-			if (m.matches()) {
-				String multiShort = m.group(1);
-				for (int i = 0; i < multiShort.length(); i++) {
-					String c = multiShort.substring(i, i + 1);
-					ret.add(newToken(root, Type.SHORT_OPTION, c, String.format("%s%s", TokenLoader.OPTION_MARKER, c)));
-				}
-				continue;
-			}
-
-			// Is it a long option?
-			m = this.patLong.matcher(current);
-			if (m.matches()) {
-				ret.add(newToken(root, Type.LONG_OPTION, m.group(1), current));
-				continue;
-			}
-
-			// Must be a string...
-			ret.add(newToken(root, Type.STRING, current, current));
-		}
-		return ret;
-	}
-
-	public boolean hasNext() throws IOException, TokenSourceRecursionLoopException {
-		// If we have a token waiting in the wings, we go with that...
-		if (this.next != null) { return true; }
-
-		// We don't? Ok...then get the next string from our current state,
-		// and parse it into a token
-		while (!this.states.isEmpty()) {
-			final boolean atRoot = (this.states.size() == 1);
-			State state = this.states.peek();
-
-			// If we're processing a multishort (-aBcdEfX), process and return the next short option
-			this.next = getNextShortToken(state);
-			if (this.next != null) { return true; }
-
-			Iterator<String> it = state.getStrings();
-			if (!it.hasNext()) {
-				// This state is spent... so remove it
-				this.recursions.remove(state.source.getKey());
-				this.states.pop();
-				continue;
-			}
-
-			// There are still strings in the current state's list, so we
-			// parse them out...
-			final String rawArg = getRawValue(it.next(), atRoot);
-			final String current = (atRoot ? rawArg : rawArg.trim());
-			state.position++;
-
-			if (StringUtils.isEmpty(current) && !atRoot) {
-				// If we're inside a recursion, we skip empty tokens. We don't do that
-				// when processing values in the primary (i.e. static command line)
-				continue;
-			}
-
-			if (state.terminated) {
-				// The state has been terminated with -- or equivalent, so all tokens that follow
-				// are strictly strings for this source
-				this.next = newToken(state, Type.STRING, current, current);
-				return true;
-			}
-
-			// If the next string is a recursion, then we follow the recursion...
-			if (isRecursion(current)) {
-				State newState = recurse(current, state.source);
-				if (!this.recursions.add(newState.source.getKey())) {
-					// Recursion loop!! This is an error!
-					throw new TokenSourceRecursionLoopException(newState.source, this.recursions);
-				}
-
-				// No recursion loop, so we simply push the new state into the stack and repeat the
-				// loop
-				this.states.push(newState);
-				continue;
-			}
-
-			if (this.terminator.equals(current)) {
-				state.terminated = true;
-				continue;
-			}
-
-			Matcher m = this.patShort.matcher(current);
-			if (m.matches()) {
-				this.multiShort = m.group(1);
-				this.next = getNextShortToken(state);
-				return true;
-			}
-
-			m = this.patLong.matcher(current);
-			if (m.matches()) {
-				this.next = newToken(state, Type.LONG_OPTION, m.group(1), current);
-				return true;
-			}
-
-			this.next = newToken(state, Type.STRING, current, current);
-			return true;
-		}
-
-		return false;
-	}
-
-	private Token newToken(State state, Type type, String value, String rawString) {
-		return new Token(state.source, state.position, type, value, rawString);
-	}
-
-	private String getRawValue(String rawArg, boolean atRoot) {
-		rawArg = Tools.coalesce(rawArg, StringUtils.EMPTY);
-		if (atRoot) { return rawArg; }
-		// If we're not on the root, then we need to remove everything to the right of the
-		// first unescaped #, and remove the backslashes from it...
-		Matcher commentMatcher = TokenLoader.COMMENT.matcher(rawArg);
-		if (commentMatcher.find()) {
-			// Strip everything past the first comment character
-			rawArg = rawArg.substring(0, commentMatcher.start());
-		}
-		return rawArg.replaceAll("\\\\#", "#");
-	}
-
 	private boolean isRecursion(String str) {
 		return (this.fileMarker != null) && str.startsWith(this.fileMarker);
 	}
@@ -357,73 +358,40 @@ public class TokenLoader implements Iterable<Token> {
 		return (this.fileMarker != null);
 	}
 
-	private State recurse(String current, TokenSource source) throws IOException {
-		// Remove the file marker
-		String fileName = current.substring(this.fileMarker.length());
-		TokenSource newSource = null;
-		if (fileName.charAt(0) == this.fileMarkerChar.charValue()) {
-			// It's a URL...
-			// TODO: Eventually port this to support Commons-VFS URLs?
-			final String uriStr = fileName.substring(1);
-			final URI sourceUri;
-			try {
-				sourceUri = new URI(uriStr);
-			} catch (URISyntaxException e) {
-				throw new IOException(String.format("Failed to properly process the URI [%s]", uriStr), e);
-			}
-			if (!sourceUri.getScheme().equals("file")) {
-				// Not a local file, use the URL source
-				newSource = new UriTokenSource(sourceUri);
-			} else {
-				// Local file... treat it as such...
-				fileName = new File(sourceUri).getAbsolutePath();
-			}
-		}
-
-		if (newSource == null) {
-			// It's a local file... if the current source is another local file,
-			// and the given path isn't absolute, take its path to be relative to that one
-			Path path = Paths.get(fileName);
-			if (!path.isAbsolute() && LocalPathTokenSource.class.isInstance(source)) {
-				LocalPathTokenSource pathSource = LocalPathTokenSource.class.cast(source);
-				Path relativeTo = pathSource.getSourcePath().getParent();
-				path = relativeTo.resolve(path);
-			}
-			newSource = new LocalPathTokenSource(path);
-		}
-
-		return new State(newSource);
+	public Collection<Token> getBaseTokens() {
+		return getTokens(false);
 	}
 
-	public Token next() throws IOException, TokenSourceRecursionLoopException {
-		if (!hasNext()) { throw new NoSuchElementException(); }
-		Token ret = this.next;
-		this.next = null;
-		return ret;
-	}
-
-	public void restart() {
-		while (this.states.size() > 1) {
-			State s = this.states.pop();
-			this.recursions.remove(s.source.getKey());
+	public Collection<Token> getTokens(boolean recursing) {
+		List<Token> ret = new ArrayList<>();
+		TokenIterator it = iterator(recursing);
+		try {
+			while (it.hasNext()) {
+				ret.add(it.next());
+			}
+			return ret;
+		} catch (TokenSourceRecursionLoopException | IOException e) {
+			throw new RuntimeException("Unexpected exception reading from memory", e);
 		}
-		this.states.peek().reset();
-		this.next = null;
 	}
 
-	public void reset() {
-		restart();
-		this.cache.clear();
+	public void clearCache() {
+		this.globalCache.clear();
+	}
+
+	public TokenIterator iterator(final boolean enableRecursion) {
+		return new TokenIterator(TokenLoader.this.start, enableRecursion);
 	}
 
 	@Override
 	public Iterator<Token> iterator() {
 		return new Iterator<Token>() {
+			final TokenIterator it = iterator(true);
 
 			@Override
 			public boolean hasNext() {
 				try {
-					return TokenLoader.this.hasNext();
+					return this.it.hasNext();
 				} catch (TokenSourceRecursionLoopException e) {
 					throw new RuntimeException("Token recursion loop detected", e);
 				} catch (IOException e) {
@@ -434,7 +402,7 @@ public class TokenLoader implements Iterable<Token> {
 			@Override
 			public Token next() {
 				try {
-					return TokenLoader.this.next();
+					return this.it.next();
 				} catch (TokenSourceRecursionLoopException e) {
 					throw new RuntimeException("Token recursion loop detected", e);
 				} catch (IOException e) {
@@ -446,7 +414,6 @@ public class TokenLoader implements Iterable<Token> {
 			public void remove() {
 				throw new UnsupportedOperationException();
 			}
-
 		};
 	}
 }
