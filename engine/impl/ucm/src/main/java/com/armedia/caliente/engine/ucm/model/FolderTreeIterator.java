7796977be14477c61b3c86d370402c0e4d32b83d
Diego Rivera <diego.rivera@armedia.com>
2017-09-19 09:28:42 -0600

    Some enhancements to facilitate avoiding unnecessary recursions

--- engine/impl/ucm/src/main/java/com/armedia/caliente/engine/ucm/model/FolderTreeIterator.java
+++ engine/impl/ucm/src/main/java/com/armedia/caliente/engine/ucm/model/FolderTreeIterator.java
@@ -1,5 +1,6 @@
 package com.armedia.caliente.engine.ucm.model;
 
+import java.io.Serializable;
 import java.net.URI;
 import java.util.NoSuchElementException;
 import java.util.Objects;
@@ -10,7 +11,43 @@ import com.armedia.commons.utilities.Tools;
 
 public class FolderTreeIterator {
 
+	public static final class Config implements Serializable {
+		private static final long serialVersionUID = 1L;
+
+		private int pageSize = FolderTreeIterator.DEFAULT_PAGE_SIZE;
+		private FolderIteratorMode mode = FolderTreeIterator.DEFAULT_MODE;
+		private boolean recurseShortcuts = FolderTreeIterator.DEFAULT_RECURSE_SHORTCUTS;
+
+		public int getPageSize() {
+			return this.pageSize;
+		}
+
+		public void setPageSize(int pageSize) {
+			this.pageSize = pageSize;
+		}
+
+		public FolderIteratorMode getMode() {
+			return this.mode;
+		}
+
+		public void setMode(FolderIteratorMode mode) {
+			this.mode = mode;
+		}
+
+		public boolean isRecurseShortcuts() {
+			return this.recurseShortcuts;
+		}
+
+		public void setRecurseShortcuts(boolean recurseShortcuts) {
+			this.recurseShortcuts = recurseShortcuts;
+		}
+	}
+
+	private static final Config DEFAULT_CONFIG = new Config();
+
 	public static final FolderIteratorMode DEFAULT_MODE = FolderIteratorMode.COMBINED;
+
+	public static final boolean DEFAULT_RECURSE_SHORTCUTS = true;
 
 	public static final int MINIMUM_PAGE_SIZE = 1;
 	public static final int DEFAULT_PAGE_SIZE = 1000;
@@ -21,6 +58,7 @@ public class FolderTreeIterator {
 	private final Object searchKey;
 	private final int pageSize;
 	private final FolderIteratorMode mode;
+	private final boolean recurseShortcuts;
 
 	private boolean rootExamined = false;
 	private UcmAttributes current = null;
@@ -28,64 +66,43 @@ public class FolderTreeIterator {
 	private Stack<FolderContentsIterator> recursion = new Stack<>();
 
 	public FolderTreeIterator(UcmSession session, String path) {
-		this(session, FolderLocatorMode.BY_PATH, path, null, FolderContentsIterator.DEFAULT_PAGE_SIZE);
-	}
-
-	public FolderTreeIterator(UcmSession session, String path, FolderIteratorMode folderIteratorMode) {
-		this(session, FolderLocatorMode.BY_PATH, path, null, FolderContentsIterator.DEFAULT_PAGE_SIZE);
-	}
-
-	public FolderTreeIterator(UcmSession session, String path, int pageSize) {
-		this(session, FolderLocatorMode.BY_PATH, path, null, pageSize);
-	}
-
-	public FolderTreeIterator(UcmSession session, String path, FolderIteratorMode mode, int pageSize) {
-		this(session, FolderLocatorMode.BY_PATH, path, mode, pageSize);
+		this(session, FolderLocatorMode.BY_PATH, path, null);
 	}
 
 	public FolderTreeIterator(UcmSession session, URI uri) {
-		this(session, FolderLocatorMode.BY_URI, uri, null, FolderContentsIterator.DEFAULT_PAGE_SIZE);
-	}
-
-	public FolderTreeIterator(UcmSession session, URI uri, FolderIteratorMode folderIteratorMode) {
-		this(session, FolderLocatorMode.BY_URI, uri, null, FolderContentsIterator.DEFAULT_PAGE_SIZE);
-	}
-
-	public FolderTreeIterator(UcmSession session, URI uri, int pageSize) {
-		this(session, FolderLocatorMode.BY_URI, uri, null, pageSize);
-	}
-
-	public FolderTreeIterator(UcmSession session, URI uri, FolderIteratorMode mode, int pageSize) {
-		this(session, FolderLocatorMode.BY_URI, uri, mode, pageSize);
+		this(session, FolderLocatorMode.BY_URI, uri, null);
 	}
 
 	public FolderTreeIterator(UcmSession session, UcmUniqueURI guid) {
-		this(session, FolderLocatorMode.BY_GUID, guid, null, FolderContentsIterator.DEFAULT_PAGE_SIZE);
+		this(session, FolderLocatorMode.BY_GUID, guid, null);
 	}
 
-	public FolderTreeIterator(UcmSession session, UcmUniqueURI guid, FolderIteratorMode folderIteratorMode) {
-		this(session, FolderLocatorMode.BY_GUID, guid, null, FolderContentsIterator.DEFAULT_PAGE_SIZE);
+	public FolderTreeIterator(UcmSession session, String path, Config config) {
+		this(session, FolderLocatorMode.BY_PATH, path, config);
 	}
 
-	public FolderTreeIterator(UcmSession session, UcmUniqueURI guid, int pageSize) {
-		this(session, FolderLocatorMode.BY_GUID, guid, null, pageSize);
+	public FolderTreeIterator(UcmSession session, URI uri, Config config) {
+		this(session, FolderLocatorMode.BY_URI, uri, config);
 	}
 
-	public FolderTreeIterator(UcmSession session, UcmUniqueURI guid, FolderIteratorMode mode, int pageSize) {
-		this(session, FolderLocatorMode.BY_GUID, guid, mode, pageSize);
+	public FolderTreeIterator(UcmSession session, UcmUniqueURI guid, Config config) {
+		this(session, FolderLocatorMode.BY_GUID, guid, config);
 	}
 
-	private FolderTreeIterator(UcmSession session, FolderLocatorMode searchMode, Object key, FolderIteratorMode mode,
-		int pageSize) {
+	private FolderTreeIterator(UcmSession session, FolderLocatorMode searchMode, Object key, Config config) {
 		Objects.requireNonNull(session, "Must provide a non-null session");
 		this.searchKey = searchMode.sanitizeKey(key);
 		this.searchMode = searchMode;
 		this.session = session;
-		this.pageSize = Tools.ensureBetween(FolderContentsIterator.MINIMUM_PAGE_SIZE, pageSize,
+		if (config == null) {
+			config = FolderTreeIterator.DEFAULT_CONFIG;
+		}
+		this.pageSize = Tools.ensureBetween(FolderContentsIterator.MINIMUM_PAGE_SIZE, config.pageSize,
 			FolderContentsIterator.MAXIMUM_PAGE_SIZE);
-		this.mode = Tools.coalesce(mode, FolderContentsIterator.DEFAULT_MODE);
-		this.recursion.push(
-			new FolderContentsIterator(session, searchMode, this.searchKey, FolderIteratorMode.COMBINED, pageSize));
+		this.mode = Tools.coalesce(config.mode, FolderContentsIterator.DEFAULT_MODE);
+		this.recursion.push(new FolderContentsIterator(session, searchMode, this.searchKey, FolderIteratorMode.COMBINED,
+			this.pageSize));
+		this.recurseShortcuts = config.recurseShortcuts;
 	}
 
 	public UcmSession getSession() {
@@ -125,6 +142,11 @@ public class FolderTreeIterator {
 				if (this.current != null) { return true; }
 			}
 
+			// Special edge case - if we're in the first recursion and we're not recursing
+			// shortcuts, and the first recursion is a shortcut, we quite simply don't move past
+			if ((this.recursion.size() == 1) && UcmModel.isShortcut(currentRecursion.getFolder())
+				&& !this.recurseShortcuts) { return false; }
+
 			if (!currentRecursion.hasNext()) {
 				// If this level is exhausted, we move to the next level...
 				this.recursion.pop();
@@ -137,8 +159,11 @@ public class FolderTreeIterator {
 			if (UcmModel.isFolderURI(uri)) {
 				// If this is a folder, we return it, but we store the recursion for the next
 				// call to hasNext()
-				this.recursion.push(new FolderContentsIterator(this.session, FolderLocatorMode.BY_URI, uri,
-					FolderIteratorMode.COMBINED, this.pageSize));
+				if (!UcmModel.isShortcut(att) || this.recurseShortcuts) {
+					// We only recurse if it's not a shortcut
+					this.recursion.push(new FolderContentsIterator(this.session, FolderLocatorMode.BY_URI, uri,
+						FolderIteratorMode.COMBINED, this.pageSize));
+				}
 				if (this.mode == FolderIteratorMode.FILES) {
 					// We're not supposed to iterate over folders, so we simply loop back up
 					continue nextLevel;
