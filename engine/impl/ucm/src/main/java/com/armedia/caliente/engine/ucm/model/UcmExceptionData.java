c06da5e12f7845d9bd3d0d390c71b07cedbe0b0b
Diego Rivera <diego.rivera@armedia.com>
2017-09-19 08:17:43 -0600

    Some fixes to help in the testing

--- engine/impl/ucm/src/main/java/com/armedia/caliente/engine/ucm/model/UcmExceptionData.java
+++ engine/impl/ucm/src/main/java/com/armedia/caliente/engine/ucm/model/UcmExceptionData.java
@@ -8,48 +8,111 @@ import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 
-class UcmExceptionData {
+import com.armedia.commons.utilities.Tools;
+
+public class UcmExceptionData {
+
+	public static final class ExceptionEntry {
+		private final String tag;
+		private final List<String> parameters;
+		private volatile String string = null;
+
+		private ExceptionEntry(String tag, List<String> parameters) {
+			this.tag = tag;
+			this.parameters = Tools.freezeList(parameters);
+		}
+
+		public String getTag() {
+			return this.tag;
+		}
+
+		public List<String> getParameters() {
+			return this.parameters;
+		}
+
+		@Override
+		public String toString() {
+			if (this.string == null) {
+				synchronized (this) {
+					if (this.string == null) {
+						StringBuilder sb = new StringBuilder();
+						sb.append('!').append(UcmExceptionData.escape(this.tag));
+						for (String p : this.parameters) {
+							sb.append(',').append(UcmExceptionData.escape(p));
+						}
+						this.string = sb.toString();
+					}
+				}
+			}
+			return this.string;
+		}
+	}
+
 	private static final Pattern MSGKEY_EXCL = Pattern.compile("(?<!\\\\)!");
 	private static final Pattern MSGKEY_COMMA = Pattern.compile("(?<!\\\\),");
 
-	private static List<String> parseCommas(String msg) {
+	private static String unescape(String string) {
+		return string //
+			.replaceAll("\\\\!", "!") //
+			.replaceAll("\\\\,", ",") //
+		;
+	}
+
+	private static String escape(String string) {
+		return string //
+			.replaceAll("!", "\\\\!") //
+			.replaceAll(",", "\\\\,") //
+		;
+	}
+
+	private static List<String> parseParameters(String msg) {
 		int prev = 0;
 		List<String> ret = new ArrayList<>();
 		Matcher m = UcmExceptionData.MSGKEY_COMMA.matcher(msg);
 		while (m.find()) {
-			String part = msg.substring(prev, m.start());
-			part = part.replaceAll("\\\\!", "!");
-			part = part.replaceAll("\\\\,", ",");
-			ret.add(part);
+			ret.add(UcmExceptionData.unescape(msg.substring(prev, m.start())));
 			prev = m.end();
 		}
 		if (prev < msg.length()) {
-			String part = msg.substring(prev);
-			part = part.replaceAll("\\\\!", "!");
-			part = part.replaceAll("\\\\,", ",");
-			ret.add(part);
+			ret.add(UcmExceptionData.unescape(msg.substring(prev)));
 		}
 		return ret;
 	}
 
-	static List<List<String>> parseMessages(String msg) {
+	public static List<ExceptionEntry> parseMessageKey(String msg) {
 		if (msg == null) { return Collections.emptyList(); }
 		int prev = 0;
-		List<List<String>> data = new ArrayList<>();
+		List<ExceptionEntry> data = new ArrayList<>();
 		Matcher m = UcmExceptionData.MSGKEY_EXCL.matcher(msg);
 		while (m.find()) {
 			String part = msg.substring(prev, m.start());
 			if (!StringUtils.isEmpty(part)) {
-				data.add(UcmExceptionData.parseCommas(part));
+				List<String> parameters = UcmExceptionData.parseParameters(part);
+				if (!parameters.isEmpty()) {
+					String tag = parameters.remove(0);
+					data.add(new ExceptionEntry(tag, parameters));
+				}
 			}
 			prev = m.end();
 		}
 		if (prev < msg.length()) {
 			String part = msg.substring(prev);
 			if (!StringUtils.isEmpty(part)) {
-				data.add(UcmExceptionData.parseCommas(part));
+				List<String> parameters = UcmExceptionData.parseParameters(part);
+				if (!parameters.isEmpty()) {
+					String tag = parameters.remove(0);
+					data.add(new ExceptionEntry(tag, parameters));
+				}
 			}
 		}
 		return data;
+	}
+
+	public static String generateMessageKey(List<ExceptionEntry> entries) {
+		StringBuilder sb = new StringBuilder();
+		for (ExceptionEntry e : entries) {
+			sb.append(e.toString());
+		}
+		return sb.toString();
 	}
 }
--- engine/impl/ucm/src/test/java/com/armedia/caliente/engine/ucm/model/UcmExceptionParserTest.java
+++ engine/impl/ucm/src/test/java/com/armedia/caliente/engine/ucm/model/UcmExceptionParserTest.java
@@ -1,6 +1,11 @@
 package com.armedia.caliente.engine.ucm.model;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.armedia.caliente.engine.ucm.model.UcmExceptionData.ExceptionEntry;
 
 public class UcmExceptionParserTest {
 
@@ -10,7 +15,19 @@ public class UcmExceptionParserTest {
 			"!csUnableToGetRevInfo2,SOME\\!THING\\!\\,WEIRD\\!\\!!csGetFileUnableToFindRevision,csGetFileRevMethodLabel_Latest,SOME\\!THING\\!WE\\,IRD\\!\\!"
 		};
 
-		System.out.printf("%s%n", UcmExceptionData.parseMessages(msg[0]));
-	}
+		List<ExceptionEntry> l = UcmExceptionData.parseMessageKey(msg[0]);
 
+		ExceptionEntry e = l.get(0);
+		Assert.assertEquals("csUnableToGetRevInfo2", e.getTag());
+		Assert.assertEquals(1, e.getParameters().size());
+		Assert.assertEquals("SOME!THING!,WEIRD!!", e.getParameters().get(0));
+
+		e = l.get(1);
+		Assert.assertEquals("csGetFileUnableToFindRevision", e.getTag());
+		Assert.assertEquals(2, e.getParameters().size());
+		Assert.assertEquals("csGetFileRevMethodLabel_Latest", e.getParameters().get(0));
+		Assert.assertEquals("SOME!THING!WE,IRD!!", e.getParameters().get(1));
+
+		Assert.assertEquals(msg[0], UcmExceptionData.generateMessageKey(l));
+	}
 }
