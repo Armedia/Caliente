46998a082b326e83c90c3d56630dead4e75b703b
Diego Rivera <diego.rivera@armedia.com>
2017-09-21 07:55:21 -0600

    Modified to facilitate the use of a builder pattern, for easier coding.

--- store/api/src/main/java/com/armedia/caliente/store/CmfAttribute.java
+++ store/api/src/main/java/com/armedia/caliente/store/CmfAttribute.java
@@ -25,6 +25,30 @@ public class CmfAttribute<V> extends CmfProperty<V> {
 	}
 
 	@Override
+	public CmfAttribute<V> setValues(Collection<V> values) {
+		super.setValues(values);
+		return this;
+	}
+
+	@Override
+	public CmfAttribute<V> addValue(V value) {
+		super.addValue(value);
+		return this;
+	}
+
+	@Override
+	public CmfAttribute<V> addValues(Collection<V> values) {
+		super.addValues(values);
+		return this;
+	}
+
+	@Override
+	public CmfAttribute<V> setValue(V value) {
+		super.setValue(value);
+		return this;
+	}
+
+	@Override
 	public String toString() {
 		return String.format("CmfAttribute [name=%s, type=%s, repeating=%s %s=%s]", getName(), getType(), isRepeating(),
 			(isRepeating() ? "values" : "singleValue"), (isRepeating() ? getValues() : getValue()));
