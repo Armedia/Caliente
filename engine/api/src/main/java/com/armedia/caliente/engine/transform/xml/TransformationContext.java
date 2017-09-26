32e3413435214086c047e11bd0a3c66c013c28ed
Diego Rivera <diego.rivera@armedia.com>
2017-09-25 21:13:40 -0600

    Added methods that will assist during transformation

--- engine/api/src/main/java/com/armedia/caliente/engine/transform/xml/TransformationContext.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/transform/xml/TransformationContext.java
@@ -1,9 +1,17 @@
 package com.armedia.caliente.engine.transform.xml;
 
+import java.util.Set;
+
 import com.armedia.caliente.store.CmfObject;
 
 public interface TransformationContext<V> {
 
 	public CmfObject<V> getObject();
+
+	public String getCurrentSubtype();
+
+	public void setCurrentSubtype();
+
+	public Set<String> getCurrentDecorators();
 
 }
