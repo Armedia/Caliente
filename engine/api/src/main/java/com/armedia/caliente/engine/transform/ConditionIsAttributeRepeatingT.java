7b2cc68e74e6dfefcf068b4be730b0a18d603019
Diego Rivera <diego.rivera@armedia.com>
2017-09-26 17:52:05 -0600

    More condition implementations.

--- engine/api/src/main/java/com/armedia/caliente/engine/transform/ConditionIsAttributeRepeatingT.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/transform/ConditionIsAttributeRepeatingT.java
@@ -1,9 +1,15 @@
 
 package com.armedia.caliente.engine.transform;
 
+import java.util.Set;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
+
+import com.armedia.caliente.store.CmfAttribute;
+import com.armedia.caliente.store.CmfObject;
+import com.armedia.commons.utilities.Tools;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "conditionIsAttributeRepeating.t")
@@ -11,7 +17,23 @@ public class ConditionIsAttributeRepeatingT extends ConditionExpressionCompariso
 
 	@Override
 	public <V> boolean check(TransformationContext<V> ctx) {
-		// TODO implement this condition
+		final CmfObject<V> object = ctx.getObject();
+		final String comparand = Tools.toString(evaluate(ctx));
+		final Comparison comparison = getComparison();
+
+		Set<String> names = object.getAttributeNames();
+		if (comparison == Comparison.EQ) {
+			// Shortcut!! Look for only one!
+			CmfAttribute<V> att = object.getAttribute(comparand);
+			return ((att != null) && att.isRepeating());
+		}
+		for (String s : names) {
+			if (comparison.check(comparand, s)) {
+				// This attribute matches...if this one is repeating, we're done!
+				if (object.getAttribute(s).isRepeating()) { return true; }
+			}
+		}
+		// None of the matching attributes was repeating...so this is false
 		return false;
 	}
 
--- engine/api/src/main/java/com/armedia/caliente/engine/transform/ConditionIsCalientePropertyRepeatingT.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/transform/ConditionIsCalientePropertyRepeatingT.java
@@ -1,9 +1,15 @@
 
 package com.armedia.caliente.engine.transform;
 
+import java.util.Set;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
+
+import com.armedia.caliente.store.CmfObject;
+import com.armedia.caliente.store.CmfProperty;
+import com.armedia.commons.utilities.Tools;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "conditionIsCalientePropertyRepeating.t")
@@ -11,7 +17,23 @@ public class ConditionIsCalientePropertyRepeatingT extends ConditionExpressionCo
 
 	@Override
 	public <V> boolean check(TransformationContext<V> ctx) {
-		// TODO implement this condition
+		final CmfObject<V> object = ctx.getObject();
+		final String comparand = Tools.toString(evaluate(ctx));
+		final Comparison comparison = getComparison();
+
+		Set<String> names = object.getAttributeNames();
+		if (comparison == Comparison.EQ) {
+			// Shortcut!! Look for only one!
+			CmfProperty<V> prop = object.getProperty(comparand);
+			return ((prop != null) && prop.isRepeating());
+		}
+		for (String s : names) {
+			if (comparison.check(comparand, s)) {
+				// This property matches...if this one is empty, we're done!
+				if (object.getProperty(s).isRepeating()) { return true; }
+			}
+		}
+		// None of the matching properties was repeating...so this is false
 		return false;
 	}
 
