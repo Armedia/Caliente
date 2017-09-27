55a248c656e803bcf2ed16418af51bab10995848
Diego Rivera <diego.rivera@armedia.com>
2017-09-27 07:27:44 -0600

    Added methods to the Transformation Context that will facilitate the actual transformation of objects.  Likely will also change things to use CmfValue instead of <V> - since this should make life significantly easier for the transformation layer.

--- engine/api/src/main/java/com/armedia/caliente/engine/transform/TransformationContext.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/transform/TransformationContext.java
@@ -2,31 +2,101 @@ package com.armedia.caliente.engine.transform;
 
 import java.util.Set;
 
+import com.armedia.caliente.store.CmfAttribute;
 import com.armedia.caliente.store.CmfDataType;
+import com.armedia.caliente.store.CmfEncodeableName;
 import com.armedia.caliente.store.CmfObject;
 import com.armedia.caliente.store.CmfProperty;
+import com.armedia.caliente.store.CmfType;
 import com.armedia.caliente.store.CmfValue;
 
 public interface TransformationContext<V> {
 
 	public CmfObject<V> getObject();
 
-	public String getCurrentSubtype();
+	public String getObjectId();
 
-	public void setCurrentSubtype();
+	public String getHistoryId();
+
+	public boolean isHistoryCurrent();
+
+	public CmfType getType();
+
+	public String getOriginalSubtype();
+
+	public String getSubtype();
+
+	public void setSubtype();
 
 	public Set<String> getCurrentDecorators();
 
-	public CmfProperty<CmfValue> createVariable(String name, CmfDataType type);
+	public int getDependencyTier();
 
-	public CmfProperty<CmfValue> createVariable(String name, CmfDataType type, boolean repeating);
+	public String getName();
 
-	public boolean hasVariable(String name);
+	public void setName(String name);
 
-	public void removeVariable(String name);
+	public String getProductName();
 
-	public CmfProperty<CmfValue> getVariable(String name);
+	public String getProductVersion();
+
+	public Set<String> getAttributeNames();
+
+	public boolean hasAttribute(CmfEncodeableName name);
+
+	public boolean hasAttribute(String name);
+
+	public CmfAttribute<CmfValue> getAttribute(CmfEncodeableName name);
+
+	public CmfAttribute<CmfValue> getAttribute(String name);
+
+	public CmfAttribute<CmfValue> setAttribute(CmfEncodeableName name, CmfDataType type);
+
+	public CmfAttribute<CmfValue> setAttribute(String name, CmfDataType type);
+
+	public CmfAttribute<CmfValue> setAttribute(CmfEncodeableName name, CmfDataType type, boolean multivalue);
+
+	public CmfAttribute<CmfValue> setAttribute(String name, CmfDataType type, boolean multivalue);
+
+	public CmfAttribute<CmfValue> removeAttribute(CmfEncodeableName name);
+
+	public CmfAttribute<CmfValue> removeAttribute(String name);
+
+	public Set<String> getPropertyNames();
+
+	public boolean hasProperty(CmfEncodeableName name);
+
+	public boolean hasProperty(String name);
+
+	public CmfProperty<CmfValue> getProperty(CmfEncodeableName name);
+
+	public CmfProperty<CmfValue> getProperty(String name);
+
+	public CmfProperty<CmfValue> setProperty(CmfEncodeableName name, CmfDataType type);
+
+	public CmfProperty<CmfValue> setProperty(String name, CmfDataType type);
+
+	public CmfProperty<CmfValue> setProperty(CmfEncodeableName name, CmfDataType type, boolean multivalue);
+
+	public CmfProperty<CmfValue> setProperty(String name, CmfDataType type, boolean multivalue);
+
+	public CmfProperty<CmfValue> removeProperty(CmfEncodeableName name);
+
+	public CmfProperty<CmfValue> removeProperty(String name);
 
 	public Set<String> getVariableNames();
 
+	public boolean hasVariable(String name);
+
+	public CmfProperty<CmfValue> createVariable(CmfEncodeableName name, CmfDataType type);
+
+	public CmfProperty<CmfValue> createVariable(String name, CmfDataType type);
+
+	public CmfProperty<CmfValue> createVariable(CmfEncodeableName name, CmfDataType type, boolean repeating);
+
+	public CmfProperty<CmfValue> createVariable(String name, CmfDataType type, boolean repeating);
+
+	public CmfProperty<CmfValue> getVariable(String name);
+
+	public CmfProperty<CmfValue> removeVariable(String name);
 }
