91d3c9f5a431c20e75ce67597154471a72838430
Diego Rivera <diego.rivera@armedia.com>
2017-09-28 16:37:18 -0600

    Removed unnecessary object factories

--- engine/api/src/main/java/com/armedia/caliente/engine/transform/xml/conditions/package-info.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/transform/xml/conditions/package-info.java
@@ -1,5 +1,5 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = ObjectFactory.NAMESPACE, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
-	@XmlNs(prefix = "", namespaceURI = ObjectFactory.NAMESPACE)
+@javax.xml.bind.annotation.XmlSchema(namespace = com.armedia.caliente.engine.transform.xml.ObjectFactory.NAMESPACE, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
+	@XmlNs(prefix = "", namespaceURI = com.armedia.caliente.engine.transform.xml.ObjectFactory.NAMESPACE)
 })
 package com.armedia.caliente.engine.transform.xml.conditions;
 
