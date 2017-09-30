e0b210fd6d0f507f7081cb5a55479082f941caff
Diego Rivera <diego.rivera@armedia.com>
2017-09-30 12:49:25 -0600

    Added the beginnings of the unit test for Comparisons

---
+++ engine/api/src/test/java/com/armedia/caliente/engine/transform/xml/ComparisonTest.java
@@ -0,0 +1,118 @@
+package com.armedia.caliente.engine.transform.xml;
+
+import org.junit.Test;
+
+public class ComparisonTest {
+
+	@Test
+	public void testEQ() {
+	}
+
+	@Test
+	public void testNE() {
+	}
+
+	@Test
+	public void testGT() {
+	}
+
+	@Test
+	public void testGE() {
+	}
+
+	@Test
+	public void testLT() {
+	}
+
+	@Test
+	public void testLE() {
+	}
+
+	@Test
+	public void testSW() {
+	}
+
+	@Test
+	public void testEW() {
+	}
+
+	@Test
+	public void testCN() {
+	}
+
+	@Test
+	public void testNC() {
+	}
+
+	@Test
+	public void testRE() {
+	}
+
+	@Test
+	public void testNRE() {
+	}
+
+	@Test
+	public void testGLOB() {
+	}
+
+	@Test
+	public void testNGLOB() {
+	}
+
+	@Test
+	public void testEQI() {
+	}
+
+	@Test
+	public void testNEI() {
+	}
+
+	@Test
+	public void testGTI() {
+	}
+
+	@Test
+	public void testGEI() {
+	}
+
+	@Test
+	public void testLTI() {
+	}
+
+	@Test
+	public void testLEI() {
+	}
+
+	@Test
+	public void testSWI() {
+	}
+
+	@Test
+	public void testEWI() {
+	}
+
+	@Test
+	public void testCNI() {
+	}
+
+	@Test
+	public void testNCI() {
+	}
+
+	@Test
+	public void testREI() {
+	}
+
+	@Test
+	public void testNREI() {
+	}
+
+	@Test
+	public void testGLOBI() {
+	}
+
+	@Test
+	public void testNGLOBI() {
+	}
+}
