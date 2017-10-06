faae823f01e08310064bfe77daf8543980964b36
Diego Rivera <diego.rivera@armedia.com>
2017-10-05 18:20:18 -0600

    Marshaling unit test fixed

--- engine/api/src/test/java/com/armedia/caliente/engine/xml/TransformationsTest.java
+++ engine/api/src/test/java/com/armedia/caliente/engine/xml/TransformationsTest.java
@@ -8,9 +8,6 @@ import javax.xml.bind.JAXBException;
 
 import org.junit.Test;
 
-import com.armedia.caliente.engine.xml.Transformations;
-import com.armedia.commons.utilities.XmlTools;
-
 public class TransformationsTest {
 
 	private InputStream getXml(String name) {
@@ -31,8 +28,7 @@ public class TransformationsTest {
 		l.add(3, "last");
 
 		try (InputStream in = getXml("transformations-test-1.xml")) {
-			Transformations xform = XmlTools.unmarshal(Transformations.class, "transformations.xsd", in);
-			XmlTools.marshal(xform, "transformations.xsd", System.out, true);
+			Transformations.loadFromXML(in).storeToXML(System.out, true);
 		}
 	}
 
