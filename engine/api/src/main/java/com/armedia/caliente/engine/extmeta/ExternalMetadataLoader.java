cbf0ae47d4c3515e2731292f953bc26a62639b4d
Diego Rivera <diego.rivera@armedia.com>
2017-10-10 10:10:32 -0600

    Modified to provide better error reporting

--- engine/api/src/main/java/com/armedia/caliente/engine/extmeta/ExternalMetadataLoader.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/extmeta/ExternalMetadataLoader.java
@@ -1,13 +1,17 @@
 package com.armedia.caliente.engine.extmeta;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,25 +33,24 @@ public class ExternalMetadataLoader {
 
 	private boolean initialized = false;
 
+	private final String locationDesc;
 	private final ExternalMetadata metadata;
 
-	private final Map<String, MetadataSource> sources = new HashMap<>();
+	private final Map<String, MetadataSource> sources = new LinkedHashMap<>();
 
 	private final ReadWriteLock rwLock = new ReentrantReadWriteLock();
 
 	public ExternalMetadataLoader(String location) throws ExternalMetadataException {
+		if (location == null) {
+			this.locationDesc = "the default configuration";
+		} else {
+			this.locationDesc = String.format("configuration [%s]", location);
+		}
 		try {
 			this.metadata = ExternalMetadataLoader.INSTANCES.getInstance(location);
 		} catch (XmlInstanceException e) {
-			String pre = "";
-			String post = "";
-			if (location == null) {
-				pre = "default ";
-			} else {
-				post = String.format(" from [%s]", location);
-			}
 			throw new ExternalMetadataException(
-				String.format("Failed to load the %sexternal metadata configuration%s", pre, post), e);
+				String.format("Failed to load the external metadata configuration from %s", this.locationDesc), e);
 		}
 	}
 
@@ -87,54 +90,67 @@ public class ExternalMetadataLoader {
 		}
 	}
 
-	public <V> Map<String, CmfAttribute<V>> getAttributeValues(CmfObject<V> object, String... sources)
-		throws ExternalMetadataException {
-		if (sources != null) { return getAttributeValues(object, Arrays.asList(sources)); }
+	public <V> Map<String, CmfAttribute<V>> getAttributeValues(CmfObject<V> object) throws ExternalMetadataException {
 		return getAttributeValues(object, ExternalMetadataLoader.ALL_SOURCES);
 	}
 
-	public <V> Map<String, CmfAttribute<V>> getAttributeValues(CmfObject<V> object, Collection<String> sources)
+	public <V> Map<String, CmfAttribute<V>> getAttributeValues(CmfObject<V> object, String firstSourceName,
+		String... sourceNames) throws ExternalMetadataException {
+		if (StringUtils.isEmpty(firstSourceName)) { throw new IllegalArgumentException(
+			"Must provide the name of a source to retrieve the values from"); }
+		List<String> finalSources = new ArrayList<>();
+		finalSources.add(firstSourceName);
+		for (String source : sourceNames) {
+			if (StringUtils.isEmpty(source)) { throw new IllegalArgumentException(
+				String.format("The given source name [%s] is not valid from %s", source, this.locationDesc)); }
+			finalSources.add(source);
+		}
+		return getAttributeValues(object, finalSources);
+	}
+
+	public <V> Map<String, CmfAttribute<V>> getAttributeValues(CmfObject<V> object, Collection<String> sourceNames)
 		throws ExternalMetadataException {
+		Objects.requireNonNull(object, "Must provide a CmfObject instance to retrieve extra metadata for");
 		final Lock l = this.rwLock.readLock();
 		l.lock();
 		try {
 			if (this.metadata == null) {
 				initialize(l);
 			}
-			if (sources == null) {
-				sources = this.sources.keySet();
+			if (sourceNames == null) {
+				sourceNames = this.sources.keySet();
 			}
 			Map<String, CmfAttribute<V>> finalMap = new HashMap<>();
-			for (String src : sources) {
-				final MetadataSource desc = this.sources.get(src);
-				if (desc == null) { throw new ExternalMetadataException(
-					String.format("No metadata source named [%s] has been defined", src)); }
+			for (String src : sourceNames) {
+				final MetadataSource source = this.sources.get(src);
+				if (source == null) { throw new ExternalMetadataException(
+					String.format("No metadata source named [%s] has been defined at %s", src, this.locationDesc)); }
 
 				Map<String, CmfAttribute<V>> m = null;
 				try {
-					m = desc.getAttributeValues(object);
+					m = source.getAttributeValues(object);
 				} catch (Exception e) {
-					if (desc.isFailOnError()) {
+					if (source.isFailOnError()) {
 						// There was an error which we should fail on
-						throw new ExternalMetadataException(
-							String.format("Exception caught while loading required external metadata for %s [%s](%s)",
-								object.getType(), object.getLabel(), object.getId()),
-							e);
+						throw new ExternalMetadataException(String.format(
+							"Exception caught while retrieving required external metadata for %s [%s](%s) from source [%s] at %s",
+							object.getType(), object.getLabel(), object.getId(), source.getId(), this.locationDesc), e);
 					}
-					this.log.warn("Exception caught while retrieving external metadata for {} [{}]({})",
-						object.getType(), object.getLabel(), object.getId(), e);
+					this.log.warn(
+						"Exception caught while retrieving external metadata for {} [{}]({}) from source [{}] at {}",
+						object.getType(), object.getLabel(), object.getId(), source.getId(), this.locationDesc, e);
 					continue;
 				}
 				if (m == null) {
-					if (desc.isFailOnMissing()) {
+					if (source.isFailOnMissing()) {
 						// The data is required, but not present - explode!!
-						throw new ExternalMetadataException(
-							String.format("Did not find any required external metadata for %s [%s](%s)",
-								object.getType(), object.getLabel(), object.getId()));
+						throw new ExternalMetadataException(String.format(
+							"Did not retrieve any required external metadata for %s [%s](%s) from source [%s] at %s",
+							object.getType(), object.getLabel(), object.getId(), source.getId(), this.locationDesc));
 					}
 					if (this.log.isTraceEnabled()) {
-						this.log.warn("Did not retrieve any external metadata for {} [{}]({})", object.getType(),
-							object.getLabel(), object.getId());
+						this.log.warn("Did not retrieve any external metadata for {} [{}]({}) from source [{}] at {}",
+							object.getType(), object.getLabel(), object.getId(), source.getId(), this.locationDesc);
 					}
 					continue;
 				}
@@ -154,7 +170,8 @@ public class ExternalMetadataLoader {
 				try {
 					desc.close();
 				} catch (Throwable t) {
-					this.log.warn("Exception caught while closing a metadata source", t);
+					this.log.warn("Exception caught while closing metadata source [{}] at {}", desc.getId(),
+						this.locationDesc, t);
 				}
 			}
 		} finally {
