ea4781618c764ccdba0850296c688a8fc42c29d8
Diego Rivera <diego.rivera@armedia.com>
2017-10-10 20:50:30 -0600

    Minor annotation tweak to gain more liberty

--- engine/api/src/main/java/com/armedia/caliente/engine/dynamic/Action.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/dynamic/Action.java
@@ -1,5 +1,8 @@
 package com.armedia.caliente.engine.dynamic;
 
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlTransient
 public interface Action {
 
 	public void apply(DynamicElementContext ctx) throws ActionException;
--- engine/api/src/main/java/com/armedia/caliente/engine/dynamic/Condition.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/dynamic/Condition.java
@@ -1,5 +1,8 @@
 package com.armedia.caliente.engine.dynamic;
 
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlTransient
 public interface Condition {
 
 	public boolean check(DynamicElementContext ctx) throws ConditionException;
