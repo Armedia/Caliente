88b86a0204241060b37ab9aef7ed88c6635b5a01
Diego Rivera <diego.rivera@armedia.com>
2017-11-15 10:54:29 -0600

    Modified to better handle the overrides and produce cleaner results

--- engine/impl/alfresco-bi/src/main/java/com/armedia/caliente/engine/alfresco/bi/importer/mapper/MappingRendererSet.java
+++ engine/impl/alfresco-bi/src/main/java/com/armedia/caliente/engine/alfresco/bi/importer/mapper/MappingRendererSet.java
@@ -1,39 +1,32 @@
 package com.armedia.caliente.engine.alfresco.bi.importer.mapper;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
-import org.apache.commons.lang3.StringUtils;
+import java.util.Map;
+import java.util.TreeMap;
 
 import com.armedia.caliente.engine.alfresco.bi.importer.jaxb.mapper.ResidualsMode;
-import com.armedia.caliente.engine.alfresco.bi.importer.model.SchemaMember;
 import com.armedia.caliente.store.CmfObject;
 import com.armedia.caliente.store.CmfValue;
 import com.armedia.commons.utilities.Tools;
 
 public class MappingRendererSet implements MappingRenderer {
 
-	private final SchemaMember<?> type;
+	private final String name;
 	private final ResidualsMode residualsMode;
 	private final Character separator;
 	private final List<MappingRenderer> renderers;
 
-	/**
-	 * @param type
-	 */
-	public MappingRendererSet(SchemaMember<?> type, Character separator, ResidualsMode residualsMode,
+	public MappingRendererSet(String name, Character separator, ResidualsMode residualsMode,
 		List<MappingRenderer> renderers) {
-		this.type = type;
+		this.name = name;
 		this.renderers = Tools.freezeList(renderers);
 		this.residualsMode = residualsMode;
 		this.separator = separator;
 	}
 
-	public final SchemaMember<?> getType() {
-		return this.type;
+	public String getName() {
+		return this.name;
 	}
 
 	public List<MappingRenderer> getRenderers() {
@@ -56,10 +49,8 @@ public class MappingRendererSet implements MappingRenderer {
 	 */
 	@Override
 	public final Collection<AttributeValue> render(CmfObject<CmfValue> object) {
-		Collection<AttributeValue> ret = new ArrayList<>();
+		Map<String, AttributeValue> ret = new TreeMap<>();
 
-		Set<String> mappedTargetNames = new HashSet<>();
-		Set<String> mappedSourceNames = new HashSet<>();
 		for (MappingRenderer r : this.renderers) {
 			if (r == null) {
 				continue;
@@ -71,29 +62,12 @@ public class MappingRendererSet implements MappingRenderer {
 			}
 
 			for (AttributeValue value : values) {
-				final String sourceName = value.getSourceName();
 				final String targetName = value.getTargetName();
-				final boolean override = value.isOverride();
-
-				if (!StringUtils.isBlank(sourceName)) {
-					mappedSourceNames.add(sourceName);
+				if (value.isOverride() || !ret.containsKey(targetName)) {
+					ret.put(targetName, value);
 				}
-
-				if ((this.type != null) && !this.type.getAttributeNames().contains(targetName)) {
-					// This attribute doesn't exist, we skip it
-					continue;
-				}
-
-				if (mappedTargetNames.contains(targetName) && !override) {
-					// If this attribute is already mapped, but isn't being overridden, we simply
-					// skip it
-					continue;
-				}
-
-				mappedTargetNames.add(targetName);
-				ret.add(value);
 			}
 		}
-		return ret;
+		return ret.values();
 	}
 }
