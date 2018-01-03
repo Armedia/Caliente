91419019b3e094b2731e32400046a748c35f5922
Diego Rivera <diego.rivera@armedia.com>
2018-01-03 10:38:13 -0600

    BUGFIX: Added files missing due to a bug in .gitignore

---
+++ cli/exe/caliente/src/main/java/com/armedia/caliente/cli/caliente/launcher/ucm/Caliente_encrypt.java
@@ -0,0 +1,11 @@
+package com.armedia.caliente.cli.caliente.launcher.ucm;
+
+import com.armedia.caliente.cli.caliente.launcher.AbstractEncrypt;
+
+/**
+ * The main method of this class is an entry point for the Caliente application.
+ *
+ * @author Shridev Makim 6/15/2010
+ */
+public class Caliente_encrypt extends AbstractEncrypt {
+}
