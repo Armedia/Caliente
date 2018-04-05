bb296b0f17094c313c531a77a8efb7f721efc43e
Diego Rivera <diego.rivera@armedia.com>
2018-04-05 11:31:32 -0600

    Modified the launcher to better initialize the metadata and content stores

--- cli/exe/caliente/src/main/java/com/armedia/caliente/cli/caliente/newlauncher/Launcher.java
+++ cli/exe/caliente/src/main/java/com/armedia/caliente/cli/caliente/newlauncher/Launcher.java
@@ -1,15 +1,24 @@
 package com.armedia.caliente.cli.caliente.newlauncher;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.InvalidPropertiesFormatException;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Properties;
 import java.util.TreeMap;
+
+import javax.xml.stream.XMLStreamException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
@@ -28,8 +37,13 @@ import com.armedia.caliente.cli.launcher.AbstractLauncher;
 import com.armedia.caliente.cli.launcher.CommandLineProcessingException;
 import com.armedia.caliente.cli.launcher.LaunchClasspathHelper;
 import com.armedia.caliente.cli.utils.LibLaunchHelper;
+import com.armedia.caliente.engine.tools.LocalOrganizationStrategy;
 import com.armedia.caliente.store.CmfContentStore;
 import com.armedia.caliente.store.CmfObjectStore;
+import com.armedia.caliente.store.CmfStoreFactory;
+import com.armedia.caliente.store.CmfStores;
+import com.armedia.caliente.store.xml.StoreConfiguration;
+import com.armedia.caliente.tools.xml.XmlProperties;
 import com.armedia.commons.utilities.PluggableServiceLocator;
 import com.armedia.commons.utilities.PluggableServiceSelector;
 import com.armedia.commons.utilities.Tools;
@@ -46,6 +60,11 @@ public class Launcher extends AbstractLauncher {
 	}
 
 	public static final String DEFAULT_LOG_FORMAT = CalienteBaseOptions.DEFAULT_LOG_FORMAT;
+
+	private static final String STORE_TYPE_PROPERTY = "caliente.store.type";
+	private static final String DEFAULT_DB_PATH = Paths.get("caliente").toString();
+	private static final String DEFAULT_CONTENT_PATH = Paths.get(Launcher.DEFAULT_DB_PATH, "content").toString();
+	private static final String DEFAULT_CONTENT_STRATEGY = LocalOrganizationStrategy.NAME;
 
 	private final LibLaunchHelper libLaunchHelper = new LibLaunchHelper();
 
@@ -171,9 +190,202 @@ public class Launcher extends AbstractLauncher {
 		}
 
 		// Now go try to initialize the stores if required
+		try {
+			initializeStores(baseValues);
+		} catch (Exception e) {
+			throw new CommandLineProcessingException(1, "Failed to initialize the metadata/content stores", e);
+		}
+	}
+
+	private void initializeStores(OptionValues baseValues) throws Exception {
 		if (!this.command.isRequiresStorage()) { return; }
 
-		// TODO: Initialize the content and object stores
+		String path = null;
+		if (CLIParam.content.isPresent(baseValues)) {
+			path = CLIParam.db.getString(baseValues);
+		} else {
+			path = Launcher.DEFAULT_DB_PATH;
+		}
+
+		final File objectStore = createFile(path);
+		if (objectStore.exists() && !objectStore.isFile() && !objectStore.isDirectory()) {
+			// ERROR! Not a file or directory! What is this?
+			throw new CommandLineProcessingException(1, String.format(
+				"The object at path [%s] is neither a file nor a directory - can't use it to describe the metadata store",
+				objectStore.getAbsolutePath()));
+		}
+
+		// Now we build the content store. If there is no explicit configuration or location, and
+		// the either object store's destination is a folder (existent or not), configure the
+		// content store relative to the object store
+
+		if (CLIParam.content.isPresent(baseValues)) {
+			path = CLIParam.content.getString(baseValues);
+		} else {
+			if (!objectStore.isFile()) {
+				path = null;
+			} else {
+				path = Launcher.DEFAULT_CONTENT_PATH;
+			}
+		}
+
+		final File contentStore = ((path != null) ? createFile(path) : new File(objectStore, "content"));
+		if (contentStore.exists() && !contentStore.isFile() && !contentStore.isDirectory()) {
+			// ERROR! Not a file or directory! What is this?
+			throw new CommandLineProcessingException(1, String.format(
+				"The object at path [%s] is neither a file nor a directory - can't use it to describe the content store",
+				objectStore.getAbsolutePath()));
+
+		}
+
+		CmfStores.initializeConfigurations();
+
+		Map<String, String> commonValues = new HashMap<>();
+		if (!objectStore.exists() || objectStore.isDirectory()) {
+			commonValues.put("dir.content", objectStore.getAbsolutePath());
+		}
+		if (!contentStore.exists() || contentStore.isDirectory()) {
+			commonValues.put("dir.metadata", contentStore.getAbsolutePath());
+		}
+		commonValues.put("db.name", "caliente");
+		commonValues.put(CmfStoreFactory.CFG_CLEAN_DATA, String.valueOf(this.command.isRequiresCleanData()));
+
+		StoreConfiguration cfg = CmfStores.getObjectStoreConfiguration("default");
+		if (objectStore.isFile()) {
+			applyStoreProperties(cfg, loadStoreProperties("object", objectStore.getAbsolutePath()));
+		}
+		cfg.getSettings().putAll(commonValues);
+		this.objectStore = CmfStores.createObjectStore(cfg);
+
+		final boolean directFsExport = CLIParam.direct_fs.isPresent(baseValues);
+
+		final String contentStoreName = (directFsExport ? "direct" : "default");
+		cfg = CmfStores.getContentStoreConfiguration(contentStoreName);
+		if (!directFsExport) {
+			String strategy = CLIParam.content_strategy.getString(baseValues);
+			if (StringUtils.isBlank(strategy)) {
+				strategy = Launcher.DEFAULT_CONTENT_STRATEGY;
+			}
+			if (!StringUtils.isBlank(strategy)) {
+				cfg.getSettings().put("dir.content.strategy", strategy);
+			}
+			if (contentStore.isFile()) {
+				applyStoreProperties(cfg, loadStoreProperties("content", contentStore.getAbsolutePath()));
+			}
+		}
+		cfg.getSettings().putAll(commonValues);
+		this.contentStore = CmfStores.createContentStore(cfg);
+
+		// Set the filesystem location where files will be created or read from
+		this.log.info(String.format("Using database directory: [%s]", objectStore.getAbsolutePath()));
+
+		// Set the filesystem location where the content files will be created or read from
+		this.log.info(String.format("Using content directory: [%s]", contentStore.getAbsolutePath()));
+	}
+
+	protected boolean applyStoreProperties(StoreConfiguration cfg, Properties properties) {
+		if ((properties == null) || properties.isEmpty()) { return false; }
+		String storeType = properties.getProperty(Launcher.STORE_TYPE_PROPERTY);
+		if (!StringUtils.isEmpty(storeType)) {
+			cfg.setType(storeType);
+		}
+		Map<String, String> m = cfg.getSettings();
+		for (String s : properties.stringPropertyNames()) {
+			String v = properties.getProperty(s);
+			if (v != null) {
+				m.put(s, v);
+			}
+		}
+		return true;
+	}
+
+	private File createFile(String path) {
+		return Tools.canonicalize(new File(path));
+	}
+
+	protected File locateFile(String path, boolean required) throws IOException {
+		File f = createFile(path);
+		if (!f.exists()) {
+			if (required) { throw new IOException(String.format("The file [%s] doesn't exist", f.getAbsolutePath())); }
+			return null;
+		}
+
+		// We've found the path we're looking for...verify that it's regular file. Otherwise,
+		// just ignore it. If this is an explicit configuration setting, then we explode!
+		if (!f.isFile()) {
+			if (required) { throw new IOException(
+				String.format("The file [%s] is not a regular file", f.getAbsolutePath())); }
+			return null;
+		}
+
+		// Regardless, if it exists and is a regular file, explode if we can't read it
+		if (!f.canRead()) { throw new IOException(String.format("The file [%s] can't be read", f.getAbsolutePath())); }
+
+		return f;
+	}
+
+	protected Properties loadStoreProperties(String type, String jdbcConfig) throws IOException {
+		final boolean usingDefault = (jdbcConfig == null);
+		boolean loadedSettingsFile = false;
+
+		if (usingDefault) {
+			// Follow a default value if it exists
+			jdbcConfig = String.format("caliente-%s-store.properties", type.toLowerCase());
+		}
+
+		// Try to find the file...only explode if it's been explicitly requested
+		File f = locateFile(jdbcConfig, !usingDefault);
+		if ((f == null) && usingDefault) {
+			jdbcConfig = String.format("caliente-%s-store.xml", type.toLowerCase());
+			f = locateFile(jdbcConfig, false);
+		}
+
+		if (f == null) {
+			this.console.info("No special {} store properties set, using defaulted values", type);
+			return new Properties();
+		}
+
+		final boolean supportsFallback = (!StringUtils.endsWithIgnoreCase(jdbcConfig, ".xml"));
+
+		try {
+			// Ok...so we have the file...try to load it!
+			try (InputStream xmlIn = new FileInputStream(f)) {
+				Properties p = XmlProperties.loadFromXML(xmlIn);
+				loadedSettingsFile = true;
+				return p;
+			} catch (InvalidPropertiesFormatException | XMLStreamException e) {
+				if (this.log.isTraceEnabled()) {
+					this.log.trace("The {} store properties at [{}] aren't in XML format{}", type, f.getAbsolutePath(),
+						supportsFallback ? ", trying the classic format" : "", e);
+				}
+				this.console.warn("The {} store properties at [{}] aren't in XML format", type, f.getAbsolutePath(),
+					supportsFallback ? ", trying the classic format" : "");
+			}
+
+			if (supportsFallback) {
+				// We only make it this far if the XML read failed, and the file isn't named "*.xml"
+				try (InputStream textIn = new FileInputStream(f)) {
+					Properties p = new Properties();
+					p.load(textIn);
+					loadedSettingsFile = true;
+					return p;
+				} catch (IllegalArgumentException e) {
+					if (this.log.isTraceEnabled()) {
+						this.log.trace("The {} store properties at [{}] aren't in the legacy format", type,
+							f.getAbsolutePath(), e);
+					}
+					this.console.warn("The {} store properties at [{}] aren't in the legacy format", type,
+						f.getAbsolutePath());
+				}
+			}
+		} finally {
+			if (loadedSettingsFile) {
+				this.console.info("Loaded the {} store properties from [{}]", type, f.getAbsolutePath());
+			}
+		}
+		throw new IOException(String.format(
+			"Failed to load the %s store properties from [%s] - the file is not a properties file (XML or legacy)",
+			type, f.getAbsolutePath()));
 	}
 
 	@Override
@@ -207,7 +419,7 @@ public class Launcher extends AbstractLauncher {
 		String logCfg = CLIParam.log_cfg.getString(baseValues);
 		boolean customLog = false;
 		if (logCfg != null) {
-			final File cfg = new File(logCfg);
+			final File cfg = createFile(logCfg);
 			if (cfg.exists() && cfg.isFile() && cfg.canRead()) {
 				DOMConfigurator.configureAndWatch(Tools.canonicalize(cfg).getAbsolutePath());
 				customLog = true;
