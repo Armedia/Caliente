02c66a902e615f597e8adc3b50854b301ff24c4b
Diego Rivera <diego.rivera@armedia.com>
2018-06-12 09:38:52 -0600

    More refactoring progress

--- cli/exe/caliente/src/main/java/com/armedia/caliente/cli/caliente/newlauncher/EngineProxy.java
+++ cli/exe/caliente/src/main/java/com/armedia/caliente/cli/caliente/newlauncher/EngineProxy.java
@@ -1,15 +1,33 @@
 package com.armedia.caliente.cli.caliente.newlauncher;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 
+import com.armedia.caliente.cli.OptionValues;
+import com.armedia.caliente.cli.caliente.cfg.Setting;
+import com.armedia.caliente.cli.caliente.exception.CalienteException;
 import com.armedia.caliente.cli.launcher.LaunchClasspathHelper;
+import com.armedia.caliente.engine.TransferEngine;
+import com.armedia.caliente.engine.TransferEngineSetting;
+import com.armedia.caliente.engine.TransferSetting;
+import com.armedia.caliente.engine.WarningTracker;
 import com.armedia.caliente.engine.exporter.ExportEngine;
+import com.armedia.caliente.engine.exporter.ExportEngineListener;
+import com.armedia.caliente.engine.exporter.ExportException;
+import com.armedia.caliente.engine.exporter.ExportResult;
 import com.armedia.caliente.engine.importer.ImportEngine;
+import com.armedia.caliente.engine.importer.ImportEngineListener;
+import com.armedia.caliente.engine.importer.ImportException;
+import com.armedia.caliente.engine.importer.ImportResult;
+import com.armedia.caliente.store.CmfContentStore;
+import com.armedia.caliente.store.CmfObjectCounter;
+import com.armedia.caliente.store.CmfObjectStore;
+import com.armedia.caliente.store.CmfStorageException;
 import com.armedia.caliente.tools.CmfCrypt;
 import com.armedia.commons.utilities.PluggableServiceLocator;
 import com.armedia.commons.utilities.PluggableServiceSelector;
@@ -17,17 +35,185 @@ import com.armedia.commons.utilities.Tools;
 
 public abstract class EngineProxy {
 
+	abstract class ProxyBase<LISTENER, ENGINE extends TransferEngine<?, ?, ?, ?, ?, LISTENER>> {
+
+		protected final ENGINE engine;
+
+		private ProxyBase(ENGINE engine) {
+			this.engine = engine;
+		}
+
+		public boolean addListener(LISTENER listener) {
+			return this.engine.addListener(listener);
+		}
+
+		public boolean removeListener(LISTENER listener) {
+			return this.engine.removeListener(listener);
+		}
+
+		public final CmfCrypt getCrypto() {
+			return this.engine.getCrypto();
+		}
+
+		public final boolean isSupportsDuplicateFileNames() {
+			return this.engine.isSupportsDuplicateFileNames();
+		}
+
+		public final Collection<TransferEngineSetting> getSupportedSettings() {
+			return this.engine.getSupportedSettings();
+		}
+
+		public final void initialize(Map<String, Object> settings) {
+			// By default, do nothing...maybe do threading configurations? Common stuff?
+			if (!preInitialize(settings)) { return; }
+			if (!doInitialize(settings)) { return; }
+			if (!postInitialize(settings)) { return; }
+		}
+
+		protected boolean preInitialize(Map<String, Object> settings) {
+			settings.put(TransferSetting.THREAD_COUNT.getLabel(),
+				Setting.THREADS.getInt(CommandModule.DEFAULT_THREADS));
+			return true;
+		}
+
+		protected boolean doInitialize(Map<String, Object> settings) {
+			return true;
+		}
+
+		protected boolean postInitialize(Map<String, Object> settings) {
+			return true;
+		}
+
+		public final void configure(OptionValues commandValues, Map<String, Object> settings) throws CalienteException {
+			preValidateSettings(settings);
+			if (preConfigure(commandValues, settings)) {
+				if (doConfigure(commandValues, settings)) {
+					postConfigure(commandValues, settings);
+				}
+			}
+			postValidateSettings(settings);
+		}
+
+		protected void preValidateSettings(Map<String, Object> settings) throws CalienteException {
+		}
+
+		protected boolean preConfigure(OptionValues commandValues, Map<String, Object> settings)
+			throws CalienteException {
+			return true;
+		}
+
+		protected boolean doConfigure(OptionValues commandValues, Map<String, Object> settings)
+			throws CalienteException {
+			return true;
+		}
+
+		protected void postConfigure(OptionValues commandValues, Map<String, Object> settings)
+			throws CalienteException {
+		}
+
+		protected void postValidateSettings(Map<String, Object> settings) throws CalienteException {
+
+		}
+	}
+
+	public class Exporter extends ProxyBase<ExportEngineListener, ExportEngine<?, ?, ?, ?, ?, ?>> {
+
+		protected Exporter(ExportEngine<?, ?, ?, ?, ?, ?> engine) {
+			super(engine);
+		}
+
+		public final CmfObjectCounter<ExportResult> runExport(Logger output, WarningTracker warningTracker,
+			CmfObjectStore<?, ?> objectStore, CmfContentStore<?, ?, ?> contentStore, Map<String, ?> settings)
+			throws ExportException, CmfStorageException {
+			return this.engine.runExport(output, warningTracker, objectStore, contentStore, settings);
+		}
+
+		public final CmfObjectCounter<ExportResult> runExport(Logger output, WarningTracker warningTracker,
+			CmfObjectStore<?, ?> objectStore, CmfContentStore<?, ?, ?> contentStore, Map<String, ?> settings,
+			CmfObjectCounter<ExportResult> counter) throws ExportException, CmfStorageException {
+			return this.engine.runExport(output, warningTracker, objectStore, contentStore, settings, counter);
+		}
+
+		@Override
+		protected boolean doConfigure(OptionValues commandValues, Map<String, Object> settings)
+			throws CalienteException {
+			/*
+			settings.put(TransferSetting.EXCLUDE_TYPES.getLabel(), Setting.CMF_EXCLUDE_TYPES.getString(""));
+			settings.put(TransferSetting.IGNORE_CONTENT.getLabel(), CLIParam.skip_content.isPresent());
+			settings.put(TransferSetting.LATEST_ONLY.getLabel(),
+				CLIParam.no_versions.isPresent() || CLIParam.direct_fs.isPresent());
+			settings.put(TransferSetting.NO_RENDITIONS.getLabel(),
+				CLIParam.no_renditions.isPresent() || CLIParam.direct_fs.isPresent());
+			settings.put(TransferSetting.TRANSFORMATION.getLabel(), CLIParam.transformations.getString());
+			settings.put(TransferSetting.EXTERNAL_METADATA.getLabel(), CLIParam.external_metadata.getString());
+			settings.put(TransferSetting.FILTER.getLabel(), CLIParam.filters.getString());
+			
+			ConfigurationSetting setting = null;
+			
+			setting = getUserSetting();
+			if ((this.user != null) && (setting != null)) {
+				settings.put(setting.getLabel(), this.user);
+			}
+			
+			setting = getPasswordSetting();
+			if ((this.password != null) && (setting != null)) {
+				settings.put(setting.getLabel(), this.password);
+			}
+			
+			setting = getDomainSetting();
+			if ((this.domain != null) && (setting != null)) {
+				settings.put(setting.getLabel(), this.domain);
+			}
+			*/
+			return true;
+		}
+	}
+
+	public class Importer extends ProxyBase<ImportEngineListener, ImportEngine<?, ?, ?, ?, ?, ?>> {
+
+		protected Importer(ImportEngine<?, ?, ?, ?, ?, ?> engine) {
+			super(engine);
+		}
+
+		public final CmfObjectCounter<ImportResult> runImport(Logger output, WarningTracker warningTracker,
+			CmfObjectStore<?, ?> objectStore, CmfContentStore<?, ?, ?> streamStore, Map<String, ?> settings)
+			throws ImportException, CmfStorageException {
+			return this.engine.runImport(output, warningTracker, objectStore, streamStore, settings);
+		}
+
+		public final CmfObjectCounter<ImportResult> runImport(Logger output, WarningTracker warningTracker,
+			CmfObjectStore<?, ?> objectStore, CmfContentStore<?, ?, ?> streamStore, Map<String, ?> settings,
+			CmfObjectCounter<ImportResult> counter) throws ImportException, CmfStorageException {
+			return this.engine.runImport(output, warningTracker, objectStore, streamStore, settings, counter);
+		}
+
+	}
+
 	public abstract String getName();
 
 	public abstract Set<String> getAliases();
 
 	public abstract CmfCrypt getCrypt();
 
-	@SuppressWarnings("rawtypes")
-	public abstract ExportEngine getExportEngine();
+	public final Exporter getExporter() {
+		return newExporter(getExportEngine());
+	}
 
-	@SuppressWarnings("rawtypes")
-	public abstract ImportEngine getImportEngine();
+	protected Exporter newExporter(ExportEngine<?, ?, ?, ?, ?, ?> engine) {
+		return new Exporter(engine);
+	}
+
+	protected abstract ExportEngine<?, ?, ?, ?, ?, ?> getExportEngine();
+
+	public final Importer getImporter() {
+		return newImporter(getImportEngine());
+	}
+
+	protected Importer newImporter(ImportEngine<?, ?, ?, ?, ?, ?> engine) {
+		return new Importer(engine);
+	}
+
+	protected abstract ImportEngine<?, ?, ?, ?, ?, ?> getImportEngine();
 
 	public abstract Collection<? extends LaunchClasspathHelper> getClasspathHelpers();
 
@@ -50,6 +236,7 @@ public abstract class EngineProxy {
 		}
 
 		engineProxies.setDefaultSelector(new PluggableServiceSelector<EngineProxy>() {
+
 			@Override
 			public boolean matches(EngineProxy service) {
 				if (StringUtils.equalsIgnoreCase(engine, service.getName())) { return true; }
--- cli/exe/caliente/src/main/java/com/armedia/caliente/cli/caliente/newlauncher/ExportCommandModule.java
+++ cli/exe/caliente/src/main/java/com/armedia/caliente/cli/caliente/newlauncher/ExportCommandModule.java
@@ -4,7 +4,6 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -18,20 +17,16 @@ import org.apache.commons.lang3.time.DateFormatUtils;
 import com.armedia.caliente.cli.OptionValue;
 import com.armedia.caliente.cli.OptionValues;
 import com.armedia.caliente.cli.caliente.cfg.CLIParam;
-import com.armedia.caliente.cli.caliente.cfg.Setting;
 import com.armedia.caliente.cli.caliente.exception.CalienteException;
+import com.armedia.caliente.cli.caliente.newlauncher.EngineProxy.Exporter;
 import com.armedia.caliente.cli.caliente.utils.EmailUtils;
-import com.armedia.caliente.engine.TransferSetting;
-import com.armedia.caliente.engine.exporter.ExportEngine;
 import com.armedia.caliente.engine.exporter.ExportEngineListener;
 import com.armedia.caliente.engine.exporter.ExportResult;
 import com.armedia.caliente.store.CmfContentStore;
 import com.armedia.caliente.store.CmfObjectCounter;
 import com.armedia.caliente.store.CmfObjectStore;
 import com.armedia.caliente.store.CmfType;
-import com.armedia.commons.utilities.ConfigurationSetting;
 import com.armedia.commons.utilities.PluggableServiceLocator;
-import com.armedia.commons.utilities.Tools;
 
 public class ExportCommandModule extends CommandModule {
 
@@ -52,18 +47,16 @@ public class ExportCommandModule extends CommandModule {
 		CmfContentStore<?, ?, ?> contentStore, OptionValues commandValues, Collection<String> positionals)
 		throws CalienteException {
 
-		Set<ExportResult> outcomes = Tools.parseEnumCSV(ExportResult.class, Setting.MANIFEST_OUTCOMES.getString(),
-			CommandModule.ALL, false);
-		Set<CmfType> types = Tools.parseEnumCSV(CmfType.class, Setting.MANIFEST_TYPES.getString(), CommandModule.ALL,
-			false);
+		Set<ExportResult> outcomes = commandValues.getAllEnums(ExportResult.class, false, CLIParam.manifest_outcomes);
+		Set<CmfType> types = commandValues.getAllEnums(CmfType.class, false, CLIParam.manifest_types);
 
-		final ExportEngine<?, ?, ?, ?, ?, ?> engine = engineProxy.getExportEngine();
+		final Exporter exporter = engineProxy.getExporter();
 		final ExportCommandListener mainListener = new ExportCommandListener(this.console);
 		final CalienteWarningTracker warningTracker = mainListener.getWarningTracker();
 		final CmfObjectCounter<ExportResult> counter = mainListener.getCounter();
 
-		engine.addListener(mainListener);
-		engine.addListener(new ExportManifest(outcomes, types));
+		exporter.addListener(mainListener);
+		exporter.addListener(new ExportManifest(outcomes, types));
 
 		PluggableServiceLocator<ExportEngineListener> extraListeners = new PluggableServiceLocator<>(
 			ExportEngineListener.class);
@@ -76,13 +69,11 @@ public class ExportCommandModule extends CommandModule {
 		});
 		extraListeners.setHideErrors(false);
 		for (ExportEngineListener l : extraListeners) {
-			engine.addListener(l);
+			exporter.addListener(l);
 		}
 
 		Map<String, Object> settings = new TreeMap<>();
-		prepareSettings(settings);
-
-		settings.put(TransferSetting.THREAD_COUNT.getLabel(), Setting.THREADS.getInt(CommandModule.DEFAULT_THREADS));
+		exporter.initialize(settings);
 
 		Date end = null;
 		Map<CmfType, Long> summary = null;
@@ -90,18 +81,12 @@ public class ExportCommandModule extends CommandModule {
 		StringBuilder report = new StringBuilder();
 		Date start = null;
 		try {
-			prepareState(settings);
 
-			Map<String, Object> m = loadDefaultSettings();
-			if ((m != null) && !m.isEmpty()) {
-				settings.putAll(m);
-			}
-
-			processSettings(settings);
+			exporter.configure(commandValues, settings);
 			start = new Date();
 			try {
 				this.log.info("##### Export Process Started #####");
-				engine.runExport(this.console, warningTracker, objectStore, contentStore, settings);
+				exporter.runExport(this.console, warningTracker, objectStore, contentStore, settings);
 				this.log.info("##### Export Process Finished #####");
 				summary = objectStore.getStoredObjectTypes();
 			} catch (Throwable t) {
@@ -114,7 +99,6 @@ public class ExportCommandModule extends CommandModule {
 		} finally {
 			// unlock
 			end = new Date();
-			cleanupState();
 		}
 
 		long duration = (end.getTime() - start.getTime());
@@ -142,11 +126,6 @@ public class ExportCommandModule extends CommandModule {
 			} else {
 				report.append(String.format("%n\t%s", key));
 			}
-		}
-
-		report.append(String.format("%n%n%nSettings in use:%n")).append(StringUtils.repeat("=", 30));
-		for (Setting s : Setting.values()) {
-			report.append(String.format("%n\t%s = [%s]", s.name, s.getString()));
 		}
 
 		if (summary != null) {
@@ -195,80 +174,6 @@ public class ExportCommandModule extends CommandModule {
 		}
 
 		return 0;
-	}
-
-	protected void validateState() throws CalienteException {
-		if (this.server == null) { throw new CalienteException(
-			"Must provide the location where the repository may be accessed"); }
-	}
-
-	protected final void prepareSettings(Map<String, Object> settings) throws CalienteException {
-		settings.put(TransferSetting.EXCLUDE_TYPES.getLabel(), Setting.CMF_EXCLUDE_TYPES.getString(""));
-		settings.put(TransferSetting.IGNORE_CONTENT.getLabel(), CLIParam.skip_content.isPresent());
-		settings.put(TransferSetting.LATEST_ONLY.getLabel(),
-			CLIParam.no_versions.isPresent() || CLIParam.direct_fs.isPresent());
-		settings.put(TransferSetting.NO_RENDITIONS.getLabel(),
-			CLIParam.no_renditions.isPresent() || CLIParam.direct_fs.isPresent());
-		settings.put(TransferSetting.TRANSFORMATION.getLabel(), CLIParam.transformations.getString());
-		settings.put(TransferSetting.EXTERNAL_METADATA.getLabel(), CLIParam.external_metadata.getString());
-		settings.put(TransferSetting.FILTER.getLabel(), CLIParam.filters.getString());
-
-		ConfigurationSetting setting = null;
-
-		setting = getUserSetting();
-		if ((this.user != null) && (setting != null)) {
-			settings.put(setting.getLabel(), this.user);
-		}
-
-		setting = getPasswordSetting();
-		if ((this.password != null) && (setting != null)) {
-			settings.put(setting.getLabel(), this.password);
-		}
-
-		setting = getDomainSetting();
-		if ((this.domain != null) && (setting != null)) {
-			settings.put(setting.getLabel(), this.domain);
-		}
-		customizeSettings(settings);
-	}
-
-	protected ConfigurationSetting getUserSetting() {
-		return null;
-	}
-
-	protected ConfigurationSetting getPasswordSetting() {
-		return null;
-	}
-
-	protected ConfigurationSetting getDomainSetting() {
-		return null;
-	}
-
-	protected void customizeSettings(Map<String, Object> settings) throws CalienteException {
-	}
-
-	protected Map<String, Object> loadSettings(String jobName) throws CalienteException {
-		return null;
-	}
-
-	protected Map<String, Object> loadDefaultSettings() throws CalienteException {
-		return new HashMap<>();
-	}
-
-	protected boolean storeSettings(String jobName, Map<String, Object> settings, Date exportStart, Date exportEnd)
-		throws CalienteException {
-		return false;
-	}
-
-	protected void processSettings(Map<String, Object> settings) throws CalienteException {
-	}
-
-	protected void prepareState(Map<String, Object> settings) throws CalienteException {
-
-	}
-
-	protected void cleanupState() {
-
 	}
 
 	@Override
