bffefa59dfec2a6ee736109a0d0e9bffa2156203
Diego Rivera <diego.rivera@armedia.com>
2018-07-10 13:48:37 -0600

    Simplified the error message

--- cli/api/src/main/java/com/armedia/caliente/cli/exception/MissingRequiredCommandException.java
+++ cli/api/src/main/java/com/armedia/caliente/cli/exception/MissingRequiredCommandException.java
@@ -1,10 +1,5 @@
 package com.armedia.caliente.cli.exception;
 
-import java.util.Set;
-
-import org.apache.commons.lang3.StringUtils;
-
-import com.armedia.caliente.cli.Command;
 import com.armedia.caliente.cli.CommandScheme;
 
 public class MissingRequiredCommandException extends CommandLineSyntaxException {
@@ -23,29 +18,6 @@ public class MissingRequiredCommandException extends CommandLineSyntaxException 
 
 	@Override
 	protected String renderMessage() {
-		StringBuilder msg = new StringBuilder();
-		final String nl = String.format("%n");
-		for (Command c : this.commandScheme.getCommands()) {
-			String name = c.getName();
-			Set<String> aliases = c.getAliases();
-			msg.append(nl).append('\t').append(name);
-			if (!aliases.isEmpty()) {
-				msg.append(" (");
-				boolean firstAlias = true;
-				for (String a : aliases) {
-					if (StringUtils.equalsIgnoreCase(name, a)) {
-						continue;
-					}
-					if (!firstAlias) {
-						msg.append(", ");
-					}
-					msg.append(a);
-					firstAlias = false;
-				}
-				msg.append(')');
-			}
-		}
-
-		return String.format("A command name is required, please use one of%s", msg);
+		return "A command name is required";
 	}
 }
