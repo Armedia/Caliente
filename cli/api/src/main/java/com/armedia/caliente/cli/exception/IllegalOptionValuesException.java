9232c6ff57180f41a549b5ac708cba3258d9c5be
Diego Rivera <diego.rivera@armedia.com>
2018-07-13 15:15:13 -0600

    Added support for describing allowed values when illegal values are detected, for easier command-line debugging

--- cli/api/src/main/java/com/armedia/caliente/cli/exception/IllegalOptionValuesException.java
+++ cli/api/src/main/java/com/armedia/caliente/cli/exception/IllegalOptionValuesException.java
@@ -2,18 +2,32 @@ package com.armedia.caliente.cli.exception;
 
 import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.armedia.caliente.cli.Option;
 import com.armedia.caliente.cli.OptionScheme;
+import com.armedia.caliente.cli.OptionValueFilter;
 import com.armedia.commons.utilities.Tools;
 
 public class IllegalOptionValuesException extends CommandLineSyntaxException {
 	private static final long serialVersionUID = 1L;
 
 	private final Set<String> values;
+	private final String definition;
 
 	public IllegalOptionValuesException(OptionScheme optionScheme, Option option, Set<String> values) {
 		super(optionScheme, option, null);
 		this.values = Tools.freezeCopy(values);
+		OptionValueFilter filter = option.getValueFilter();
+		String definition = null;
+		if (filter != null) {
+			definition = StringUtils.strip(filter.getDefinition());
+		}
+		if (!StringUtils.isBlank(definition)) {
+			this.definition = String.format(", must be %s", option.getValueFilter().getDefinition());
+		} else {
+			this.definition = "";
+		}
 	}
 
 	public Set<String> getValues() {
@@ -26,6 +40,8 @@ public class IllegalOptionValuesException extends CommandLineSyntaxException {
 		String longOpt = o.getLongOpt();
 		Character shortOpt = o.getShortOpt();
 		String label = "";
+		final String plural = (this.values.size() == 1 ? "" : "s");
+		final String is_are = (this.values.size() == 1 ? "is" : "are");
 		if ((longOpt != null) && (shortOpt != null)) {
 			label = String.format("-%s/--%s", shortOpt, longOpt);
 		} else if (longOpt != null) {
@@ -33,6 +49,7 @@ public class IllegalOptionValuesException extends CommandLineSyntaxException {
 		} else {
 			label = String.format("-%s", shortOpt);
 		}
-		return String.format("The values %s are not valid for the option %s", this.values, label);
+		return String.format("The value%s %s %s not valid for the option %s%s", plural, this.values, is_are, label,
+			this.definition);
 	}
 }
