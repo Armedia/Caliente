e7f4f849b4c4df7f039484b331f374b5d41ef8d7
Diego Rivera <diego.rivera@armedia.com>
2018-09-27 12:31:06 -0600

    Updated to use the correct commons-lang version

--- engine/impl/dfc/src/main/java/com/armedia/caliente/engine/dfc/importer/DctmImportUser.java
+++ engine/impl/dfc/src/main/java/com/armedia/caliente/engine/dfc/importer/DctmImportUser.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import com.armedia.caliente.engine.dfc.DctmAttributes;
 import com.armedia.caliente.engine.dfc.DctmConstant;
