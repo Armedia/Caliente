af3a967d0250555a20b5a6e29981d5f0f329184f
Diego Rivera <diego.rivera@armedia.com>
2018-10-14 13:12:34 -0600

    Removed vestigial code

--- tools/scratchpad/src/main/java/com/armedia/cmf/generator/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = com.armedia.cmf.generator.ObjectFactory.NS, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.armedia.cmf.generator;
