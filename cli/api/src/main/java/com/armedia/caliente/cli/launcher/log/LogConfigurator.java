fc32efcc4d69f7039a179ff9ff326d1ef9928f81
Diego Rivera <diego.rivera@armedia.com>
2019-02-25 12:31:46 -0600

    Minor algorithm tweak for simpler code

--- cli/api/src/main/java/com/armedia/caliente/cli/launcher/log/LogConfigurator.java
+++ cli/api/src/main/java/com/armedia/caliente/cli/launcher/log/LogConfigurator.java
@@ -50,9 +50,7 @@ public abstract class LogConfigurator {
 				try {
 					Class<?> c = Class.forName(configurator);
 					if (c.isAssignableFrom(LogConfigurator.class)) {
-						@SuppressWarnings("unchecked")
-						Class<LogConfigurator> i = (Class<LogConfigurator>) c;
-						return i.newInstance();
+						return LogConfigurator.class.cast(c.newInstance());
 					}
 				} catch (Exception e) {
 					throw new RuntimeException(
