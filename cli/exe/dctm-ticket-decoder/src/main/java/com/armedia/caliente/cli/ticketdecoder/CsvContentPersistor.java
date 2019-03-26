cf388256891c7b268c722b672432fb284fe1d876
Diego Rivera <diego.rivera@armedia.com>
2019-03-26 09:04:07 -0600

    Updated to reflect the latest structure changes

--- cli/exe/dctm-ticket-decoder/src/main/java/com/armedia/caliente/cli/ticketdecoder/CsvContentPersistor.java
+++ cli/exe/dctm-ticket-decoder/src/main/java/com/armedia/caliente/cli/ticketdecoder/CsvContentPersistor.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import org.apache.commons.text.StringEscapeUtils;
 
 import com.armedia.caliente.cli.ticketdecoder.xml.Content;
+import com.armedia.caliente.cli.ticketdecoder.xml.Page;
 import com.armedia.caliente.cli.ticketdecoder.xml.Rendition;
 import com.armedia.commons.utilities.Tools;
 import com.armedia.commons.utilities.concurrent.BaseReadWriteLockable;
@@ -16,6 +17,7 @@ public class CsvContentPersistor extends BaseReadWriteLockable implements Conten
 	private PrintWriter out = null;
 
 	private static final Rendition NULL_RENDITION = new Rendition();
+	private static final Page NULL_PAGE = new Page().setPath("");
 
 	@Override
 	public void initialize(final File target) throws Exception {
@@ -36,11 +38,19 @@ public class CsvContentPersistor extends BaseReadWriteLockable implements Conten
 		} else {
 			rendition = CsvContentPersistor.NULL_RENDITION;
 		}
+		final Page page;
+		if (!rendition.getPages().isEmpty()) {
+			page = rendition.getPages().get(0);
+		} else {
+			page = CsvContentPersistor.NULL_PAGE;
+		}
 		writeLocked(() -> {
 			this.out.printf("%s,%s,%d,%s,%s%n", //
 				StringEscapeUtils.escapeCsv(content.getId()), //
 				StringEscapeUtils.escapeCsv(content.getPath()), //
-				StringEscapeUtils.escapeCsv(rendition.getFormat()) //
+				page.getLength(), //
+				StringEscapeUtils.escapeCsv(rendition.getFormat()), //
+				StringEscapeUtils.escapeCsv(page.getPath()) //
 			);
 		});
 	}
