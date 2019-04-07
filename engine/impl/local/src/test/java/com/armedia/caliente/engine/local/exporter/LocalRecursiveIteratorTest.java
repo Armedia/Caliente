9bb4b49fb565cc048081345fa815846457dca9f9
Diego Rivera <diego.rivera@armedia.com>
2019-04-07 15:35:43 -0600

    Updated to JUnit Jupiter

--- engine/impl/local/src/test/java/com/armedia/caliente/engine/local/exporter/LocalRecursiveIteratorTest.java
+++ engine/impl/local/src/test/java/com/armedia/caliente/engine/local/exporter/LocalRecursiveIteratorTest.java
@@ -3,30 +3,30 @@ package com.armedia.caliente.engine.local.exporter;
 import java.io.File;
 import java.util.Iterator;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import com.armedia.caliente.engine.exporter.ExportTarget;
 import com.armedia.caliente.engine.local.common.LocalRoot;
 
 public class LocalRecursiveIteratorTest {
 
-	@BeforeClass
+	@BeforeAll
 	public static void setUpBeforeClass() throws Exception {
 	}
 
-	@AfterClass
+	@AfterAll
 	public static void tearDownAfterClass() throws Exception {
 	}
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() throws Exception {
 	}
 
