916280c904d36f78c49c02ec908aba75753b9fe3
Diego Rivera <diego.rivera@armedia.com>
2019-04-18 08:25:38 -0600

    BUGFIX: properly handle the new project structure

--- tools/datasource/src/main/java/com/armedia/caliente/tools/datasource/DataSourceLocator.java
+++ tools/datasource/src/main/java/com/armedia/caliente/tools/datasource/DataSourceLocator.java
@@ -2,12 +2,12 @@ package com.armedia.caliente.tools.datasource;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ServiceLoader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.armedia.commons.utilities.CfgTools;
+import com.armedia.commons.utilities.PluggableServiceLocator;
 
 public abstract class DataSourceLocator {
 
@@ -18,36 +18,43 @@ public abstract class DataSourceLocator {
 
 	public abstract DataSourceDescriptor<?> locateDataSource(CfgTools settings) throws Exception;
 
-	private static final ServiceLoader<DataSourceLocator> LOADER = ServiceLoader.load(DataSourceLocator.class);
+	private static final PluggableServiceLocator<DataSourceLocator> LOADER = new PluggableServiceLocator<>(
+		DataSourceLocator.class);
+
+	static {
+		DataSourceLocator.LOADER.setErrorListener((c, e) -> {
+			if (DataSourceLocator.LOG.isDebugEnabled()) {
+				DataSourceLocator.LOG.warn("Failed to instantiate a DataSourceLocator class", e);
+			}
+		});
+		DataSourceLocator.LOADER.setHideErrors(false);
+	}
 
 	public static void clearCaches() {
 		DataSourceLocator.LOG.debug("Clearing the DataSourceLocator ServiceLoader caches");
 		DataSourceLocator.LOADER.reload();
 	}
 
-	public static DataSourceLocator getFirstLocatorFor(String locationType) {
+	public static DataSourceLocator getFirstLocatorFor(final String locationType) {
 		DataSourceLocator.LOG.debug("Looking for the first locator to match the location type [{}]", locationType);
-		for (DataSourceLocator locator : DataSourceLocator.LOADER) {
-			if (locator.supportsLocationType(locationType)) {
-				DataSourceLocator.LOG.debug("The first locator to match the location type [{}] was of class [{}]",
-					locationType, locator.getClass().getCanonicalName());
-				return locator;
-			}
+		DataSourceLocator first = DataSourceLocator.LOADER.getFirst((l) -> l.supportsLocationType(locationType));
+		if (first != null) {
+			DataSourceLocator.LOG.debug("The first locator to match the location type [{}] was of class [{}]",
+				locationType, first.getClass().getCanonicalName());
+		} else {
+			DataSourceLocator.LOG.debug("No locator found to match the location type [{}]", locationType);
 		}
-		DataSourceLocator.LOG.debug("No locator found to match the location type [{}]", locationType);
-		return null;
+		return first;
 	}
 
-	public static List<DataSourceLocator> getAllLocatorsFor(String locationType) {
+	public static List<DataSourceLocator> getAllLocatorsFor(final String locationType) {
 		DataSourceLocator.LOG.debug("Looking for all the locators to match the location type [{}]", locationType);
 		List<DataSourceLocator> ret = new ArrayList<>();
-		for (DataSourceLocator locator : DataSourceLocator.LOADER) {
-			if (locator.supportsLocationType(locationType)) {
-				DataSourceLocator.LOG.debug("Found a first locator to match the location type [{}] of class [{}]",
-					locationType, locator.getClass().getCanonicalName());
-				ret.add(locator);
-			}
-		}
+		DataSourceLocator.LOADER.getAll((l) -> l.supportsLocationType(locationType)).forEachRemaining((l) -> {
+			DataSourceLocator.LOG.debug("Found a locator to match the location type [{}] of class [{}]", locationType,
+				l.getClass().getCanonicalName());
+			ret.add(l);
+		});
 		DataSourceLocator.LOG.debug("Returning {} locators that matched the location type [{}] ", ret.size(),
 			locationType);
 		return ret;
