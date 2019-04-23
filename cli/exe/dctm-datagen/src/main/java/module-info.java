b2a67a57998dfcfab46618957421a38e88ccb40d
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 17:17:37 -0600

    Modified for module support

---
+++ cli/exe/dctm-datagen/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.armedia.caliente.cli.dctm.datagen {
+	requires com.armedia.caliente.cli;
+	requires com.armedia.caliente.tools;
+	requires com.armedia.caliente.tools.dfc;
+	requires com.armedia.commons.utilities;
+	requires commons.csv;
+	requires dfc;
+	requires java.xml.bind;
+	requires org.apache.commons.io;
+	requires org.apache.commons.lang3;
+	requires org.apache.commons.text;
+	requires slf4j.api;
+}
