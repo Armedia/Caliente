8fb10ea8867e6f1241b0d406f201375c5a6e9b13
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 17:13:39 -0600

    Modified for module support

---
+++ cli/exe/dctm-ticket-decoder/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module com.armedia.caliente.cli.dctm.ticketdecoder {
+	requires com.armedia.caliente.cli;
+	requires com.armedia.caliente.tools;
+	requires com.armedia.caliente.tools.dfc;
+	requires com.armedia.commons.utilities;
+	requires com.ctc.wstx;
+	requires dfc;
+	requires java.scripting;
+	requires java.xml;
+	requires java.xml.bind;
+	// requires javax.xml.stream.1.0.1.v201004272200;
+	requires log4j;
+	requires org.apache.commons.lang3;
+	requires org.apache.commons.text;
+	requires org.codehaus.stax2;
+	requires slf4j.api;
+	requires stax.utils;
+}
