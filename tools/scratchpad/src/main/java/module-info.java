09fe88467b04b5af3b28f0e1cc0a78c57a466b6a
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 17:15:50 -0600

    Modified for module support

---
+++ tools/scratchpad/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.armedia.caliente.tools.scratchpad {
+	requires com.armedia.caliente.cli;
+	requires com.armedia.caliente.tools.datasource;
+	requires com.armedia.caliente.tools.dfc;
+	requires com.armedia.commons.utilities;
+	requires com.ctc.wstx;
+	requires dfc;
+	requires java.xml;
+	requires org.apache.commons.io;
+	requires org.apache.commons.lang3;
+	requires org.codehaus.stax2;
+	requires slf4j.api;
+	requires stax.utils;
+}
