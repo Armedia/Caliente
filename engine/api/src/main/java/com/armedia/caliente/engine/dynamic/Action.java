94bc7e0cfd6742ca0367433a257a792a1e47d738
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 16:06:18 -0600

    Modified to support CheckedPredicate/CheckedConsumer

--- engine/api/src/main/java/com/armedia/caliente/engine/dynamic/Action.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/dynamic/Action.java
@@ -2,10 +2,16 @@ package com.armedia.caliente.engine.dynamic;
 
 import javax.xml.bind.annotation.XmlTransient;
 
+import com.armedia.commons.utilities.function.CheckedConsumer;
+
 @XmlTransient
 @FunctionalInterface
-public interface Action {
+public interface Action extends CheckedConsumer<DynamicElementContext, ActionException> {
 
 	public void apply(DynamicElementContext ctx) throws ActionException;
 
+	@Override
+	public default void acceptChecked(DynamicElementContext ctx) throws ActionException {
+		apply(ctx);
+	}
 }
--- engine/api/src/main/java/com/armedia/caliente/engine/dynamic/Condition.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/dynamic/Condition.java
@@ -2,10 +2,16 @@ package com.armedia.caliente.engine.dynamic;
 
 import javax.xml.bind.annotation.XmlTransient;
 
+import com.armedia.commons.utilities.function.CheckedPredicate;
+
 @XmlTransient
 @FunctionalInterface
-public interface Condition {
+public interface Condition extends CheckedPredicate<DynamicElementContext, ConditionException> {
 
 	public boolean check(DynamicElementContext ctx) throws ConditionException;
 
+	@Override
+	public default boolean testChecked(DynamicElementContext ctx) throws ConditionException {
+		return check(ctx);
+	}
 }
