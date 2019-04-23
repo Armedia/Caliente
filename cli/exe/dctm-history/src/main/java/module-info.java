9d0205e66258b32419b33f05b4ba52ba3897a22d
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 17:16:29 -0600

    Modified for module support

---
+++ cli/exe/dctm-history/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.armedia.caliente.cli.dctm.history {
+	requires com.armedia.caliente.cli;
+	requires com.armedia.caliente.tools;
+	requires com.armedia.caliente.tools.dfc;
+	requires dfc;
+	requires org.apache.commons.lang3;
+	requires slf4j.api;
+}
