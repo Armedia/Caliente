82b658f0e7f035a520b83b80096af6f5ba5f5901
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 17:10:03 -0600

    Re-added the token - it's still needed

--- cli/api/src/main/java/module-info.java
+++ cli/api/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.armedia.caliente.cli {
 	exports com.armedia.caliente.cli.filter;
 	exports com.armedia.caliente.cli.launcher;
 	exports com.armedia.caliente.cli.launcher.log;
+	exports com.armedia.caliente.cli.token;
 	exports com.armedia.caliente.cli.utils;
 
 	requires java.xml;
