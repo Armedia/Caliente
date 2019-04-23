1b6b28308699ece3309347b7fe897b01ae2e80da
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 17:20:06 -0600

    Modified for module support

---
+++ cli/exe/caliente/src/main/java/module-info.java
@@ -0,0 +1,28 @@
+module com.armedia.caliente.cli.caliente {
+	requires static chemistry.opencmis.client.api;
+	requires com.armedia.caliente.cli;
+	requires com.armedia.caliente.engine;
+	requires static com.armedia.caliente.engine.alfrescobi;
+	requires static com.armedia.caliente.engine.cmis;
+	requires static com.armedia.caliente.engine.dfc;
+	requires static com.armedia.caliente.engine.local;
+	requires static com.armedia.caliente.engine.sharepoint;
+	requires static com.armedia.caliente.engine.ucm;
+	requires static com.armedia.caliente.engine.xml;
+	requires com.armedia.caliente.store;
+	requires static com.armedia.caliente.store.jdbc;
+	requires static com.armedia.caliente.store.local;
+	requires com.armedia.caliente.tools;
+	requires com.armedia.caliente.tools.datasource;
+	requires static com.armedia.caliente.tools.dfc;
+	requires com.armedia.commons.utilities;
+	requires static dfc;
+	requires java.activation;
+	requires java.xml;
+	requires log4j;
+	requires mailapi;
+	requires org.apache.commons.io;
+	requires org.apache.commons.lang3;
+	requires org.apache.commons.text;
+	requires slf4j.api;
+}
