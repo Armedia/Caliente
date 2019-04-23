7796ddcd458a6b37e351e0451b16a252f53f1e21
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 16:22:44 -0600

    Added module support

---
+++ engine/impl/alfresco-bi/src/main/java/module-info.java
@@ -0,0 +1,27 @@
+module com.armedia.caliente.engine.alfrescobi {
+	exports com.armedia.caliente.engine.alfresco.bi;
+	exports com.armedia.caliente.engine.alfresco.bi.importer;
+	// exports com.armedia.caliente.engine.alfresco.bi.importer.jaxb.model;
+	exports com.armedia.caliente.engine.alfresco.bi.importer.model;
+	// exports com.armedia.caliente.engine.alfresco.bi.importer.model.jaxb;
+
+	requires java.activation;
+	requires java.xml;
+	requires java.xml.bind;
+	// requires javax.xml.stream.1.0.1.v201004272200;
+
+	requires org.apache.commons.codec;
+	requires org.apache.commons.io;
+	requires org.apache.commons.lang3;
+	requires org.apache.commons.text;
+
+	requires transitive com.armedia.caliente.engine;
+	requires com.armedia.caliente.store;
+	requires com.armedia.caliente.tools;
+	requires transitive com.armedia.caliente.tools.alfrescobi;
+	requires com.armedia.caliente.tools.datasource;
+	requires com.armedia.commons.utilities;
+
+	requires commons.pool2;
+	requires slf4j.api;
+}
