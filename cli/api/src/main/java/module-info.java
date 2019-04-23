c8f9a7eaacb61d184823ea71a9daabd383344787
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 17:08:31 -0600

    Removed packages that shouldn't be exported

--- cli/api/src/main/java/module-info.java
+++ cli/api/src/main/java/module-info.java
@@ -3,10 +3,8 @@ module com.armedia.caliente.cli {
 	exports com.armedia.caliente.cli.classpath;
 	exports com.armedia.caliente.cli.exception;
 	exports com.armedia.caliente.cli.filter;
-	exports com.armedia.caliente.cli.help;
 	exports com.armedia.caliente.cli.launcher;
 	exports com.armedia.caliente.cli.launcher.log;
-	exports com.armedia.caliente.cli.token;
 	exports com.armedia.caliente.cli.utils;
 
 	requires java.xml;
