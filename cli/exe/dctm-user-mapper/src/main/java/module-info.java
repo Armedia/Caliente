4a7b62d823e777256860e308ebfc01c6ca91a510
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 17:11:35 -0600

    Modified for module support

---
+++ cli/exe/dctm-user-mapper/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.armedia.caliente.cli.dctm.usermapper {
+	requires com.armedia.caliente.cli;
+	requires com.armedia.caliente.tools;
+	requires com.armedia.caliente.tools.dfc;
+	requires com.armedia.commons.utilities;
+	requires commons.csv;
+	requires dfc;
+	requires java.xml.bind;
+	requires org.apache.commons.lang3;
+	requires slf4j.api;
+	requires unboundid.ldapsdk;
+}
