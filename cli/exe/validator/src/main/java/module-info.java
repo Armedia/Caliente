4f49de7f34f8bc58d5f1507de5a0ce5c6b4985d6
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 17:10:29 -0600

    Modified for module support

---
+++ cli/exe/validator/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.armedia.caliente.cli.validator {
+	requires com.armedia.caliente.cli;
+	requires com.armedia.caliente.engine;
+	requires com.armedia.caliente.engine.alfrescobi;
+	requires com.armedia.caliente.store;
+	requires com.armedia.caliente.tools;
+	requires com.armedia.caliente.tools.datasource;
+	requires com.armedia.commons.utilities;
+	requires commons.csv;
+	requires java.xml;
+	requires java.xml.bind;
+	requires org.apache.commons.io;
+	requires org.apache.commons.lang3;
+	requires slf4j.api;
+}
