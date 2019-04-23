6d21bd3e76fd193aca7bc912ab58adf191f6b53f
Diego Rivera <diego.rivera@armedia.com>
2019-04-23 17:18:23 -0600

    Modified for module support

---
+++ cli/exe/dctm-bulkdel/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.armedia.caliente.cli.dctm.bulkdel {
+	requires com.armedia.caliente.cli;
+	requires com.armedia.caliente.tools;
+	requires com.armedia.caliente.tools.dfc;
+	requires com.armedia.commons.utilities;
+	requires dfc;
+	requires slf4j.api;
+}
