f76b31d1d923a98d5f7764262a0b4b4a69312d3b
Diego Rivera <diego.rivera@armedia.com>
2019-04-30 17:29:04 -0600

    Added the base for the Flat2DB tool development

---
+++ cli/exe/flat2db/src/main/java/com/armedia/caliente/cli/flat2db/CLIParam.java
@@ -0,0 +1,57 @@
+package com.armedia.caliente.cli.flat2db;
+
+import java.util.function.Supplier;
+
+import com.armedia.caliente.cli.Option;
+import com.armedia.caliente.cli.OptionImpl;
+
+public enum CLIParam implements Supplier<Option> {
+	bulk_import(
+		new OptionImpl() //
+			.setRequired(true) //
+			.setShortOpt('i') //
+			.setMinArguments(1) //
+			.setMaxArguments(1) //
+			.setArgumentName("bulk-import-directory") //
+			.setDescription("The location of the Bulk Import source data") //
+	), //
+	bulk_export(
+		new OptionImpl() //
+			.setRequired(true) //
+			.setShortOpt('e') //
+			.setMinArguments(1) //
+			.setMaxArguments(1) //
+			.setArgumentName("bulk-export-directory") //
+			.setDescription("The location of the Bulk Export validation data") //
+	), //
+	report_dir(
+		new OptionImpl() //
+			.setShortOpt('r') //
+			.setMinArguments(1) //
+			.setMaxArguments(1) //
+			.setArgumentName("directory") //
+			.setDescription("The directory where the validation reports will be output to") //
+	), //
+	model(
+		new OptionImpl() //
+			.setRequired(true) //
+			.setShortOpt('m') //
+			.setMinArguments(1) //
+			.setMaxArguments(-1) //
+			.setArgumentName("model") //
+			.setDescription("The (list of) content model(s) in XML format and proper dependency order") //
+	), //
+		//
+	;
+
+	private final Option option;
+
+	private CLIParam(OptionImpl parameter) {
+		this.option = OptionImpl.initOptionName(this, parameter);
+	}
+
+	@Override
+	public Option get() {
+		return this.option;
+	}
+}
