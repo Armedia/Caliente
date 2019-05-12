fb5502acc7fb23dd638f57d0d2ee25d3d62d4b8b
Diego Rivera <diego.rivera@armedia.com>
2019-05-12 16:41:58 -0600

    Minor typing fix

--- tools/scratchpad/src/main/java/com/armedia/caliente/tools/PropertiesTest.java
+++ tools/scratchpad/src/main/java/com/armedia/caliente/tools/PropertiesTest.java
@@ -44,7 +44,7 @@ public class PropertiesTest {
 		}
 
 		@Override
-		public Iterator<?> getPrefixes(String namespaceURI) {
+		public Iterator<String> getPrefixes(String namespaceURI) {
 			return null;
 		}
 
