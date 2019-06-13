9511b297087a34086122818f7bc5c0296f775db4
Diego Rivera <diego.rivera@armedia.com>
2019-06-12 18:37:47 -0600

    Test almost ready - need to render the streams

--- tools/scratchpad/src/main/java/com/armedia/caliente/content/JackrabbitTest.java
+++ tools/scratchpad/src/main/java/com/armedia/caliente/content/JackrabbitTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 
 import javax.jcr.Credentials;
@@ -15,13 +16,15 @@ import javax.jcr.SimpleCredentials;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.jackrabbit.commons.JcrUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.armedia.caliente.tools.ProgressTrigger;
 import com.armedia.caliente.tools.ProgressTrigger.ProgressReport;
 
 public class JackrabbitTest implements Callable<Void> {
 
-	private InputStream generateInputStream() {
+	private InputStream generateInputStream(long count) {
 		// TODO: Generate random data? Maybe just grab a random chunk
 		// from a larger one?
 		return null;
@@ -34,12 +37,19 @@ public class JackrabbitTest implements Callable<Void> {
 		final Credentials credentials = new SimpleCredentials("admin", "admin".toCharArray());
 		final ExecutorService executor = Executors.newWorkStealingPool(10);
 		final Organizer<?> organizer = new SequentialOrganizer();
+		final Logger console = LoggerFactory.getLogger("console");
 		final Consumer<Long> startTrigger = (s) -> {
-
+			console.info("Started generating content files");
 		};
 		final Consumer<ProgressReport> progressTrigger = (pr) -> {
-
+			String id = pr.getIntervalDuration().toString();
+			String irps = String.format("%.3f", pr.getIntervalRatePerSecond());
+			String td = pr.getTotalDuration().toString();
+			String trps = String.format("%.3f", pr.getTotalRatePerSecond());
+			console.info("\n\tProgress report: {}/{} (~{}/s) | {}/{} (~{}/s)\n", pr.getIntervalCount(), id, irps,
+				pr.getTotalCount(), td, trps);
 		};
+		final AtomicLong counter = new AtomicLong(0);
 		final ProgressTrigger progress = new ProgressTrigger(startTrigger, progressTrigger);
 		try {
 			Callable<Void> worker = () -> {
@@ -49,9 +59,9 @@ public class JackrabbitTest implements Callable<Void> {
 				try {
 					Pair<Node, String> target = organizer.newContentLocation(session, client);
 					Node folder = target.getLeft();
-					try (InputStream data = generateInputStream()) {
+					try (InputStream data = generateInputStream(counter.getAndIncrement())) {
 						Node file = JcrUtils.putFile(folder, target.getRight(), "application/octet-stream", data);
-						String path = file.getPath();
+						console.info("Generated: {}", file.getPath());
 						progress.trigger();
 					}
 					session.save();
@@ -64,7 +74,6 @@ public class JackrabbitTest implements Callable<Void> {
 			for (int i = 0; i < 100000; i++) {
 				executor.submit(worker);
 			}
-
 		} finally {
 			executor.shutdown();
 			boolean abort = false;
