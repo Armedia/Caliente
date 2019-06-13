1ea8a6176e1ff35ff0754aec1493f52cbc7ee581
Diego Rivera <diego.rivera@armedia.com>
2019-06-13 12:41:55 -0600

    Fixed the default log name

--- tools/scratchpad/src/main/java/com/armedia/caliente/tools/CLIConst.java
+++ tools/scratchpad/src/main/java/com/armedia/caliente/tools/CLIConst.java
@@ -6,7 +6,7 @@ import org.apache.commons.text.StringSubstitutor;
 
 public class CLIConst {
 
-	public static final String DEFAULT_LOG_FORMAT = "caliente-dctm-ticket-decoder-${logTimeStamp}";
+	public static final String DEFAULT_LOG_FORMAT = "caliente-tools-scratchpad-${logTimeStamp}";
 
 	public static final String getLogName(Map<String, ?> properties) {
 		return StringSubstitutor.replace(CLIConst.DEFAULT_LOG_FORMAT, properties);
