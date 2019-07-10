8ca705bc210aa21c9b019d3210f9c22b900cded3
Diego Rivera <diego.rivera@armedia.com>
2019-07-10 13:29:12 -0600

    Fixed some comment typos

--- cli/api/src/main/java/com/armedia/caliente/cli/utils/DfcLaunchHelper.java
+++ cli/api/src/main/java/com/armedia/caliente/cli/utils/DfcLaunchHelper.java
@@ -4,17 +4,17 @@
  * %%
  * Copyright (c) 2010 - 2019 Armedia LLC
  * %%
- * This file is part of the Caliente software. 
- *  
- * If the software was purchased under a paid Caliente license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the Caliente software.
+ *
+ * If the software was purchased under a paid Caliente license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
  *
  * Caliente is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *   
+ *
  * Caliente is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -229,7 +229,7 @@ public final class DfcLaunchHelper extends Options implements LaunchClasspathHel
 				ret.add(CliUtils.newFileObject(f, "config").toURI().toURL());
 			}
 
-			// Next, identify the DOCUMENTUM_SHARED location, and if paramDctm.jar is in there
+			// Next, identify the DOCUMENTUM_SHARED location, and if dctm.jar is in there
 			var = cli.getString(this.paramDfc, System.getenv(DfcLaunchHelper.ENV_DOCUMENTUM_SHARED));
 			// Go with the environment
 			if (var == null) {
@@ -247,7 +247,7 @@ public final class DfcLaunchHelper extends Options implements LaunchClasspathHel
 						DfcLaunchHelper.ENV_DOCUMENTUM_SHARED, f.getAbsolutePath()));
 				}
 
-				// Next, does paramDctm.jar exist in there?
+				// Next, does dctm.jar exist in there?
 				if (!dfcFound) {
 					File tgt = CliUtils.newFileObject(f, DfcLaunchHelper.DCTM_JAR);
 					if (!tgt.isFile()) {
