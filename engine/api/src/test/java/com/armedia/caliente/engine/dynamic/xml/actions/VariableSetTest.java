1e4ae50c298ac454269ea34f03b6a30b7e1abe25
Diego Rivera <diego.rivera@armedia.com>
2019-08-20 13:32:33 -0600

    BUGFIX: Test file had the wrong name

--- engine/api/src/test/java/com/armedia/caliente/engine/dynamic/xml/actions/VariableSetTest.java
+++ engine/api/src/test/java/com/armedia/caliente/engine/dynamic/xml/actions/VariableSetTest.java
@@ -2,7 +2,7 @@ package com.armedia.caliente.engine.dynamic.xml.actions;
 
 import org.junit.jupiter.api.Test;
 
-public class VariableSet {
+public class VariableSetTest {
 	@Test
 	public void test() {
 		// TODO: Implement this test...
