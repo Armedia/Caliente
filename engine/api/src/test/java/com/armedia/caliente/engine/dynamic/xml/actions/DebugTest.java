618026a087d9dd953831caa9b0dd43986ad50a12
Diego Rivera <diego.rivera@armedia.com>
2019-08-20 18:15:48 -0600

    Added another unit test

--- engine/api/src/test/java/com/armedia/caliente/engine/dynamic/xml/actions/DebugTest.java
+++ engine/api/src/test/java/com/armedia/caliente/engine/dynamic/xml/actions/DebugTest.java
@@ -1,10 +1,22 @@
 package com.armedia.caliente.engine.dynamic.xml.actions;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import com.armedia.caliente.engine.dynamic.ActionException;
+import com.armedia.caliente.engine.dynamic.TestObjectContext;
 
 public class DebugTest {
 	@Test
-	public void test() {
-		// TODO: Implement this test...
+	public void test() throws ActionException {
+		final AtomicInteger counter = new AtomicInteger(0);
+		Debug debug = new Debug();
+		debug.getActions().add((ctx) -> counter.incrementAndGet());
+
+		Assertions.assertEquals(0, counter.get());
+		debug.apply(new TestObjectContext());
+		Assertions.assertEquals(1, counter.get());
 	}
 }
