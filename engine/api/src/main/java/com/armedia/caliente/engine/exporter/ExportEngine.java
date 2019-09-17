cee68d6c03b4470b3d035a0836a015db81b7f74f
Diego Rivera <diego.rivera@armedia.com>
2019-09-17 13:31:03 -0600

    Modified to generate the metadata filename based on the object type

--- engine/api/src/main/java/com/armedia/caliente/engine/exporter/ExportEngine.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/exporter/ExportEngine.java
@@ -522,7 +522,7 @@ public abstract class ExportEngine<//
 
 			if (ctx.isSupportsCompanionMetadata(type)) {
 				CmfContentStream md = new CmfContentStream(0, "metadata", 1);
-				md.setProperty(CmfContentStream.BASENAME, "metadata");
+				md.setProperty(CmfContentStream.BASENAME, "metadata." + type.name());
 				md.setExtension("xml");
 				CmfContentStore<?, ?>.Handle h = streamStore.getHandle(CmfAttributeTranslator.CMFVALUE_TRANSLATOR,
 					encoded, md);
