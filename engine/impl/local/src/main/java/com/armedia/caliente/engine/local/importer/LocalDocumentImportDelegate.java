f081b1227ce04e5a1810d4126dad0b28c081dc5a
Diego Rivera <diego.rivera@armedia.com>
2019-09-17 12:33:19 -0600

    Slight logic modification, preparing for multistream/version support

--- engine/impl/local/src/main/java/com/armedia/caliente/engine/local/importer/LocalDocumentImportDelegate.java
+++ engine/impl/local/src/main/java/com/armedia/caliente/engine/local/importer/LocalDocumentImportDelegate.java
@@ -27,6 +27,7 @@
 package com.armedia.caliente.engine.local.importer;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -35,9 +36,14 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import com.armedia.caliente.engine.converter.IntermediateAttribute;
 import com.armedia.caliente.engine.importer.ImportException;
 import com.armedia.caliente.engine.importer.ImportOutcome;
 import com.armedia.caliente.engine.importer.ImportResult;
+import com.armedia.caliente.engine.tools.LocalVersionedOrganizer;
+import com.armedia.caliente.engine.tools.xml.MetadataT;
+import com.armedia.caliente.engine.tools.xml.XmlBase;
+import com.armedia.caliente.store.CmfAttribute;
 import com.armedia.caliente.store.CmfAttributeTranslator;
 import com.armedia.caliente.store.CmfContentStore;
 import com.armedia.caliente.store.CmfContentStream;
@@ -63,45 +69,6 @@ public class LocalDocumentImportDelegate extends LocalImportDelegate {
 				String.format("Failed to calculate the target file for %s", this.cmfObject.getDescription()), e);
 		}
 
-		File parent = targetFile.getParentFile();
-		if (parent != null) {
-			parent.mkdirs();
-		}
-
-		final boolean created;
-		try {
-			created = targetFile.createNewFile();
-		} catch (IOException e) {
-			throw new ImportException(String.format("Failed to create the new file [%s] for %s",
-				targetFile.getAbsolutePath(), this.cmfObject.getDescription()), e);
-		}
-
-		if (!created) {
-			if (!targetFile.isFile()) {
-				throw new ImportException(String.format("Failed to create the file (and parents) at [%s] for %s",
-					targetFile, this.cmfObject.getDescription()));
-			}
-			if (!targetFile.exists()) {
-				throw new ImportException(String.format("A non-file object already exists at [%s] for %s", targetFile,
-					this.cmfObject.getDescription()));
-			}
-
-			if (this.factory.isFailOnCollisions()) {
-				throw new ImportException(String.format("A file already exists at [%s] for document [%s](%s)",
-					targetFile, this.cmfObject.getLabel(), this.cmfObject.getId()));
-			}
-
-			try {
-				if (isSameDatesAndOwners(targetFile, translator)) {
-					return Collections.singleton(
-						new ImportOutcome(ImportResult.DUPLICATE, getNewId(targetFile), targetFile.getAbsolutePath()));
-				}
-			} catch (Exception e) {
-				throw new ImportException(String.format("Failed to validate the dates and owners at [%s] for %s",
-					this.cmfObject.getDescription()), e);
-			}
-		}
-
 		// Copy the contents over...
 		List<CmfContentStream> contents;
 		try {
@@ -112,8 +79,8 @@ public class LocalDocumentImportDelegate extends LocalImportDelegate {
 				e);
 		}
 
-		if (!contents.isEmpty()) {
-			CmfContentStream info = contents.get(0);
+		boolean created = true;
+		for (CmfContentStream info : contents) {
 			CmfContentStore<?, ?>.Handle h = ctx.getContentStore().getHandle(translator, this.cmfObject, info);
 			final File src;
 			try {
@@ -122,31 +89,88 @@ public class LocalDocumentImportDelegate extends LocalImportDelegate {
 				throw new ImportException(String.format("Failed to obtain the content file for %s, content [%s]",
 					this.cmfObject.getDescription(), info), e);
 			}
+
+			if (this.factory.isIncludeAllStreams()) {
+				String baseName = LocalVersionedOrganizer.getBaseName(this.cmfObject.getTranslator(), this.cmfObject,
+					info);
+				targetFile = new File(originalTarget, baseName);
+			}
+
+			File parent = targetFile.getParentFile();
+			if (parent != null) {
+				parent.mkdirs();
+			}
+
+			try {
+				created = targetFile.createNewFile();
+			} catch (IOException e) {
+				throw new ImportException(String.format("Failed to create the new file [%s] for %s, stream %s",
+					targetFile.getAbsolutePath(), this.cmfObject.getDescription(), info), e);
+			}
+
+			if (!created) {
+				if (!targetFile.isFile()) {
+					throw new ImportException(
+						String.format("Failed to create the file (and parents) at [%s] for %s, stream %s", targetFile,
+							this.cmfObject.getDescription(), info));
+				}
+				if (!targetFile.exists()) {
+					throw new ImportException(
+						String.format("A non-file object already exists at [%s] for %s, stream %s", targetFile,
+							this.cmfObject.getDescription(), info));
+				}
+
+				if (this.factory.isFailOnCollisions()) {
+					throw new ImportException(String.format("A file already exists at [%s] for %s, stream %s",
+						targetFile, this.cmfObject.getDescription(), info));
+				}
+
+				try {
+					if (isSameDatesAndOwners(targetFile, translator)) {
+						return Collections.singleton(new ImportOutcome(ImportResult.DUPLICATE, getNewId(targetFile),
+							targetFile.getAbsolutePath()));
+					}
+				} catch (Exception e) {
+					throw new ImportException(
+						String.format("Failed to validate the dates and owners at [%s] for %s, stream %s",
+							this.cmfObject.getDescription(), info),
+						e);
+				}
+			}
+
 			if (src != null) {
 				try {
 					Files.copy(src.toPath(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
 				} catch (IOException e) {
-					throw new ImportException(String.format("Failed to copy the contents from [%s] to [%s] for %s", src,
-						targetFile, this.cmfObject.getDescription()), e);
+					throw new ImportException(
+						String.format("Failed to copy the contents from [%s] to [%s] for %s, stream %s", src,
+							targetFile, this.cmfObject.getDescription(), info),
+						e);
 				}
 			} else {
 				try (InputStream in = h.openInput()) {
 					Files.copy(in, targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
 				} catch (IOException e) {
 					throw new ImportException(
-						String.format("Failed to copy the default content object into [%s] for %s", targetFile,
-							this.cmfObject.getDescription()),
+						String.format("Failed to copy the default content object into [%s] for %s, stream %s",
+							targetFile, this.cmfObject.getDescription(), info),
 						e);
 				}
 			}
+
+			try {
+				applyAttributes(targetFile, translator);
+			} catch (Exception e) {
+				throw new ImportException(
+					String.format("Failed to apply attributes to the target file [%s] for %s, stream %s", targetFile,
+						this.cmfObject.getDescription(), info),
+					e);
+			}
+
+			// We only do the primary stream, so break
+			break;
 		}
 
-		try {
-			applyAttributes(targetFile, translator);
-		} catch (Exception e) {
-			throw new ImportException(String.format("Failed to apply attributes to the target file [%s] for %s",
-				targetFile, this.cmfObject.getDescription()), e);
-		}
 		return Collections.singleton(new ImportOutcome(created ? ImportResult.CREATED : ImportResult.UPDATED,
 			getNewId(targetFile), targetFile.getAbsolutePath()));
 	}
