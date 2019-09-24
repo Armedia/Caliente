6514e70195e49cc007a6c717a6eef2afe2c9aef9
Diego Rivera <diego.rivera@armedia.com>
2019-09-24 10:01:22 -0600

    Removed an unnecessary TODO marker

--- engine/api/src/main/java/com/armedia/caliente/engine/dynamic/xml/metadata/MetadataFromDDL.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/dynamic/xml/metadata/MetadataFromDDL.java
@@ -168,9 +168,6 @@ public class MetadataFromDDL extends MetadataReaderBase {
 							CmfValueCodec<V> codec = translator.getCodec(attribute.getType());
 							V finalValue = codec.getNullValue();
 							Object value = getValue(rs, column, attribute.getType());
-							// TODO: Use the structure information to determine if we need to
-							// deserialize the value or not... should we also make this bit
-							// configurable?
 							if (!rs.wasNull()) {
 								finalValue = translator.getValue(attribute.getType(), value);
 							}
