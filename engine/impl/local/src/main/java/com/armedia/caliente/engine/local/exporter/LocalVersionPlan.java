66911b7973202eafc4eaf0eba8e0a7f0fac853d6
Diego Rivera <diego.rivera@armedia.com>
2019-12-24 12:15:23 -0600

    Rolled back empty tag code: this should be handled by the tag comparator

--- engine/impl/local/src/main/java/com/armedia/caliente/engine/local/exporter/LocalVersionPlan.java
+++ engine/impl/local/src/main/java/com/armedia/caliente/engine/local/exporter/LocalVersionPlan.java
@@ -6,31 +6,23 @@ import java.util.Objects;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
 
-import org.apache.commons.lang3.StringUtils;
-
 public final class LocalVersionPlan implements Comparator<String> {
 
 	private final LocalFile pattern;
 	private final Comparator<String> tagCompare;
 	private final BiPredicate<LocalFile, Path> siblingCheck;
 	private final Function<Path, Path> converter;
-	private final boolean emptyTagRoot;
 
 	public LocalVersionPlan(LocalFile pattern, Comparator<String> tagCompare, Function<Path, Path> converter,
-		BiPredicate<LocalFile, Path> siblingCheck, boolean emptyTagRoot) {
+		BiPredicate<LocalFile, Path> siblingCheck) {
 		this.pattern = Objects.requireNonNull(pattern, "Must provide a file to pattern the plan on");
 		this.tagCompare = Objects.requireNonNull(tagCompare, "Must provide a Comparator to order tags with");
 		this.siblingCheck = Objects.requireNonNull(siblingCheck, "Must provide a BiPredicate to check for siblings");
 		this.converter = (converter != null ? converter : Function.identity());
-		this.emptyTagRoot = emptyTagRoot;
 	}
 
 	public final LocalFile getPatternFile() {
 		return this.pattern;
-	}
-
-	public final boolean isEmptyTagRoot() {
-		return this.emptyTagRoot;
 	}
 
 	@Override
@@ -39,8 +31,6 @@ public final class LocalVersionPlan implements Comparator<String> {
 		if (a == null) { return -1; }
 		if (b == null) { return 1; }
 		if (Objects.equals(a, b)) { return 0; }
-		if (StringUtils.isEmpty(a)) { return (this.emptyTagRoot ? -1 : 1); }
-		if (StringUtils.isEmpty(b)) { return (this.emptyTagRoot ? 1 : -1); }
 		return this.tagCompare.compare(a, b);
 	}
 
