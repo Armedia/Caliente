8a381a4fc67101bf3f90261a3bf2861a23f0ecff
Diego Rivera <diego.rivera@armedia.com>
2020-01-15 20:29:52 -0600

    Fixed unit tests for the joiner tags

--- engine/api/src/test/java/com/armedia/caliente/engine/dynamic/xml/actions/AttributeJoinTest.java
+++ engine/api/src/test/java/com/armedia/caliente/engine/dynamic/xml/actions/AttributeJoinTest.java
@@ -1,22 +1,26 @@
 package com.armedia.caliente.engine.dynamic.xml.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.armedia.caliente.engine.dynamic.ActionException;
 import com.armedia.caliente.engine.dynamic.DynamicValue;
 import com.armedia.caliente.engine.dynamic.TestObjectContext;
-import com.armedia.caliente.engine.dynamic.TestObjectFacade;
 import com.armedia.caliente.engine.dynamic.xml.Expression;
 import com.armedia.caliente.store.CmfValue.Type;
+import com.armedia.commons.utilities.Tools;
 
 public class AttributeJoinTest {
 	@Test
 	public void test() throws ActionException {
+		final Random r = new Random(System.nanoTime());
 		TestObjectContext ctx = new TestObjectContext();
-		TestObjectFacade object = ctx.getDynamicObject();
 
-		AttributeRemove action = new AttributeRemove();
+		AttributeJoin action = new AttributeJoin();
 		try {
 			action.apply(ctx);
 			Assertions.fail("Did not fail with a null name");
@@ -26,11 +30,112 @@ public class AttributeJoinTest {
 
 		final String attributeName = "testAttribute";
 
-		action.setName(new Expression(attributeName));
-		object.getAtt().put(attributeName, new DynamicValue(attributeName, Type.BOOLEAN, true));
+		final String separators = "`~!@#$%^&*()_+[]\\{}|;':\",./<>?";
 
-		Assertions.assertTrue(object.getAtt().containsKey(attributeName));
-		action.apply(ctx);
-		Assertions.assertFalse(object.getAtt().containsKey(attributeName));
+		action.setName(new Expression(attributeName));
+
+		action.setKeepEmpty(true);
+		for (int i = 0; i < separators.length(); i++) {
+			final char sep = separators.charAt(i);
+			action.setSeparator(new Expression(String.valueOf(sep)));
+			DynamicValue orig = new DynamicValue(attributeName, Type.STRING, true);
+			ctx.getDynamicObject().getAtt().put(orig.getName(), orig);
+
+			// Render the splittable value, and render its split version
+			List<String> baseStrings = new ArrayList<>(8);
+			for (int j = 0; j < 8; j++) {
+				String str = String.format("%016x", r.nextLong());
+				if ((i % 2) == 0) {
+					str = String.format("%s%s", str, sep);
+				}
+				baseStrings.add(str);
+			}
+			String finalString = Tools.joinEscaped(sep, baseStrings);
+			orig.setValue(finalString);
+
+			action.apply(ctx);
+			DynamicValue result = ctx.getDynamicObject().getAtt().get(attributeName);
+			Assertions.assertEquals(1, result.getSize(), () -> String.format("Separator: %s", sep));
+			Assertions.assertEquals(finalString, Tools.toString(result.getValue()),
+				() -> String.format("Separator: %s", sep));
+		}
+
+		action.setKeepEmpty(false);
+		for (int i = 0; i < separators.length(); i++) {
+			final char sep = separators.charAt(i);
+			action.setSeparator(new Expression(String.valueOf(sep)));
+			DynamicValue orig = new DynamicValue(attributeName, Type.STRING, true);
+			ctx.getDynamicObject().getAtt().put(orig.getName(), orig);
+
+			// Render the splittable value, and render its split version
+			List<String> baseStrings = new ArrayList<>(8);
+			for (int j = 0; j < 8; j++) {
+				String str = String.format("%016x", r.nextLong());
+				if ((i % 2) == 0) {
+					str = String.format("%s%s", str, sep);
+				}
+				baseStrings.add(str);
+			}
+			String finalString = Tools.joinEscaped(sep, baseStrings);
+			orig.setValues(baseStrings);
+
+			action.apply(ctx);
+			DynamicValue result = ctx.getDynamicObject().getAtt().get(attributeName);
+			Assertions.assertEquals(1, result.getSize(), () -> String.format("Separator: %s", sep));
+			Assertions.assertEquals(finalString, Tools.toString(result.getValue()),
+				() -> String.format("Separator: %s", sep));
+		}
+
+		for (int i = 0; i < separators.length(); i++) {
+			final char sep = separators.charAt(i);
+			action.setSeparator(new Expression(String.valueOf(sep)));
+			DynamicValue orig = new DynamicValue(attributeName, Type.STRING, true);
+			ctx.getDynamicObject().getAtt().put(orig.getName(), orig);
+
+			// Render the splittable value, and render its split version
+			List<String> baseStrings = new ArrayList<>(8);
+			for (int j = 0; j < 8; j++) {
+				String str = String.format("%016x", r.nextLong());
+				if ((i % 2) == 0) {
+					String str2 = String.format("%016x", r.nextLong());
+					str = String.format("%s%s%s", str, sep, str2);
+				}
+				baseStrings.add(str);
+			}
+			String finalString = Tools.joinEscaped(sep, baseStrings);
+			orig.setValues(baseStrings);
+
+			action.apply(ctx);
+			DynamicValue result = ctx.getDynamicObject().getAtt().get(attributeName);
+			Assertions.assertEquals(1, result.getSize(), () -> String.format("Separator: %s", sep));
+			Assertions.assertEquals(finalString, Tools.toString(result.getValue()),
+				() -> String.format("Separator: %s", sep));
+		}
+
+		for (int i = 0; i < separators.length(); i++) {
+			final char sep = separators.charAt(i);
+			action.setSeparator(new Expression(String.valueOf(sep)));
+			DynamicValue orig = new DynamicValue(attributeName, Type.STRING, true);
+			ctx.getDynamicObject().getAtt().put(orig.getName(), orig);
+
+			// Render the splittable value, and render its split version
+			List<String> baseStrings = new ArrayList<>(8);
+			for (int j = 0; j < 8; j++) {
+				String str = String.format("%016x", r.nextLong());
+				if ((i % 2) == 0) {
+					String str2 = String.format("%016x", r.nextLong());
+					str = String.format("%s\\%s%s", str, sep, str2);
+				}
+				baseStrings.add(str);
+			}
+			String finalString = Tools.joinEscaped(sep, baseStrings);
+			orig.setValues(baseStrings);
+
+			action.apply(ctx);
+			DynamicValue result = ctx.getDynamicObject().getAtt().get(attributeName);
+			Assertions.assertEquals(1, result.getSize(), () -> String.format("Separator: %s", sep));
+			Assertions.assertEquals(finalString, Tools.toString(result.getValue()),
+				() -> String.format("Separator: %s", sep));
+		}
 	}
 }
