1522667167f1e35ec7719d1c0ab3e19ce0ecc0c2
Diego Rivera <diego.rivera@armedia.com>
2020-05-29 17:11:12 -0600

    Minor fix: re-add the context

--- engine/api/src/main/java/com/armedia/caliente/engine/exporter/ContentExtractor.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/exporter/ContentExtractor.java
@@ -9,7 +9,8 @@ import com.armedia.caliente.store.CmfObject;
 
 public interface ContentExtractor extends AutoCloseable {
 
-	public <VALUE> List<CmfContentStream> storeContent(CmfAttributeTranslator<VALUE> translator,
-		CmfObject<VALUE> marshalled, CmfContentStore<?, ?> streamStore, boolean includeRenditions);
+	public <VALUE, CONTEXT extends ExportContext<?, VALUE, ?>> List<CmfContentStream> storeContent(CONTEXT ctx,
+		CmfAttributeTranslator<VALUE> translator, CmfObject<VALUE> marshalled, CmfContentStore<?, ?> streamStore,
+		boolean includeRenditions) throws ExportException;
 
 }
