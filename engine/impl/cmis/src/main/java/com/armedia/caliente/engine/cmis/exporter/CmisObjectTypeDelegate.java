588f6c27f9b49eadc3a311dfc0934d2c60e626b7
Diego Rivera <diego.rivera@armedia.com>
2020-06-01 20:20:50 -0600

    Added code to facilitate reading property definition values

--- engine/impl/cmis/src/main/java/com/armedia/caliente/engine/cmis/exporter/CmisObjectTypeDelegate.java
+++ engine/impl/cmis/src/main/java/com/armedia/caliente/engine/cmis/exporter/CmisObjectTypeDelegate.java
@@ -27,7 +27,9 @@
 package com.armedia.caliente.engine.cmis.exporter;
 
 import java.util.Collection;
+import java.util.EnumMap;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
@@ -35,7 +37,12 @@ import java.util.function.Function;
 
 import org.apache.chemistry.opencmis.client.api.ObjectType;
 import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.commons.definitions.PropertyDateTimeDefinition;
+import org.apache.chemistry.opencmis.commons.definitions.PropertyDecimalDefinition;
 import org.apache.chemistry.opencmis.commons.definitions.PropertyDefinition;
+import org.apache.chemistry.opencmis.commons.definitions.PropertyIntegerDefinition;
+import org.apache.chemistry.opencmis.commons.definitions.PropertyStringDefinition;
+import org.apache.chemistry.opencmis.commons.enums.PropertyType;
 
 import com.armedia.caliente.engine.exporter.ExportException;
 import com.armedia.caliente.store.CmfObject;
@@ -44,24 +51,143 @@ import com.armedia.commons.utilities.Tools;
 
 public class CmisObjectTypeDelegate extends CmisExportDelegate<ObjectType> {
 
-	private static final Map<String, Function<PropertyDefinition<?>, Object>> READERS;
+	private static class PropertyReader<T extends PropertyDefinition<?>> {
+		private final Class<T> propertyClass;
+		private final Function<T, Object> reader;
+
+		private PropertyReader(Function<T, Object> reader) {
+			this(null, reader);
+		}
+
+		private PropertyReader(Class<T> propertyClass, Function<T, Object> reader) {
+			this.propertyClass = propertyClass;
+			this.reader = reader;
+		}
+
+		public Object readValue(Object def) {
+			if (this.propertyClass != null) {
+				if (!this.propertyClass.isInstance(def)) { return null; }
+				return this.reader.apply(this.propertyClass.cast(def));
+			}
+
+			if (!PropertyDefinition.class.isInstance(def)) { return null; }
+
+			@SuppressWarnings("unchecked")
+			T t = (T) def;
+			return this.reader.apply(t);
+		}
+	}
+
+	private static final Map<String, PropertyReader<?>> COMMON_READERS;
+	private static final Map<PropertyType, Map<String, PropertyReader<?>>> TYPED_READERS;
 	static {
-		Map<String, Function<PropertyDefinition<?>, Object>> readers = new HashMap<>();
-		readers.put("id", PropertyDefinition::getId);
-		readers.put("localNamespace", PropertyDefinition::getLocalNamespace);
-		readers.put("localName", PropertyDefinition::getLocalName);
-		readers.put("queryName", PropertyDefinition::getQueryName);
-		readers.put("displayName", PropertyDefinition::getDisplayName);
-		readers.put("description", PropertyDefinition::getDescription);
-		readers.put("propertyType", PropertyDefinition::getPropertyType);
-		readers.put("cardinality", PropertyDefinition::getCardinality);
-		readers.put("updatability", PropertyDefinition::getUpdatability);
-		readers.put("inherited", PropertyDefinition::isInherited);
-		readers.put("required", PropertyDefinition::isRequired);
-		readers.put("queryable", PropertyDefinition::isQueryable);
-		readers.put("orderable", PropertyDefinition::isOrderable);
-		readers.put("openChoice", PropertyDefinition::isOpenChoice);
-		READERS = Tools.freezeMap(readers);
+		Map<String, PropertyReader<?>> readers = new HashMap<>();
+		readers.put("id", new PropertyReader<>(PropertyDefinition::getId));
+		readers.put("localNamespace", new PropertyReader<>(PropertyDefinition::getLocalNamespace));
+		readers.put("localName", new PropertyReader<>(PropertyDefinition::getLocalName));
+		readers.put("queryName", new PropertyReader<>(PropertyDefinition::getQueryName));
+		readers.put("displayName", new PropertyReader<>(PropertyDefinition::getDisplayName));
+		readers.put("description", new PropertyReader<>(PropertyDefinition::getDescription));
+		readers.put("propertyType", new PropertyReader<>(PropertyDefinition::getPropertyType));
+		readers.put("cardinality", new PropertyReader<>(PropertyDefinition::getCardinality));
+		readers.put("updatability", new PropertyReader<>(PropertyDefinition::getUpdatability));
+		readers.put("inherited", new PropertyReader<>(PropertyDefinition::isInherited));
+		readers.put("required", new PropertyReader<>(PropertyDefinition::isRequired));
+		readers.put("queryable", new PropertyReader<>(PropertyDefinition::isQueryable));
+		readers.put("orderable", new PropertyReader<>(PropertyDefinition::isOrderable));
+		readers.put("openChoice", new PropertyReader<>(PropertyDefinition::isOpenChoice));
+		COMMON_READERS = Tools.freezeMap(readers);
+
+		Map<PropertyType, Map<String, PropertyReader<?>>> typedReaders = new EnumMap<>(PropertyType.class);
+
+		// Integer
+		readers = new HashMap<>();
+		readers.put("minValue",
+			new PropertyReader<>(PropertyIntegerDefinition.class, PropertyIntegerDefinition::getMinValue));
+		readers.put("maxValue",
+			new PropertyReader<>(PropertyIntegerDefinition.class, PropertyIntegerDefinition::getMaxValue));
+		typedReaders.put(PropertyType.INTEGER, Tools.freezeMap(readers));
+
+		// Decimal
+		readers = new HashMap<>();
+		readers.put("minValue",
+			new PropertyReader<>(PropertyDecimalDefinition.class, PropertyDecimalDefinition::getMinValue));
+		readers.put("maxValue",
+			new PropertyReader<>(PropertyDecimalDefinition.class, PropertyDecimalDefinition::getMaxValue));
+		readers.put("precision",
+			new PropertyReader<>(PropertyDecimalDefinition.class, PropertyDecimalDefinition::getPrecision));
+		typedReaders.put(PropertyType.DECIMAL, Tools.freezeMap(readers));
+
+		// Date
+		readers = new HashMap<>();
+		readers.put("dateTimeResolution",
+			new PropertyReader<>(PropertyDateTimeDefinition.class, PropertyDateTimeDefinition::getDateTimeResolution));
+		typedReaders.put(PropertyType.DATETIME, Tools.freezeMap(readers));
+
+		// String
+		readers = new HashMap<>();
+		readers.put("maxLength",
+			new PropertyReader<>(PropertyStringDefinition.class, PropertyStringDefinition::getMaxLength));
+		typedReaders.put(PropertyType.STRING, Tools.freezeMap(readers));
+
+		TYPED_READERS = Tools.freezeMap(typedReaders);
+	}
+
+	protected static Map<String, String> getDefinitionValues(PropertyDefinition<?> property) {
+		/*-
+		"my:stringProperty":{
+		    "id":"my:stringProperty",
+		    "localNamespace":"local",
+		    "localName":"my:stringProperty",
+		    "queryName":"my:stringProperty",
+		    "displayName":"My String Property",
+		    "description":"This is a String.~,
+		    "propertyType":"string",
+		    "updatability":"readwrite",
+		    "inherited":false,
+		    "openChoice":false,
+		    "required":false,
+		    "cardinality":"single",
+		    "queryable":true,
+		    "orderable":true,
+		}
+		*/
+
+		Map<String, String> values = new LinkedHashMap<>();
+		for (String name : CmisObjectTypeDelegate.COMMON_READERS.keySet()) {
+			Object o = CmisObjectTypeDelegate.COMMON_READERS.get(name).readValue(property);
+			if (o == null) {
+				continue;
+			}
+
+			if (o.getClass().isEnum()) {
+				o = Enum.class.cast(o).name();
+			} else {
+				o = Tools.toString(o);
+			}
+
+			values.put(name, o.toString());
+		}
+
+		Map<String, PropertyReader<?>> typedReaders = CmisObjectTypeDelegate.TYPED_READERS
+			.get(property.getPropertyType());
+		if (typedReaders != null) {
+			for (String name : typedReaders.keySet()) {
+				Object o = typedReaders.get(name).readValue(property);
+				if (o == null) {
+					continue;
+				}
+
+				if (o.getClass().isEnum()) {
+					o = Enum.class.cast(o).name();
+				} else {
+					o = Tools.toString(o);
+				}
+
+				values.put(name, o.toString());
+			}
+		}
+		return values;
 	}
 
 	protected CmisObjectTypeDelegate(CmisExportDelegateFactory factory, Session session, ObjectType objectType)
@@ -78,45 +204,9 @@ public class CmisObjectTypeDelegate extends CmisExportDelegate<ObjectType> {
 		for (PropertyDefinition<?> p : this.object.getPropertyDefinitions().values()) {
 			// TODO: How to encode this information in an "engine-neutral" fashion?
 
-			/*-
-			"my:stringProperty":{
-			    "id":"my:stringProperty",
-			    "localNamespace":"local",
-			    "localName":"my:stringProperty",
-			    "queryName":"my:stringProperty",
-			    "displayName":"My String Property",
-			    "description":"This is a String.~,
-			    "propertyType":"string",
-			    "updatability":"readwrite",
-			    "inherited":false,
-			    "openChoice":false,
-			    "required":false,
-			    "cardinality":"single",
-			    "queryable":true,
-			    "orderable":true,
-			}
-			*/
-
-			for (String name : CmisObjectTypeDelegate.READERS.keySet()) {
-				Object o = CmisObjectTypeDelegate.READERS.get(name).apply(p);
-				if (o == null) {
-					continue;
-				}
-
-				if (o.getClass().isEnum()) {
-					o = Enum.class.cast(o).name();
-				} else {
-					o = Tools.toString(o);
-				}
-
-				// TODO: Now, what?
-			}
-
-			// TODO: Handle these
-			/*
-			List<?> defaultValue = p.getDefaultValue();
-			List<? extends Choice<?>> choices = p.getChoices();
-			*/
+			Map<String, String> values = CmisObjectTypeDelegate.getDefinitionValues(p);
+			values.size();
+			// TODO: Where to stow values?
 
 		}
 		return true;
