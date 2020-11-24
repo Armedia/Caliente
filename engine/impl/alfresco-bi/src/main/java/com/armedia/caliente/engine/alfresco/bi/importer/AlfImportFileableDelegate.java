eb6624689659a8d7610090f40ee341620fa61be5
Diego Rivera <diego.rivera@armedia.com>
2020-11-23 19:54:33 -0600

    Minor tweak: code cleanup adds "this" dereferences

--- engine/impl/alfresco-bi/src/main/java/com/armedia/caliente/engine/alfresco/bi/importer/AlfImportFileableDelegate.java
+++ engine/impl/alfresco-bi/src/main/java/com/armedia/caliente/engine/alfresco/bi/importer/AlfImportFileableDelegate.java
@@ -375,7 +375,7 @@ abstract class AlfImportFileableDelegate extends AlfImportDelegate {
 		// Now, get the head object
 		currentProperty = "cm:name";
 		if (includeProperty(currentProperty, targetType)) {
-			p.setProperty(currentProperty, factory.getFinalName(ctx, cmfObject));
+			p.setProperty(currentProperty, this.factory.getFinalName(ctx, this.cmfObject));
 		}
 	}
 
