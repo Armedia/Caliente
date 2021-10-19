ace76fc74dc53109f0a868733678764fdf9d0c91
Diego Rivera <diego.rivera@armedia.com>
2021-10-19 10:42:01 -0600

    Improve borked checkout protection

--- engine/impl/cmis/src/main/java/com/armedia/caliente/engine/cmis/importer/CmisDocumentDelegate.java
+++ engine/impl/cmis/src/main/java/com/armedia/caliente/engine/cmis/importer/CmisDocumentDelegate.java
@@ -132,20 +132,22 @@ public class CmisDocumentDelegate extends CmisFileableDelegate<Document> {
 		} catch (RuntimeException | Error e) {
 			// We exploded during checkout, but didn't get a handle to the potentially
 			// checked-out document ...
-			this.log.warn(
-				"Creating a new version of {} failed, but a checkout may have been left lingering, will attempt to cancel it",
-				this.cmfObject.getDescription(), e);
-			for (int i = 1; i <= 3; i++) {
-				try {
-					existing.refresh();
-					if (existing.isVersionSeriesCheckedOut()) {
-						newVersion = Document.class
-							.cast(ctx.getSession().getObject(existing.getVersionSeriesCheckedOutId()));
+			if (newVersion == null) {
+				this.log.warn(
+					"Creating a new version of {} failed, but a checkout may have been left lingering, will attempt to cancel it",
+					this.cmfObject.getDescription(), e);
+				for (int i = 1; i <= 3; i++) {
+					try {
+						existing.refresh();
+						if (existing.isVersionSeriesCheckedOut()) {
+							newVersion = Document.class
+								.cast(ctx.getSession().getObject(existing.getVersionSeriesCheckedOutId()));
+						}
+						break;
+					} catch (RuntimeException e2) {
+						this.log.warn("Refresh of PWC for {} failed (attempt # {})", this.cmfObject.getDescription(), i,
+							e2);
 					}
-					break;
-				} catch (RuntimeException e2) {
-					this.log.warn("Refresh of PWC for {} failed (attempt # {})", this.cmfObject.getDescription(), i,
-						e2);
 				}
 			}
 			throw e;
