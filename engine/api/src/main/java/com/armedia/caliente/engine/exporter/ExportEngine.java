0a30c02b33c9ea8d9761357525276acd7bc6064c
Diego Rivera <diego.rivera@armedia.com>
2021-12-18 21:42:14 -0600

    Added a content stream extraction retry cycle

--- engine/api/src/main/java/com/armedia/caliente/engine/exporter/ExportEngine.java
+++ engine/api/src/main/java/com/armedia/caliente/engine/exporter/ExportEngine.java
@@ -651,15 +651,27 @@ public abstract class ExportEngine<//
 				}
 			}
 
-			try {
-				final boolean includeRenditions = !ctx.getSettings().getBoolean(TransferSetting.NO_RENDITIONS);
-				List<CmfContentStream> contentStreams = sourceObject.storeContent(ctx, getTranslator(), marshaled,
-					streamStore, includeRenditions);
+			// TODO: Make this configurable?
+			final int maxAttempts = 3;
+			for (int attempt = 1; attempt <= maxAttempts; attempt++) {
+				List<CmfContentStream> contentStreams = null;
+				try {
+					final boolean includeRenditions = !ctx.getSettings().getBoolean(TransferSetting.NO_RENDITIONS);
+					contentStreams = sourceObject.storeContent(ctx, getTranslator(), marshaled, streamStore,
+						includeRenditions);
+				} catch (Exception e) {
+					if (attempt < maxAttempts) {
+						this.log.warn("Failed to execute the content storage for {} (attempt #{})", logLabel, attempt,
+							e);
+						continue;
+					}
+					throw new ExportException(String.format(
+						"Failed to execute the content storage for %s after %d attempts", logLabel, maxAttempts), e);
+				}
 				if ((contentStreams != null) && !contentStreams.isEmpty()) {
 					objectStore.setContentStreams(marshaled, contentStreams);
 				}
-			} catch (Exception e) {
-				throw new ExportException(String.format("Failed to execute the content storage for %s", logLabel), e);
+				break;
 			}
 
 			if (this.log.isDebugEnabled()) {
